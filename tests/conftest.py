7036c14ecd6b751bb4fdf675279b4c30f486f301
richtier <contact@richardtier.co.uk>
2020-02-13 22:01:57 +0000

    Store user location

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
+from unittest import mock
+
 import pytest
 from wagtail.core.models import Page
 from wagtail_factories import SiteFactory, PageFactory
 
 from tests.domestic import factories
+from tests.helpers import create_response
+
+from sso.models import BusinessSSOUser
 
 
 @pytest.fixture
@@ -22,3 +27,44 @@ def domestic_homepage(root_page):
 @pytest.fixture
 def domestic_site(domestic_homepage):
     return SiteFactory(root_page=domestic_homepage)
+
+
+@pytest.fixture(autouse=True)
+def auth_backend():
+    patch = mock.patch(
+        'directory_sso_api_client.sso_api_client.user.get_session_user',
+        return_value=create_response(status_code=404)
+    )
+    yield patch.start()
+    patch.stop()
+
+
+@pytest.fixture
+def user():
+    return BusinessSSOUser(
+        id=1,
+        pk=1,
+        mobile_phone_number='55512345',
+        email='jim@example.com',
+        first_name='Jim',
+        last_name='Cross',
+        session_id='123',
+    )
+
+
+@pytest.fixture
+def client(client, auth_backend, settings):
+    def force_login(user):
+        client.cookies[settings.SSO_SESSION_COOKIE] = '123'
+        auth_backend.return_value = create_response({
+            'id': user.id,
+            'email': user.email,
+            'hashed_uuid': user.hashed_uuid,
+            'user_profile': {
+                'mobile_phone_number': user.mobile_phone_number,
+                'first_name': user.first_name,
+                'last_name': user.last_name
+            }
+        })
+    client.force_login = force_login
+    return client
