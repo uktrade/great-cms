fb3e580e30a18828d08ac51543f27cfe4fd67146
richtier <contact@richardtier.co.uk>
2020-02-17 14:37:50 +0000

    Rename havascript folder to reactComponents

--- reactComponents/webpack.config.js
+++ reactComponents/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
-        'ditMVP': './javascript/src/bundle.js',
+        'ditMVP': './reactComponents/src/bundle.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
