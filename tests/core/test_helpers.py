4ea5d60b22b912f5f33495491c5de15fd20296e6
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-02-18 10:23:07 +0000

    increase test cov

--- tests/core/test_helpers.py
+++ tests/core/test_helpers.py
@@ -128,6 +128,13 @@ def test_get_rules_and_regulations(mock_airtable_search):
     assert rules == {'Country': 'India', 'Export Duty': 1.5}
 
 
+def test_get_rules_and_regulations_empty(mock_airtable_search):
+    mock_airtable_search.return_value = []
+    rules = helpers.get_rules_and_regulations('India')
+    assert mock_airtable_search.call_args == mock.call('country', 'India')
+    assert rules is None
+
+
 @mock.patch.object(api_client.exportplan, 'exportplan_create')
 def test_create_export_plan(mock_exportplan_create):
     export_plan_data = {'Country': 'UK', 'Commodity code': 100, 'rules': {'rule1': '12343'}}
@@ -142,7 +149,7 @@ def test_create_export_plan(mock_exportplan_create):
 
 
 @mock.patch.object(api_client.exportplan, 'exportplan_list')
-def test_get_rules_regs(mock_get_exportplan):
+def test_get_exportplan_rules_regulations(mock_get_exportplan):
     data = [{'export_countries': ['UK'], 'export_commodity_codes': [100], 'rules_regulations': {'rule1': 'AAA'}}]
     mock_get_exportplan.return_value = create_response(data)
 
@@ -151,3 +158,15 @@ def test_get_rules_regs(mock_get_exportplan):
     assert mock_get_exportplan.call_count == 1
     assert mock_get_exportplan.call_args == mock.call(123)
     assert rules == {'rule1': 'AAA'}
+
+
+@mock.patch.object(api_client.exportplan, 'exportplan_list')
+def test_get_exportplan_rules_regulations_empty(mock_get_exportplan):
+    data = []
+    mock_get_exportplan.return_value = create_response(data)
+
+    rules = helpers.get_exportplan_rules_regulations(sso_session_id=123)
+
+    assert mock_get_exportplan.call_count == 1
+    assert mock_get_exportplan.call_args == mock.call(123)
+    assert rules is None
