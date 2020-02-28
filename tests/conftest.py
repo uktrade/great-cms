77a581621907dd422e150aa6b5e9e1c230d63d13
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-28 11:32:32 +0000

    add extra browser options
    increase screen size

--- tests/conftest.py
+++ tests/conftest.py
@@ -114,6 +114,11 @@ def mock_user_location_create():
 def browser():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--window-size=1600x2200")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
