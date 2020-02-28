b51093bcc7ae03860b091623c27f3140628f9bcc
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-28 13:51:01 +0000

    rm DB decorator which was causing 1 test to fail
    wagtail.core.models.Page.DoesNotExist: Page matching query does not exist.

--- tests/unit/core/test_mixins.py
+++ tests/unit/core/test_mixins.py
@@ -4,7 +4,6 @@ from domestic.models import DomesticHomePage
 from tests.unit.domestic import factories
 
 
-@pytest.mark.django_db
 def test_wagtail_admin_exclusive_page_can_only_be_created_once(root_page):
 
     assert DomesticHomePage.can_create_at(root_page)
