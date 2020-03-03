e450923a8481744969898ada1cbadecbc248a6fd
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-03-03 08:01:31 +0000

    add local time

--- core/helpers.py
+++ core/helpers.py
@@ -1,9 +1,12 @@
 from logging import getLogger
+from datetime import datetime
+import pytz
 
 from airtable import Airtable
 from directory_api_client import api_client
 from directory_sso_api_client import sso_api_client
 from ipware import get_client_ip
+from iso3166 import countries_by_alpha3
 
 from django.contrib.gis.geoip2 import GeoIP2, GeoIP2Exception
 from django.conf import settings
@@ -155,3 +158,21 @@ def get_dashboard_export_opportunities():
 
 def get_custom_duties_url(product_code, country):
     return f'{settings.MADB_URL}/summary?d={country}&pc={product_code}'
+
+
+def country_code_iso3_to_iso2(iso3_country_code):
+    if countries_by_alpha3.get(iso3_country_code):
+        return countries_by_alpha3['CHN'].alpha2
+
+
+def get_local_time(country_code):
+    local_time = {}
+    format = '%Y-%m-%d %H:%M %Z%z'
+    iso3_country_code = country_code_iso3_to_iso2(country_code)
+    if iso3_country_code and pytz.country_timezones('CN'):
+        local_time["timezone"] = pytz.country_timezones('CN')[0]
+        country_timezone = pytz.timezone(local_time["timezone"])
+        utc_dt = datetime.now(pytz.utc)
+        loc_dt = utc_dt.astimezone(country_timezone)
+        local_time["local_time"] = loc_dt.strftime(format)
+    return local_time
