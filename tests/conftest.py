e0c2307ab9de648d32f101728d3c24324df9d6cc
kowalcj0 <kowalczykjanusz@gmail.com>
2020-03-04 18:18:18 +0000

    add option to run browser tests in headfull mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+import environ
 import pytest
 from directory_api_client import api_client
 from selenium import webdriver
@@ -113,9 +114,12 @@ def mock_user_location_create():
 @pytest.fixture(scope='session')
 def browser():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--window-size=1600x2200')
-    options.add_argument('--disable-gpu')
+    env = environ.Env()
+    headless = env.bool('HEADLESS', True)
+    if headless:
+        options.add_argument('--headless')
+        options.add_argument('--window-size=1600x2200')
+        options.add_argument('--disable-gpu')
     options.add_argument('--start-maximized')
     options.add_argument('--disable-extensions')
     options.add_argument('--no-sandbox')
