8e7039a0df02a386827e2ef472181370ce6e1102
kowalcj0 <kowalczykjanusz@gmail.com>
2020-03-12 15:22:52 +0000

    check for header elements on about your business page

--- tests/browser/test_export_plan_about_your_plan.py
+++ tests/browser/test_export_plan_about_your_plan.py
@@ -3,7 +3,12 @@ from unittest import mock
 import pytest
 
 from core import helpers
-from tests.browser.common_selectors import ExportPlanAboutYourBusiness, StickyHeader
+from tests.browser.common_selectors import (
+    ExportPlanAboutYourBusiness,
+    HeaderCommon,
+    HeaderSignedIn,
+    StickyHeader,
+)
 from tests.browser.util import should_see_all_elements
 from tests.helpers import create_response
 
@@ -24,6 +29,7 @@ def test_export_plan_about_your_business(
     live_server, user, browser = server_user_browser_dashboard
 
     browser.get(live_server.url + "/export-plan/about-your-business/")
-
+    should_see_all_elements(browser, HeaderCommon)
+    should_see_all_elements(browser, HeaderSignedIn)
     should_see_all_elements(browser, StickyHeader)
     should_see_all_elements(browser, ExportPlanAboutYourBusiness)
