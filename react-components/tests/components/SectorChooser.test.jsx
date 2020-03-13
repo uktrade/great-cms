15e55897af6fb19442ae3524b963b8963b2a7852
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2020-03-13 17:23:28 +0000

    Add more tests

--- react-components/tests/components/SectorChooser.test.jsx
+++ react-components/tests/components/SectorChooser.test.jsx
@@ -15,7 +15,7 @@ afterEach(() => {
   container = null
 })
 
-it('shows the list of sectors on click', () => {
+it('shows the list of sectors and hides button on click', () => {
 
   const sectorList = ["Aerospace", "Advanced manufacturing", "Airports", "Agriculture, horticulture and fisheries"]
 
@@ -23,15 +23,81 @@ it('shows the list of sectors on click', () => {
     ReactDOM.render(<SectorChooser sectorList={sectorList} />, container)
   })
 
-  const button = container.querySelector('button')
+  const button = container.querySelector('#sector-chooser-button')
 
   act(() => {
     Simulate.click(button)
   })
 
-  const sectors = container.querySelector('.sector-list').children
+  expect(container.querySelector('#sector-chooser-button')).toBeFalsy()
+
+  const sectors = container.querySelector('#sector-list').children
 
   expect(sectors.length).toEqual(4)
+
+})
+
+it('shows selected sectors in sector list and shows save button', () => {
+
+  const sectorList = ["Aerospace", "Advanced manufacturing", "Airports", "Agriculture, horticulture and fisheries"]
+
+  act(() => {
+    ReactDOM.render(<SectorChooser sectorList={sectorList} selectedSectors={[]} />, container)
+  })
+
+  const sectorButton = container.querySelector('#sector-chooser-button')
+
+  act(() => {
+    Simulate.click(sectorButton)
+  })
+
+  expect(container.querySelector('#sector-chooser .g-button')).toBeFalsy()
+
+  const aerospace = container.querySelector('#aerospace')
+  const manufacturing = container.querySelector('#advanced-manufacturing')
+
+  act(() => {
+    Simulate.click(aerospace)
+    Simulate.click(manufacturing)
+  })
+
+  const selected = container.querySelector('#sector-list').getElementsByClassName('selected')
+
+  expect(selected.length).toEqual(2)
+
+  expect(container.querySelector('#sector-chooser .g-button')).toBeTruthy()
+
+})
+
+it('can remove selected sectors in sector list and hide save button', () => {
+
+  const sectorList = ["Aerospace", "Advanced manufacturing", "Airports", "Agriculture, horticulture and fisheries"]
+  const selectedSectors = ["Aerospace"]
+
+  act(() => {
+    ReactDOM.render(<SectorChooser sectorList={sectorList} selectedSectors={selectedSectors} />, container)
+  })
+
+  const sectorButton = container.querySelector('#sector-chooser-button')
+
+  act(() => {
+    Simulate.click(sectorButton)
+  })
+
+  const selected = container.querySelector('#sector-list').getElementsByClassName('selected')
+
+  expect(selected.length).toEqual(1)
+  expect(selected[0].id).toEqual('aerospace')
+  expect(container.querySelector('#sector-chooser .g-button')).toBeTruthy()
+
+  act(() => {
+    Simulate.click(selected[0])
+  })
+
+  expect(selected.length).toEqual(0)
+
+  expect(container.querySelector('#sector-chooser .g-button')).toBeFalsy()
+
 })
 
 it('renders the sector chooser button with tooltip', () => {
@@ -53,4 +119,44 @@ it('renders the sector chooser button with tooltip', () => {
   })
 
   expect(tooltip.className).not.toEqual(expect.stringContaining('hidden'))
+})
+
+it('shows selected sectors', () => {
+
+  const sectorList = ["Aerospace", "Advanced manufacturing", "Airports", "Agriculture, horticulture and fisheries"]
+  const selectedSectors = ["Aerospace", "Advanced manufacturing"]
+
+  act(() => {
+    ReactDOM.render(<SectorChooser sectorList={sectorList} selectedSectors={selectedSectors} />, container)
+  })
+
+  const selectedButtons = container.querySelector('#selected-sectors').getElementsByClassName('selected')
+
+  expect(selectedButtons.length).toEqual(2)
+  expect(selectedButtons[0].id).toEqual('aerospace')
+  expect(selectedButtons[1].id).toEqual('advanced-manufacturing')
+
+})
+
+it('removes sector on click', () => {
+
+  const sectorList = ["Aerospace", "Advanced manufacturing", "Airports", "Agriculture, horticulture and fisheries"]
+  const selectedSectors = ["Aerospace", "Advanced manufacturing"]
+
+  act(() => {
+    ReactDOM.render(<SectorChooser sectorList={sectorList} selectedSectors={selectedSectors} />, container)
+  })
+
+  const selectedButtons = container.getElementsByClassName('selected')
+
+  expect(selectedButtons.length).toEqual(2)
+
+  const aerospace = container.querySelector('#aerospace')
+
+  act(() => {
+    Simulate.click(aerospace)
+  })
+
+  expect(selectedButtons.length).toEqual(1)
+
 })
