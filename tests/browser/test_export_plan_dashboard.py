7f253e6882075cd46d2e5f5b42c164e5bf11913f
kowalcj0 <kowalczykjanusz@gmail.com>
2020-03-19 19:13:11 +0000

    add UI test for dashboard page tour

--- tests/browser/test_export_plan_dashboard.py
+++ tests/browser/test_export_plan_dashboard.py
@@ -5,21 +5,39 @@ import pytest
 from core import helpers
 from tests.browser.common_selectors import (
     ExportPlanDashboard,
+    ExportPlanDashboardPageTourStep0,
+    ExportPlanDashboardPageTourStep1,
+    ExportPlanDashboardPageTourStep2,
+    ExportPlanDashboardPageTourStep3,
+    ExportPlanDashboardPageTourStep4,
+    ExportPlanDashboardPageTourStep5,
     HeaderCommon,
     HeaderSignedIn,
     StickyHeader,
 )
-from tests.browser.util import should_not_see_errors, should_see_all_elements
+from tests.browser.util import (
+    find_element,
+    should_not_see_errors,
+    should_see_all_elements,
+    try_alternative_click_on_exception,
+    should_not_see_element
+)
 from tests.helpers import create_response
 
 pytestmark = pytest.mark.browser
+
+
+def click_next(browser, step):
+    next_button = find_element(browser, step.NEXT)
+    with try_alternative_click_on_exception(browser, next_button):
+        next_button.click()
 
 
 @pytest.mark.django_db
 @mock.patch.object(helpers, 'get_dashboard_export_opportunities')
 @mock.patch.object(helpers, 'get_dashboard_events')
 @mock.patch.object(helpers, 'create_company_profile')
-def test_export_plan_dashboard(
+def test_export_plan_dashboard_without_page_tour(
     mock_create_company_profile, mock_get_dashboard_events,
     mock_get_dashboard_export_opportunities, server_user_browser_dashboard,
 ):
@@ -35,3 +53,41 @@ def test_export_plan_dashboard(
     should_see_all_elements(browser, HeaderSignedIn)
     should_see_all_elements(browser, StickyHeader)
     should_see_all_elements(browser, ExportPlanDashboard)
+
+
+@pytest.mark.django_db
+@mock.patch.object(helpers, 'get_dashboard_export_opportunities')
+@mock.patch.object(helpers, 'get_dashboard_events')
+@mock.patch.object(helpers, 'create_company_profile')
+def test_export_plan_dashboard_with_page_tour(
+    mock_create_company_profile, mock_get_dashboard_events,
+    mock_get_dashboard_export_opportunities, server_user_browser_dashboard,
+    mock_export_plan_dashboard_page_tours,
+):
+    mock_create_company_profile.return_value = create_response()
+    mock_get_dashboard_events.return_value = []
+    mock_get_dashboard_export_opportunities.return_value = []
+    live_server, user, browser = server_user_browser_dashboard
+    should_not_see_errors(browser)
+
+    browser.get(live_server.url + '/export-plan/dashboard/')
+
+    should_see_all_elements(browser, ExportPlanDashboardPageTourStep0)
+
+    steps = [
+        ExportPlanDashboardPageTourStep0,
+        ExportPlanDashboardPageTourStep1,
+        ExportPlanDashboardPageTourStep2,
+        ExportPlanDashboardPageTourStep3,
+        ExportPlanDashboardPageTourStep4,
+        ExportPlanDashboardPageTourStep5,
+    ]
+
+    # click next for every step except the last one
+    for idx, step in enumerate(steps[:-1]):
+        click_next(browser, step)
+        should_see_all_elements(browser, steps[idx+1])
+
+    # click Start Now on the last tour step
+    click_next(browser, ExportPlanDashboardPageTourStep5)
+    should_not_see_element(browser, ExportPlanDashboardPageTourStep5.STEP)
