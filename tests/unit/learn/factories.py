34ba106bc3e6047de96be291cd4055e4c210d0b7
richtier <contact@richardtier.co.uk>
2020-03-24 09:08:21 +0000

    Track lesson read progress

---
+++ tests/unit/learn/factories.py
@@ -0,0 +1,27 @@
+import factory.fuzzy
+import wagtail_factories
+
+from learn import models
+
+
+class TopicPageFactory(wagtail_factories.PageFactory):
+
+    title = 'some topic'
+    live = True
+    slug = 'some-topic'
+    description = factory.fuzzy.FuzzyText(length=200)
+
+    class Meta:
+        model = models.TopicPage
+        django_get_or_create = ['slug', 'parent']
+
+
+class LessonPageFactory(wagtail_factories.PageFactory):
+
+    title = 'some lesson'
+    live = True
+    slug = 'some-lesson'
+
+    class Meta:
+        model = models.LessonPage
+        django_get_or_create = ['slug', 'parent']
