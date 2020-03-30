ab7aa4187394d9de674ba0a8da1ced89774d1afb
richtier <contact@richardtier.co.uk>
2020-03-30 14:47:13 +0100

    Fix tests

--- tests/unit/learn/test_views.py
+++ tests/unit/learn/test_views.py
@@ -4,6 +4,6 @@ from tests.unit.learn.factories import LearnPageFactory
 
 @pytest.mark.django_db
 def test_learn_page(client, domestic_site):
-    page = LearnPageFactory(parent=domestic_site, slug='learn')
+    page = LearnPageFactory(parent=domestic_site.root_page, slug='learn')
     response = client.get(page.url)
     assert response.status_code == 200
