04c62a500669d48fcfa2653e47f713a86fe72715
richtier <contact@richardtier.co.uk>
2020-04-07 14:03:29 +0100

    Fix errors

--- exportplan/context.py
+++ exportplan/context.py
@@ -2,13 +2,12 @@ from core.context import AbstractPageContextProvider
 from exportplan import data
 
 
-# for ExportPlanDashboardPage
 class ExportPlanDashboardPageContextProvider(AbstractPageContextProvider):
 
     template_name = 'exportplan/detail.html'
 
     @staticmethod
-    def get_context(page, request):
+    def get_context_data(request, page):
         return {
             'sections': data.SECTION_TITLES
         }
--- learn/context.py
+++ learn/context.py
@@ -1,26 +1,26 @@
-# from core.context import AbstractPageContextProvider
-#
-# from learn import models
-#
-#
-# class TopicPageContextProvider(AbstractPageContextProvider):
-#
-#     template_name = 'learn/detail.html'
-#
-#     @staticmethod
-#     def get_context(request):
-#         queryset = models.LessonViewHit.objects.filter(topic=self, sso_id=request.user.pk)
-#         return {
-#             'is_read_collection': queryset.values_list('lesson__pk', flat=True)
-#         }
-#
-#
-# class LessonPageContextProvider(AbstractPageContextProvider):
-#
-#     template_name = 'learn/detail.html'
-#
-#     @staticmethod
-#     def get_context(request):
-#         return {
-#             'is_read': read_hits.filter(sso_id=request.user.pk).exists()
-#         }
+from core.context import AbstractPageContextProvider
+
+from learn import models
+
+
+class TopicPageContextProvider(AbstractPageContextProvider):
+
+    template_name = 'learn/detail.html'
+
+    @staticmethod
+    def get_context_data(request, page):
+        queryset = models.LessonViewHit.objects.filter(topic=page, sso_id=request.user.pk)
+        return {
+            'is_read_collection': queryset.values_list('lesson__pk', flat=True)
+        }
+
+
+class LessonPageContextProvider(AbstractPageContextProvider):
+
+    template_name = 'learn/detail.html'
+
+    @staticmethod
+    def get_context_data(request, page):
+        return {
+            'is_read': page.read_hits.filter(sso_id=request.user.pk).exists()
+        }
