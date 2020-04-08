555fab7cc8e1ba89d5881e15a897640f234e25d6
richtier <contact@richardtier.co.uk>
2020-04-08 09:51:34 +0100

    Remove old models

--- exportplan/context.py
+++ exportplan/context.py
@@ -4,7 +4,7 @@ from exportplan import data
 
 class ExportPlanDashboardPageContextProvider(AbstractPageContextProvider):
 
-    template_name = 'exportplan/detail.html'
+    template_name = 'exportplan/export_plan_dashboard_page.html'
 
     @staticmethod
     def get_context_data(request, page):
