31f355b0f723775cf6d3e63ad0751f602a41e440
richtier <contact@richardtier.co.uk>
2020-04-24 18:02:24 +0100

    Add tests for export plan

--- react-components/tests/views/SignupModal/Container.test.jsx
+++ react-components/tests/views/SignupModal/Container.test.jsx
@@ -4,21 +4,22 @@ import { shallow } from 'enzyme'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import { Base } from '@src/views/SignupModal/Base'
+import { Container } from '@src/views/SignupModal/Container'
 import ModalCentreScreen from '@src/views/SignupModal/ModalCentreScreen'
 import ModalHalfScreen from '@src/views/SignupModal/ModalHalfScreen'
+
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('SignupModal', () => {
   test('serves "half" display mode', () => {
-    const component = shallow(<Base mode="half" />)
+    const component = shallow(<Container mode="half" />)
 
     expect(component.containsMatchingElement(<ModalHalfScreen />)).toBe(true)
   })
 
   test('serves "centre" display mode', () => {
-    const component = shallow(<Base mode="centre" />)
+    const component = shallow(<Container mode="centre" />)
 
     expect(component.containsMatchingElement(<ModalCentreScreen />)).toBe(true)
   })
--- tests/unit/exportplan/test_views.py
+++ tests/unit/exportplan/test_views.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from datetime import datetime
 from unittest import mock
 import json
@@ -68,3 +69,26 @@ def test_exportplan_target_markets(
     assert response.context['target_markets'] == json.dumps(explan_plan_data['target_markets'])
     assert response.context['selected_sectors'] == json.dumps(explan_plan_data['sectors'])
     assert response.context['datenow'] == datetime.now()
+
+
+@pytest.mark.django_db
+@mock.patch.object(helpers, 'update_exportplan')
+@mock.patch.object(helpers, 'get_exportplan')
+def test_update_export_plan_api_view(mock_get_export_plan, mock_update_exportplan, client, user):
+    client.force_login(user)
+    mock_get_export_plan.return_value = {'pk': 1, 'target_markets': []}
+    mock_update_exportplan.return_value = {'target_markets': [{'country': 'UK'}]}
+
+    url = reverse('exportplan:api-update-export-plan')
+    response = client.post(url, {'target_markets': ['China', 'India']})
+
+    assert mock_get_export_plan.call_count == 1
+    assert mock_get_export_plan.call_args == mock.call('123')
+    assert response.status_code == 200
+
+    assert mock_update_exportplan.call_count == 1
+    assert mock_update_exportplan.call_args == mock.call(
+        data=OrderedDict([('target_markets', [{'country': 'China'}, {'country': 'India'}])]),
+        id=1,
+        sso_session_id='123'
+    )
