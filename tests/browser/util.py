5f69d15c910853136672c236c08bf2c9ffb64ec6
kowalcj0 <kowalczykjanusz@gmail.com>
2020-05-01 17:05:20 +0100

    fix type hint

--- tests/browser/util.py
+++ tests/browser/util.py
@@ -81,7 +81,7 @@ def find_element(browser: WebDriver, selector: Union[Selector, EnumMeta]) -> Web
     return browser.find_element(selector.by, selector.selector)
 
 
-def find_elements(browser: WebDriver, selector: Selector) -> List[WebElement]:
+def find_elements(browser: WebDriver, selector: SelectorsEnum) -> List[WebElement]:
     return browser.find_elements(selector.by, selector.selector)
 
 
