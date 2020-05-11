843f1d5887cdb109fcf6f090e5f174b2bd37a187
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-05-11 13:00:16 +0100

    fix pep8

--- exportplan/forms.py
+++ exportplan/forms.py
@@ -1,6 +1,5 @@
 from great_components import forms
 from django.forms import ImageField
-from django.conf import settings
 
 import directory_validators.url
 import directory_validators.string
@@ -11,9 +10,7 @@ class LogoForm(forms.Form):
     logo = ImageField(
         help_text=(
             'For best results this should be a transparent PNG file of 600 x '
-            '600 pixels and no more than 2MB'.format(
-                int(settings.VALIDATOR_MAX_LOGO_SIZE_BYTES / 1024 / 1014)
-            ),
+            '600 pixels and no more than 2MB',
         ),
         required=True,
         validators=[directory_validators.file.logo_filesize, directory_validators.file.image_format]
