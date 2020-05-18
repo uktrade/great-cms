f8da6ac48aaad4c9a302f2438affcc5971bfddbc
WillTaylor22 <wrftaylor@gmail.com>
2020-05-18 14:26:24 +0100

    Increase flexibility of ExOps API

--- core/helpers.py
+++ core/helpers.py
@@ -73,9 +73,8 @@ def get_dashboard_events(sso_session_id):
 
 
 def get_dashboard_export_opportunities(sso_session_id, company):
-    sectors = company.expertise_industries_labels if company else []
-    search_term = ' '.join(sectors)
-    results = api_client.personalisation.export_opportunities_by_relevance_list(sso_session_id, search_term)
+    query_params = {}
+    results = api_client.personalisation.export_opportunities_by_relevance_list(sso_session_id, query_params)
     if (results.status_code == 200):
         return parse_opportunities(results.json()['results'])
     return []
