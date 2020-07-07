c8638c8d6e8d016302fd88a41d5fa68b26cb69e6
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2020-07-07 17:17:16 +0100

    Add more tests

--- react-components/tests/components/Objective.test.jsx
+++ react-components/tests/components/Objective.test.jsx
@@ -7,8 +7,12 @@ import fetchMock from 'fetch-mock'
 import Objective from '@src/components/Objective'
 import Field from '@src/components/Field'
 import Services from '@src/Services'
+import Spinner from '@src/components/Spinner/Spinner'
+import ErrorList from '@src/components/ErrorList'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+const dummyFunction = () => {}
 
 
 beforeEach(() => {
@@ -30,8 +34,6 @@ afterEach(() => {
 describe('Objective', () => {
   test('should generate objective form fields from props and prepopulate', () => {
 
-    const dummyFunction = () => {}
-
     const dummyObjective = {
       description: 'Some text',
       owner: 'Jane Doe',
@@ -40,9 +42,6 @@ describe('Objective', () => {
       end_date: '',
       companyexportplan: 1,
       pk: 1,
-      showSavedMessage: false,
-      isLoading: false,
-      errors: []
     }
 
     const wrapper = Enzyme.mount(
@@ -126,6 +125,214 @@ describe('Objective', () => {
     expect(wrapper.containsMatchingElement(ownerField)).toEqual(true)
     expect(wrapper.containsMatchingElement(startDateField)).toEqual(true)
     expect(wrapper.containsMatchingElement(endDateField)).toEqual(true)
+  })
+
+  test('should show saved message when objective has been saved', () => {
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      showSavedMessage: true,
+      isLoading: false,
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={dummyFunction}
+        deleteObjective={dummyFunction}/>
+    )
+
+    expect(wrapper.containsMatchingElement(
+      <p id="objective-saved-message">Changes saved.</p>
+    )).toEqual(true)
+
+  })
+
+  test('should show spinner when objective loading', () => {
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      showSavedMessage: false,
+      isLoading: true,
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={dummyFunction}
+        deleteObjective={dummyFunction}/>
+    )
+
+    expect(wrapper.containsMatchingElement(
+      <Spinner text="Saving..."/>
+    )).toEqual(true)
+
+  })
+
+  test('should show ErrorList when there are errors', () => {
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      errors: {__all__: ['Unexpected error', 'Request timed out']}
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={dummyFunction}
+        deleteObjective={dummyFunction}/>
+    )
+
+    expect(wrapper.containsMatchingElement(
+      <ErrorList errors={['Unexpected error', 'Request timed out']} />
+    )).toEqual(true)
+
+  })
+
+  test('should not show ErrorList when there are no errors', () => {
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      errors: {__all__: []}
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={dummyFunction}
+        deleteObjective={dummyFunction}/>
+    )
+
+    expect(wrapper.exists('errorlist')).toEqual(false)
+
+  })
+
+  test('should call function on change', () => {
+
+    const mockFunction = jest.fn()
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      showSavedMessage: false,
+      isLoading: false,
+      errors: {__all__: []}
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={mockFunction}
+        deleteObjective={dummyFunction}/>
+    )
+
+    const input = wrapper.find('textarea[name="description"]')
+
+    const dummyEvent = {
+      target: {name: 'description', value: 'Lorem ipsum'}
+    }
+
+    input.simulate('change', dummyEvent)
+
+    expect(mockFunction).toHaveBeenCalled()
+
+  })
+
+  test('should call function on delete', () => {
+
+    const mockFunction = jest.fn()
+
+    const dummyObjective = {
+      description: '',
+      owner: '',
+      planned_reviews: '',
+      start_date: '',
+      end_date: '',
+      companyexportplan: 1,
+      pk: 1,
+      showSavedMessage: false,
+      isLoading: false,
+      errors: {__all__: []}
+    }
+
+    const wrapper = Enzyme.mount(
+      <Objective
+        key={0}
+        id={0}
+        isLoading={dummyObjective.isLoading}
+        errors={dummyObjective.errors}
+        showSavedMessage={dummyObjective.showSavedMessage}
+        data={dummyObjective}
+        number={1}
+        handleChange={dummyFunction}
+        deleteObjective={mockFunction}/>
+    )
+
+    const input = wrapper.find('.button--delete')
+
+    input.simulate('click', {})
+
+    expect(mockFunction).toHaveBeenCalled()
+
   })
 
 
