1a6a071e8d8baede3c5004c55611fffc3d479241
Bob Meredith <bob.meredith@ntlworld.com>
2020-08-06 09:31:24 +0100

    Fix tests

--- tests/unit/domestic/test_views.py
+++ tests/unit/domestic/test_views.py
@@ -15,4 +15,4 @@ def test_landing_page_logged_in(client, user, domestic_site):
     client.force_login(user)
     response = client.get('/')
     assert response.status_code == 302
-    assert response.url == constants.DASHBOARD_
+    assert response.url == constants.DASHBOARD_URL
