f5bf777b1ae2b3d0919e51ac2f2542665496f7ff
alexandros giamas <agiamas@gmail.com>
2020-08-10 17:08:36 +0300

    more tests

--- tests/unit/core/test_fern.py
+++ tests/unit/core/test_fern.py
@@ -4,8 +4,22 @@ from core.fern import Fern
 
 
 @pytest.mark.django_db
-def test_fern():
+def test_valid_fern():
     text = 'Cras aliquam neque consectetur'
     enc = Fern().encrypt(text)
     dec = Fern().decrypt(enc)
     assert text, dec
+
+
+@pytest.mark.django_db
+def test_custom_key_fern(key='alpal'):
+    text = 'Cras aliquam neque consectetur'
+    enc = Fern().encrypt(text)
+    assert enc is not None
+
+
+@pytest.mark.django_db
+def test_invalid_enc_fern(key='alpal'):
+    text = 'Cras aliquam neque consectetur'
+    enc = Fern().decrypt(text)
+    assert enc == ''
