d63ec92151f9715278816366f08bb963055eed61
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-09-15 13:40:51 +0100

    remove unused code

--- exportplan/helpers.py
+++ exportplan/helpers.py
@@ -11,13 +11,6 @@ def create_export_plan(sso_session_id, exportplan_data):
     return response.json()
 
 
-def get_exportplan_rules_regulations(sso_session_id):
-    response = api_client.exportplan.exportplan_list(sso_session_id)
-    response.raise_for_status()
-    if response.json():
-        return response.json()[0]['rules_regulations']
-
-
 def get_exportplan(sso_session_id):
     response = api_client.exportplan.exportplan_list(sso_session_id)
     response.raise_for_status()
