9b2585448cc0b84787f8ada1c4b521e15d49924d
Lewis Coulson <lewis@digital.trade.gov.uk>
2020-10-06 08:37:38 +0100

    Add unit tests

--- react-components/tests/components/CaseStudy.test.jsx
+++ react-components/tests/components/CaseStudy.test.jsx
@@ -3,16 +3,18 @@ import { render, waitFor, fireEvent } from '@testing-library/react'
 
 import { CaseStudy } from '../../src/components/CaseStudy/CaseStudy'
 
+const openButtonText = 'Open case study'
+const closeButtonText = 'Close'
+const bodyText = 'example body content'
+
 const props = {
-  content: {
-    heading: 'heading example',
-    company: 'example company',
-    body: '<div></div>'
-  }
+  heading: 'heading example',
+  company: 'example company',
+  body: `<div>${bodyText}</div>`
 }
 
-const setup = ({ content }) => {
-  const utils = render(<CaseStudy content={content} />)
+const setup = (props) => {
+  const utils = render(<CaseStudy content={props} />)
 
   return {
     ...utils
@@ -22,11 +24,31 @@ const setup = ({ content }) => {
 describe('CaseStudy', () => {
   it('Should have a heading', () => {
     const { getByText } = setup(props)
-    expect(getByText(props.content.heading)).toBeInTheDocument()
+    expect(getByText(props.heading)).toBeInTheDocument()
   })
 
   it('Should have an open case study button', () => {
     const { getByText } = setup(props)
-    expect(getByText('Open case study')).toBeInTheDocument()
+    expect(getByText(openButtonText)).toBeInTheDocument()
+  })
+
+  it('Should toggle body content when buttons clicked', async () => {
+    const { getByText, queryByText } = setup(props)
+
+    fireEvent.click(getByText(openButtonText))
+
+    await waitFor(() => {
+      expect(queryByText(openButtonText)).not.toBeInTheDocument()
+      expect(getByText(props.company)).toBeInTheDocument()
+      expect(getByText(bodyText)).toBeInTheDocument()
+    })
+
+    fireEvent.click(getByText(closeButtonText))
+
+    await waitFor(() => {
+      expect(getByText(openButtonText)).toBeInTheDocument()
+      expect(queryByText(props.company)).not.toBeInTheDocument()
+      expect(queryByText(bodyText)).not.toBeInTheDocument()
+    })
   })
 })
