f122ac26b33200c6304e9e12d12f56a93cfb1fb7
Steve Jalim <steve@somefantastic.co.uk>
2020-10-13 11:49:04 +0100

    GP2-755: Fix readtime tests
    (Tripped up by a markup glitch, so if we get readtimes that are massively lower than before, we know something is amiss)

--- tests/unit/core/test_wagtail_hooks.py
+++ tests/unit/core/test_wagtail_hooks.py
@@ -233,7 +233,7 @@ def test_estimated_read_time_calculation(rf, domestic_homepage):
     revision = detail_page.save_revision()
     revision.publish()
 
-    expected_duration = timedelta(seconds=151)
+    expected_duration = timedelta(seconds=152)
 
     detail_page.refresh_from_db()
     assert detail_page.estimated_read_duration != expected_duration
@@ -284,7 +284,7 @@ def test_estimated_read_time_calculation__checks_text_and_video(rf, domestic_hom
     revision = detail_page.save_revision()
     revision.publish()
 
-    expected_duration = timedelta(seconds=154 + 123)  # reading + watching
+    expected_duration = timedelta(seconds=153 + 123)  # reading + watching
 
     detail_page.refresh_from_db()
     assert detail_page.estimated_read_duration != expected_duration
@@ -331,7 +331,7 @@ def test_estimated_read_time_calculation__checks_video(rf, domestic_homepage):
     revision = detail_page.save_revision()
     revision.publish()
 
-    expected_duration = timedelta(seconds=3 + 123)  # reading + watching
+    expected_duration = timedelta(seconds=4 + 123)  # reading + watching
 
     detail_page.refresh_from_db()
     assert detail_page.estimated_read_duration != expected_duration
@@ -419,7 +419,7 @@ def test_estimated_read_time_calculation__updates_only_draft_if_appropriate(
     # NOTE: for a reason unrelated to the point of _this_ test, the readtime
     # of the published page CAN BE calculated as slightly longer than the draft.
     # This may be in part due to the page having a very small amount of content.
-    assert detail_page.estimated_read_duration == timedelta(seconds=2)
+    assert detail_page.estimated_read_duration == timedelta(seconds=3)
 
 
 @pytest.mark.django_db
