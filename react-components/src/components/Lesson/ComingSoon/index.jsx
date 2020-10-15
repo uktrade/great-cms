0deda7f57fe7b443b778519ace811c58ecddf2a0
Lewis Coulson <lewis@digital.trade.gov.uk>
2020-10-15 15:31:08 +0100

    Change to use React based implementation

--- react-components/src/components/Lesson/ComingSoon/index.jsx
+++ react-components/src/components/Lesson/ComingSoon/index.jsx
@@ -1,46 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import ReactModal from 'react-modal'
 
 import { Modal } from '@src/components/Modal/index'
 
-export const ComingSoon = ({ isOpen, setIsOpen }) => {
+export const ComingSoon = ({ title, lessons = [] }) => {
+  const [modal, setModal] = useState(false)
   return (
-    <ReactModal
-      isOpen={isOpen}
-      className="ReactModal__Content ReactModalCentreScreen"
-      overlayClassName="ReactModal__Overlay ReactModalCentreScreen"
-      contentLabel="Modal"
-    >
-      <Modal
-        backUrl="/export-plan/dashboard/"
-        header="This Lesson is coming soon"
-        content="This feature is not available in Beta version of the new great.gov.uk platform."
-        onClick={setIsOpen}
-        buttonText="Ok"
-      />
-    </ReactModal>
+    <>
+      <ReactModal
+        isOpen={modal}
+        className="ReactModal__Content ReactModalCentreScreen"
+        overlayClassName="ReactModal__Overlay ReactModalCentreScreen"
+        contentLabel="Modal"
+      >
+        <Modal
+          backUrl="/export-plan/dashboard/"
+          header="This Lesson is coming soon"
+          content="This feature is not available in Beta version of the new great.gov.uk platform."
+          onClick={() => setModal(false)}
+          buttonText="Ok"
+        />
+      </ReactModal>
+      <div className="learn__topic-item-details c-1-3-l">
+        <h2 className="learn__topic-item-title h-m p-t-0">{title}</h2>
+      </div>
+      <ul className="learn__lessons-list c-2-3-l">
+        {lessons.map((lesson) => (
+          <li className={`learn__lesson-item ${lesson.isPlaceholder ? 'learn__lesson-item--placeholder' : ''}`}>
+            <a
+              href={lesson.url}
+              className="learn__lesson-item-link h-xs"
+              onClick={(e) => {
+                if (lesson.isPlaceholder) {
+                  e.preventDefault()
+                  setModal(true)
+                }
+              }}
+            >
+              <span>{lesson.title}</span>
+              {lesson.isPlaceholder && <button className="button button--secondary button--small">Coming soon</button>}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
-function renderComingSoon(element, isOpen, toggle) {
-  ReactDOM.render(<ComingSoon isOpen={isOpen} setIsOpen={toggle} />, element)
-}
-
-function createComingSoonModal(element) {
-  let isOpen = false
-
-  const toggle = () => (isOpen = !isOpen)
-
-  return {
-    render: () => {
-      toggle()
-      renderComingSoon(element, isOpen, () => {
-        toggle()
-        renderComingSoon(element, isOpen, toggle)
-      })
-    }
-  }
+function createComingSoonModal({ element, title, lessons }) {
+  ReactDOM.render(<ComingSoon title={title} lessons={lessons} />, element)
 }
 
 export { createComingSoonModal }
