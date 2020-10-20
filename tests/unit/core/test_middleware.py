d2d29a92aaa34ab3aca862e32e3316860e46a96d
Steve Jalim <steve@somefantastic.co.uk>
2020-10-20 15:40:07 +0100

    GP2-726: Redirect unauthenticated users on CMS pages to the sign-in page
    This changeset adds`mixins.AuthenticatedUserRequired` to the base page used for most CMS pages (`CMSGenericPage`) so that, if the user is unauthenticated, they are redirected to the login page.
    
    This also has the (requested/desired) side effect that the existing behaviour upon logout (kill session and reload the same page) now causes a redirect when the page is reloaded. Ideally we'd skip the reload, but given this is is a temporary change, I think it's fine for now)
    
    Note that when a user is redirected to signin, we don't remember where they wanted to go originally. This is not ideal, but tolerable within the AC for this piece.

--- tests/unit/core/test_middleware.py
+++ tests/unit/core/test_middleware.py
@@ -43,13 +43,15 @@ def test_stores_user_location_anon_user(mock_store_user_location, rf):
 
 
 @pytest.mark.django_db
-def test_user_specific_redirect_middleware(domestic_site, client):
+def test_user_specific_redirect_middleware(domestic_site, client, user, patch_export_plan):
     learn_page = factories.LandingPageFactory(parent=domestic_site.root_page, slug='learn')
     introduction_page = factories.ListPageFactory(
         parent=learn_page, slug='introduction', template='learn/automated_list_page.html'
     )
     categories_page = factories.CuratedListPageFactory(parent=learn_page, slug='categories')
     # Given the user has gone to /learn/introduction/
+
+    client.force_login(user)  # because unauthed users get redirected
     response = client.get(introduction_page.url)
 
     assert response.status_code == 200
