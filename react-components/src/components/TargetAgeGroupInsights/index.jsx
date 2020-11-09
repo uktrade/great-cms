724f149f6a6d8c8360a0a8ecc010762b59c89ccb
Gerald Isabelle <info@geraldisabelle.me>
2020-11-09 13:43:25 +0000

    GP2-619 - create toggle table component to functionality reuseble

--- react-components/src/components/TargetAgeGroupInsights/index.jsx
+++ react-components/src/components/TargetAgeGroupInsights/index.jsx
@@ -1,80 +1,20 @@
-import React, { memo, useState } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 
 import { Table } from './Table'
-import Services from '@src/Services'
-import { mapData } from './utils'
+import { ToggleDataTable } from '@src/components/ToggleDataTable'
 
 export const TargetAgeGroupInsights = memo(({
   country,
   groups
 }) => {
-
-  const [isOpen, setIsOPen] = useState(false)
-  const [selectedGroups, setSelectedGroups] = useState([])
-  const [data, setData] = useState({})
-  const targetGroupLabels = groups
-    .filter((group) => selectedGroups.includes(group.key))
-    .map((group) => group.label)
-
-  const showTable = Object.keys(data).length >= 1 && !isOpen
-
-  const submitForm = (event) => {
-    event.preventDefault()
-    setIsOPen(!isOpen)
-
-    Services.getMarketingCountryData({ country, target_age_groups: selectedGroups })
-      .then((d) =>
-        setData(mapData(d))
-      )
-      .catch((error) => console.log(error))
-  }
-
-  const handleChange = (event) => {
-    const { value } = event.target
-    const isAlreadySelected = selectedGroups.find((group) => group === value)
-    const updatedSelectedGroups = isAlreadySelected
-      ? selectedGroups.filter((group) => group !== value)
-      : [...selectedGroups, value]
-
-    setSelectedGroups(updatedSelectedGroups)
-  }
-
   return (
-    <div className="target-age-group-insights">
-      <h3 className="body-l-b">Select target age groups</h3>
-      <button className="button button--secondary button--icon m-t-xs m-r-xs" type='button' onClick={ () => setIsOPen(!isOpen) }>
-        <i className="fa fa-chevron-circle-down" />
-        <span>Open</span>
-      </button>
-      {targetGroupLabels.map(i => <span className='target-age-group-tag body-m-b bg-blue-deep-20' key={i}>{i}</span>)}
-
-      {isOpen && (
-        <form onSubmit={submitForm}>
-          <ul className="form-group m-b-0">
-            {groups.map(({ key, label }) => (
-              <li className="great-checkbox width-full m-b-xs" key={key}>
-                <input
-                  id={key}
-                  value={key}
-                  type="checkbox"
-                  onChange={handleChange}
-                  checked={selectedGroups.includes(key)}
-                />
-                <label htmlFor={key}>
-                  {label}
-                </label>
-              </li>
-            ))}
-          </ul>
-
-          <button className="button button--secondary m-t-s" type="submit">
-            Confirm
-          </button>
-        </form>
-      )}
-      {showTable && <Table {...data} />}
-    </div>
+    <ToggleDataTable
+      country={country}
+      groups={groups}
+    >
+      <Table/>
+    </ToggleDataTable>
   )
 })
 
