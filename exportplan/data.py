471662e30f9cf2075871aaf24a19923ac26cfd12
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-11-13 21:54:53 +0000

    modal and TM country content

--- exportplan/data.py
+++ exportplan/data.py
@@ -41,6 +41,18 @@ COUNTRY_REQUIRED = [
     slugify(TRAVEL_AND_BUSINESS_POLICIES),
 ]
 
+# This maintains a collection of which pages require a country to be selected
+PRODUCT_REQUIRED = [
+    slugify(TARGET_MARKETS_RESEARCH),
+    slugify(ADAPTATION_TARGET_MARKET),
+    slugify(MARKETING_APPROACH),
+    slugify(COSTS_AND_PRICING),
+    slugify(FINANCE),
+    slugify(PAYMENT_METHODS),
+    slugify(TRAVEL_AND_BUSINESS_POLICIES),
+    slugify(BUSINESS_RISK),
+]
+
 SECTIONS = OrderedDict({
     slugify(ABOUT_YOUR_BUSINESS): {
         'title': ABOUT_YOUR_BUSINESS,
