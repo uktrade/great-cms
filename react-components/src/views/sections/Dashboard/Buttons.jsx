66be1bbcf373645503d0780e216a14304b0f8519
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2020-11-13 10:54:34 +0000

    Fix bug for missing HTML tag which not picking section title

--- react-components/src/views/sections/Dashboard/Buttons.jsx
+++ react-components/src/views/sections/Dashboard/Buttons.jsx
@@ -21,20 +21,17 @@ export const Buttons = () => {
         onClick={() => setModal(false)}
         isOpen={modal}
       />
-      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal}
-              data-sectiontitle="Save your plan as a PDF">
-        <i className="fas fa-download" />
-        <span className="body-m text-white">Save your plan as a PDF</span>
+      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal} data-sectiontitle="Save your plan as a PDF">
+        <i className="fas fa-download" data-sectiontitle="Save your plan as a PDF" />
+        <span className="body-m text-white" data-sectiontitle="Save your plan as a PDF">Save your plan as a PDF</span>
       </button>
-      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal}
-              data-sectiontitle="Share your plan">
-        <i className="fas fa-share" />
-        <span className="body-m text-white">Share your plan</span>
+      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal} data-sectiontitle="Share your plan">
+        <i className="fas fa-share" data-sectiontitle="Share your plan" />
+        <span className="body-m text-white" data-sectiontitle="Share your plan">Share your plan</span>
       </button>
-      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal}
-              data-sectiontitle="Find your target market">
-        <i className="fas fa-globe" />
-        <span className="body-m text-white">Find your target market</span>
+      <button type='button' className="button button--secondary button--full-width button--icon m-b-xs button--disabled" onClick={openComingSoonModal} data-sectiontitle="Find your target market">
+        <i className="fas fa-globe" data-sectiontitle="Find your target market" />
+        <span className="body-m text-white" data-sectiontitle="Find your target market">Find your target market</span>
       </button>
     </>
   )
