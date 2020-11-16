9c5bca54deb45949d4998b5f878b1fb89135a94a
Jake Hopking <jake@hopking.io>
2020-11-16 16:39:29 +0000

    Add dit link to env var for use in templates as tag. Add context test.

--- tests/unit/core/test_context_processors.py
+++ tests/unit/core/test_context_processors.py
@@ -9,3 +9,11 @@ def test_support_email_exists_in_template_context(client):
     response = client.get(url)
     assert 'great_support_email' in response.context
     assert response.context['great_support_email'] == 'great.support@trade.gov.uk'
+
+
+@pytest.mark.django_db
+def test_dit_link_exists_in_template_context(client):
+    url = reverse('core:signup')
+    response = client.get(url)
+    assert 'dit_on_govuk' in response.context
+    assert response.context['dit_on_govuk'] == 'www.gov.uk/government/organisations/department-for-international-trade'
