18c3626b6f3984f9a4ba168e3cf2fddf49163b09
Jake Hopking <jake@hopking.io>
2020-11-19 11:33:09 +0000

    Change image to fa icon

--- react-components/src/components/EducationalMomentIcon/EducationalMomentIcon.jsx
+++ react-components/src/components/EducationalMomentIcon/EducationalMomentIcon.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import OpenBookIcon from '@assets/open-book-icon.png'
 
 export default function EducationalMomentIcon(props) {
   const { ariaDescribedBy, hiddenText } = props
 
   return (
-      <button type="button" className="educational-moment__button" aria-describedby={ariaDescribedBy}>
-          <span className="visually-hidden">{hiddenText}</span>
-          <img className="educational-moment__button-image" src={OpenBookIcon} alt="" />
-      </button>
+    <button type="button" className="educational-moment__button" aria-describedby={ariaDescribedBy}>
+      <span className="visually-hidden">{hiddenText}</span>
+      <i className="educational-moment__button-icon fas fa-book text-blue-deep-40" />
+    </button>
   )
-
 }
 
 EducationalMomentIcon.propTypes = {
