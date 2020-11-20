2ac90f1c0df72a5d8886ba4de27745aa97787faa
Steve Jalim <steve@somefantastic.co.uk>
2020-11-20 16:09:10 +0000

    GP2-901: Add a standalong view of a CaseStudy in the CMS
    * Custom button on the ModelAdmin list page for Case Study that opens that view.
    * View renders page which renders only the Case Study component for the selected Case Study - uses official CSS and JS from public site and the same template partial

---
+++ cms_extras/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.admin.views.decorators import staff_member_required
+
+from django.urls import path
+
+from cms_extras.views import case_study as case_study_view
+
+app_name = 'cms_extra'
+
+
+urlpatterns = [
+    path(
+        'case-study/<int:case_study_id>/',
+        staff_member_required(case_study_view),
+        name='case-study-view'
+    ),
+]
