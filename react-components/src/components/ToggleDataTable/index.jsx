4cce420a8e956ba2b977a635e1032f85e1c110ed
Jake Hopking <jake@hopking.io>
2020-11-20 10:40:05 +0000

    add eslint-disable import/prefer-default-export

--- react-components/src/components/ToggleDataTable/index.jsx
+++ react-components/src/components/ToggleDataTable/index.jsx
@@ -1,21 +1,15 @@
+/* eslint-disable import/prefer-default-export */
 import React, { memo, useState, cloneElement } from 'react'
 import PropTypes from 'prop-types'
 
 import Services from '@src/Services'
 import { mapData } from '@src/components/TargetAgeGroupInsights/utils'
 
-export const ToggleDataTable = memo(({
-  country,
-  groups,
-  children
-}) => {
-
+export const ToggleDataTable = memo(({ country, groups, children }) => {
   const [isOpen, setIsOPen] = useState(false)
   const [selectedGroups, setSelectedGroups] = useState([])
   const [data, setData] = useState({})
-  const targetGroupLabels = groups
-    .filter((group) => selectedGroups.includes(group.key))
-    .map((group) => group.label)
+  const targetGroupLabels = groups.filter((group) => selectedGroups.includes(group.key)).map((group) => group.label)
   const showTable = Object.keys(data).length >= 1 && !isOpen
 
   const submitForm = (event) => {
@@ -23,9 +17,7 @@ export const ToggleDataTable = memo(({
     setIsOPen(!isOpen)
 
     Services.getMarketingCountryData({ country, target_age_groups: selectedGroups })
-      .then((d) =>
-        setData(mapData(d))
-      )
+      .then((d) => setData(mapData(d)))
       .catch((error) => console.log(error))
   }
 
@@ -41,33 +33,39 @@ export const ToggleDataTable = memo(({
 
   return (
     <>
-      <h3 className='body-l-b'>Select target age groups</h3>
-      <button className='button button--secondary button--icon m-t-xs m-r-xs' type='button' onClick={() => setIsOPen(!isOpen)}>
-        <i className='fa fa-chevron-circle-down' />
+      <h3 className="body-l-b">Select target age groups</h3>
+      <button
+        className="button button--secondary button--icon m-t-xs m-r-xs"
+        type="button"
+        onClick={() => setIsOPen(!isOpen)}
+      >
+        <i className="fa fa-chevron-circle-down" />
         <span>Open</span>
       </button>
-      {targetGroupLabels.map(i => <span className='statistic-label body-m-b bg-blue-deep-20' key={i}>{i}</span>)}
+      {targetGroupLabels.map((i) => (
+        <span className="statistic-label body-m-b bg-blue-deep-20" key={i}>
+          {i}
+        </span>
+      ))}
 
       {isOpen && (
         <form onSubmit={submitForm}>
-          <ul className='form-group m-b-0'>
+          <ul className="form-group m-b-0">
             {groups.map(({ key, label }) => (
-              <li className='great-checkbox width-full m-b-xs' key={key}>
+              <li className="great-checkbox width-full m-b-xs" key={key}>
                 <input
                   id={key}
                   value={key}
-                  type='checkbox'
+                  type="checkbox"
                   onChange={handleChange}
                   checked={selectedGroups.includes(key)}
                 />
-                <label htmlFor={key}>
-                  {label}
-                </label>
+                <label htmlFor={key}>{label}</label>
               </li>
             ))}
           </ul>
 
-          <button className='button button--secondary m-t-s' type='submit'>
+          <button className="button button--secondary m-t-s" type="submit">
             Confirm
           </button>
         </form>
@@ -79,13 +77,15 @@ export const ToggleDataTable = memo(({
 
 ToggleDataTable.propTypes = {
   country: PropTypes.string.isRequired,
-  groups: PropTypes.arrayOf(PropTypes.shape({
-    key: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired
-  })),
-  children: PropTypes.element.isRequired
+  groups: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ),
+  children: PropTypes.element.isRequired,
 }
 
 ToggleDataTable.defaultProps = {
-  groups: []
+  groups: [],
 }
--- react-components/src/views/sections/AdaptationForYourTargetMarket/adaptToTargetMarketForm/index.jsx
+++ react-components/src/views/sections/AdaptationForYourTargetMarket/adaptToTargetMarketForm/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react'
 
 import { FormWithInputWithExample } from '@src/components/FormWithInputWithExample'
