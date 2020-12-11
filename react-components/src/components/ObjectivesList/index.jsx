3a3fe27ea659843b02bdd2b6a496d5f8899589bf
Gerald Isabelle <info@geraldisabelle.me>
2020-12-11 14:51:04 +0000

    GP2-1208 update objectives

--- react-components/src/components/ObjectivesList/index.jsx
+++ react-components/src/components/ObjectivesList/index.jsx
@@ -1,169 +1,86 @@
-import React, { Component } from 'react'
+import React, { memo, useState } from 'react'
 import PropTypes from 'prop-types'
-
-import { Subject } from 'rxjs'
-import { debounceTime, delay } from 'rxjs/operators'
 
 import ErrorList from '@src/components/ErrorList'
 import { Objective } from './Objective'
 import Services from '../../Services'
 import { analytics } from '@src/Helpers'
+import { useDebounce } from '@src/components/hooks/useDebounce'
 
-export class ObjectivesList extends Component {
-  constructor(props) {
-    super(props)
+export const ObjectivesList = memo(
+  ({ exportPlanID, objectives: initialObjectives }) => {
+    const [errors, setErrors] = useState({})
+    const [objectives, setObjectives] = useState(initialObjectives || [])
+    const [message, setMessage] = useState(false)
+    const debounceMessage = useDebounce(setMessage)
 
-    this.state = {
-      errors: {},
-      objectives: props.objectives || [],
+    const update = (data) => {
+      Services.updateObjective(data)
+        .then(() => {
+          setMessage(true)
+        })
+        .then(() => {
+          analytics({
+            event: 'planSectionSaved',
+            sectionTitle: 'Objectives',
+          })
+        })
+        .catch((err) => {
+          setErrors(err)
+        })
+        .finally(() => {
+          setErrors({})
+          debounceMessage(false)
+        })
     }
 
-    const { objectives } = this.state
+    const debounceUpdate = useDebounce(update)
 
-    objectives.forEach((objective) => {
-      objective.showSavedMessage = false
-    })
+    const createObjective = () => {
+      const date = new Date()
+      const today = `${date.getFullYear().toString()}-${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`
 
-    this.inputToSave$ = new Subject()
+      Services.createObjective({
+        description: '',
+        owner: '',
+        planned_reviews: '',
+        start_date: today,
+        end_date: today,
+        companyexportplan: exportPlanID,
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            setObjectives([...objectives, { ...data }])
+            setErrors({})
+          })
+        })
+        .catch((err) => {
+          setErrors({ err })
+        })
+    }
 
-    const saveInput$ = this.inputToSave$.pipe(debounceTime(1000 * 3))
+    const deleteObjective = (pk) => {
+      Services.deleteObjective(pk)
+        .then(() => {
+          const updatedObjectives = objectives.filter(
+            (objective) => objective.pk !== pk
+          )
+          setObjectives(updatedObjectives)
+        })
+        .catch((err) => {
+          setErrors({ err })
+        })
+    }
 
-    saveInput$.subscribe((data) => {
-      this.setState(
-        (state) => {
-          const updatedObjectives = [...state.objectives]
-          return { objectives: updatedObjectives }
-        },
-        () => {
-          Services.updateObjective(data.data)
-            .then(this.handleUpdateSuccess)
-            .then(() => {
-              analytics({
-                event: 'planSectionSaved',
-                sectionTitle: 'Objectives',
-              })
-            })
-            .catch((err) => this.handleError(err, data.data.pk))
-        }
+    const updateObjective = (data) => {
+      const updatedObjectives = objectives.map((item) =>
+        item.pk === data.pk ? { ...item, ...data } : item
       )
-    })
-
-    const afterSave$ = saveInput$.pipe(delay(1000 * 3))
-
-    afterSave$.subscribe((data) => {
-      this.setState((state) => {
-        const objectiveToUpdate = state.objectives.indexOf(
-          state.objectives.filter(
-            (objective) => objective.pk === data.data.pk
-          )[0]
-        )
-        const updatedObjectives = [...state.objectives]
-        updatedObjectives[objectiveToUpdate].showSavedMessage = false
-        return { objectives: updatedObjectives }
-      })
-    })
-  }
-
-  handleCreateSuccess = (response) => {
-    response.json().then((data) => {
-      this.setState((state) => {
-        const newObjective = { ...data }
-        newObjective.showSavedMessage = false
-        newObjective.errors = {}
-        const updatedObjectives = state.objectives.concat([newObjective])
-        return { objectives: updatedObjectives }
-      })
-    })
-  }
-
-  handleUpdateSuccess = (response) => {
-    response.json().then((data) => {
-      this.setState((state) => {
-        const objectiveToUpdate = state.objectives.indexOf(
-          state.objectives.filter((objective) => objective.pk === data.pk)[0]
-        )
-        const updatedObjectives = [...state.objectives]
-        updatedObjectives[objectiveToUpdate].showSavedMessage = true
-        updatedObjectives[objectiveToUpdate].errors = {}
-        return { objectives: updatedObjectives }
-      })
-    })
-  }
-
-  handleDeleteSuccess = (response, pk) => {
-    response.json().then(() => {
-      this.setState((state) => {
-        const updatedObjectives = state.objectives.filter(
-          (objective) => objective.pk !== pk
-        )
-        return { objectives: updatedObjectives, errors: {} }
-      })
-    })
-  }
-
-  handleError = (errors, pk) => {
-    this.setState((state) => {
-      const objectiveToUpdate = state.objectives.indexOf(
-        state.objectives.filter((objective) => objective.pk === pk)[0]
-      )
-      const updatedObjectives = [...state.objectives]
-      updatedObjectives[objectiveToUpdate].errors = errors
-      return { objectives: updatedObjectives }
-    })
-  }
-
-  createObjective = () => {
-    const { exportPlanID } = this.props
-    const date = new Date()
-    const today = `${date.getFullYear().toString()}-${(date.getMonth() + 1)
-      .toString()
-      .padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`
-
-    Services.createObjective({
-      description: '',
-      owner: '',
-      planned_reviews: '',
-      start_date: today,
-      end_date: today,
-      companyexportplan: exportPlanID,
-    })
-      .then(this.handleCreateSuccess)
-      .catch(this.handleCreateError)
-  }
-
-  handleCreateError = (errors) => {
-    this.setState({ errors })
-  }
-
-  deleteObjective = (pk) => {
-    this.setState(
-      (state) => {
-        const updatedObjectives = [...state.objectives]
-        return { objectives: updatedObjectives }
-      },
-      () => {
-        Services.deleteObjective(pk)
-          .then((res) => this.handleDeleteSuccess(res, pk))
-          .catch((err) => this.handleError(err, pk))
-      }
-    )
-  }
-
-  updateObjective = (data) => {
-    this.setState(
-      (state) => {
-        const updatedObjectives = [...state.objectives]
-        updatedObjectives[data.id] = data.data
-        return { objectives: updatedObjectives }
-      },
-      () => {
-        this.inputToSave$.next(data)
-      }
-    )
-  }
-
-  render() {
-    const { errors, objectives } = this.state
+      setObjectives(updatedObjectives)
+      debounceUpdate(data)
+    }
 
     return (
       <div className="form-table bg-white br-xs m-b-m">
@@ -173,18 +90,18 @@ export class ObjectivesList extends Component {
             id={i}
             isLoading={objective.isLoading}
             errors={objective.errors}
-            showSavedMessage={objective.showSavedMessage}
             data={objective}
             number={i + 1}
-            handleChange={this.updateObjective}
-            deleteObjective={this.deleteObjective}
+            handleChange={updateObjective}
+            deleteObjective={deleteObjective}
           />
         ))}
+        {message && <p id="objective-saved-message">Changes saved.</p>}
         {objectives.length !== 5 && (
           <button
             type="button"
             className="button button--add button--large button--icon"
-            onClick={this.createObjective}
+            onClick={createObjective}
           >
             <i className="fas fa-plus-circle" />
             Add goal {objectives.length + 1} of 5
@@ -194,7 +111,7 @@ export class ObjectivesList extends Component {
       </div>
     )
   }
-}
+)
 
 ObjectivesList.propTypes = {
   objectives: PropTypes.arrayOf(
