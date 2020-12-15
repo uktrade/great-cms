e1058ce6ef72ecf650fee19768111a1105e12dae
Gerald Isabelle <info@geraldisabelle.me>
2020-12-15 17:11:40 +0000

    GP2-1195 update content cost and pricing

--- react-components/src/components/CostsAndPricing/Costs/Cost/index.jsx
+++ react-components/src/components/CostsAndPricing/Costs/Cost/index.jsx
@@ -4,46 +4,50 @@ import PropTypes from 'prop-types'
 import { Input } from '@src/components/Form/Input'
 import EducationalMomentTooltip from '@src/components/EducationalMomentTooltip'
 
-export const Cost = memo(({
-  label,
-  id,
-  currency,
-  placeholder,
-  tooltip,
-  value,
-  update,
-  type
-}) => {
-  return (
-    <tr>
-      <td>
-        <label className='form-label p-b-xs' htmlFor={id}>{label}</label>
-        <EducationalMomentTooltip description={tooltip} id='name' type='LEFT' />
-      </td>
-      <td>
-        <Input
-          label={label}
-          id={id}
-          hideLabel
-          type={type}
-          prepend={currency}
-          value={value}
-          onChange={(field) => update(field)}
-          placeholder={placeholder}
-
-        />
-      </td>
-    </tr>
-  )
-})
+export const Cost = memo(
+  ({ label, id, currency, placeholder, tooltip, value, update, type }) => {
+    return (
+      <tr>
+        <td>
+          <label className="form-label p-b-xs" htmlFor={id}>
+            {label}
+          </label>
+          {tooltip && (
+            <EducationalMomentTooltip
+              description={tooltip}
+              id="name"
+              type="LEFT"
+            />
+          )}
+        </td>
+        <td>
+          <Input
+            label={label}
+            id={id}
+            hideLabel
+            type={type}
+            prepend={currency}
+            value={value}
+            onChange={(field) => update(field)}
+            placeholder={placeholder}
+          />
+        </td>
+      </tr>
+    )
+  }
+)
 
 Cost.propTypes = {
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   currency: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
-  tooltip: PropTypes.string.isRequired,
+  tooltip: PropTypes.string,
   value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   update: PropTypes.func.isRequired,
+}
+
+Cost.defaultProps = {
+  tooltip: '',
 }
