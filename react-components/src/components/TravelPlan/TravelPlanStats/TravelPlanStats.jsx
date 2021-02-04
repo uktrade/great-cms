0ea2d03093a05e61210f1661db28b8f626963a6e
Jake Hopking <jake@hopking.io>
2021-02-04 12:56:58 +0000

    Add travel plan stats component

---
+++ react-components/src/components/TravelPlan/TravelPlanStats/TravelPlanStats.jsx
@@ -0,0 +1,47 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+import { Tooltip } from '@components/tooltip/Tooltip'
+import { ToggleSnapshot } from '@src/components/ToggleSnapshot'
+import { Stats } from '@src/components/Stats'
+import { notAvailable } from '@src/components/Stats/StatsGroup'
+import { formatLanguages } from '@src/components/TargetAgeGroupInsights/utils'
+
+export const TravelPlanStats = memo(({ languages, tooltip }) => {
+  const formatedLanguages = formatLanguages(
+    languages.cia_factbook_data.languages.language
+  )
+
+  return (
+    <ToggleSnapshot isOpen={true}>
+      <div className="width-full">
+        <div className="grid">
+          <div className="c-full">
+            <Stats
+              header="The main languages in your chosen market are:"
+              data={formatedLanguages || notAvailable}
+              childPosition="bottom"
+            >
+              {tooltip && (
+                <Tooltip
+                  id="languages-in-target-market-tooltip"
+                  title={tooltip.heading}
+                  content={`<p>${tooltip.description}</p>`}
+                  className="m-t-xs"
+                />
+              )}
+            </Stats>
+          </div>
+        </div>
+      </div>
+    </ToggleSnapshot>
+  )
+})
+
+TravelPlanStats.propTypes = {
+  languages: PropTypes.object.isRequired,
+  tooltip: PropTypes.shape({
+    heading: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }),
+}
