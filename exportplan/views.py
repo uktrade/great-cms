de45a595261e26b32543f94c54b0c53326b06285
depsiatwal <depsi.a@gmail.com>
2021-02-11 10:46:07 +0000

    remove form

--- exportplan/views.py
+++ exportplan/views.py
@@ -141,15 +141,9 @@ class ExportPlanSectionView(GA360Mixin, ExportPlanMixin, TemplateView):
         return [f'exportplan/sections/{self.slug}.html']
 
 
-class ExportPlanMarketingApproachView(
-    PageTitleMixin, LessonDetailsMixin, FormContextMixin, ExportPlanSectionView, FormView
-):
-    form_class = forms.ExportPlanMarketingApproachForm
+class ExportPlanMarketingApproachView(PageTitleMixin, LessonDetailsMixin, ExportPlanSectionView):
     slug = 'marketing-approach'
     title = 'Marketing approach'
-
-    def get_initial(self):
-        return self.request.user.export_plan.data['marketing_approach']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -166,6 +160,7 @@ class ExportPlanMarketingApproachView(
         context['selected_age_groups'] = (
             self.request.user.export_plan.data['ui_options'].get(self.slug, {}).get('target_ages', [])
         )
+        context['marketing_approach'] = self.request.user.export_plan.data['marketing_approach']
 
         return context
 
