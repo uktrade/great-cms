e5ea5b043d72c91b79d1a934827bb586dfc939b7
Jake Hopking <jake@hopking.io>
2021-02-15 15:10:47 +0000

    Move options to constants file. Deflate.

---
+++ react-components/src/components/BusinessRisk/BusinessRisks/constants.js
@@ -0,0 +1,35 @@
+export const risk_likelihood_options = [
+  {
+    value: 'rare',
+  },
+  {
+    value: 'unlikely',
+  },
+  {
+    value: 'possible',
+  },
+  {
+    value: 'likely',
+  },
+  {
+    value: 'certain',
+  },
+]
+
+export const risk_impact_options = [
+  {
+    value: 'trivial',
+  },
+  {
+    value: 'minor',
+  },
+  {
+    value: 'moderate',
+  },
+  {
+    value: 'major',
+  },
+  {
+    value: 'severe',
+  },
+]
