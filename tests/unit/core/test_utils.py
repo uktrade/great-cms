d6cec31075148ab58ca0bd96ff343877717b9bda
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-02-15 16:19:24 +0000

    Fix tests

--- tests/unit/core/test_utils.py
+++ tests/unit/core/test_utils.py
@@ -119,84 +119,6 @@ def test_multiple_modules(domestic_homepage, client, user):
     assert page4_response.context_data.get('next_module') is None  # no next module, even though final lesson
 
 
-@pytest.mark.django_db
-def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
-    # Almost literally the same test as above, but with some placeholder blocks
-    # mixed in to show that they don't affect lesson counts or 'next' lessons
-
-    list_page = factories.ListPageFactory(parent=domestic_homepage, record_read_progress=True)
-    module_1 = factories.CuratedListPageFactory(
-        title='Module 1',
-        parent=list_page,
-    )
-    module_2 = factories.CuratedListPageFactory(
-        title='Module 2',
-        parent=list_page,
-    )
-    topic_1 = factories.TopicPageFactory(title='Topic 1', parent=module_1)
-    topic_2 = factories.TopicPageFactory(title='Topic 2', parent=module_1)
-    topic_3 = factories.TopicPageFactory(title='Topic 2', parent=module_2)
-
-    # Topic One's children
-    detail_page_1 = factories.DetailPageFactory(slug='detail-page-11', parent=topic_1)
-    factories.LessonPlaceholderPageFactory(title='Topic One: Placeholder One', parent=topic_1)
-    detail_page_2 = factories.DetailPageFactory(slug='detail-page-12', parent=topic_1)
-    factories.LessonPlaceholderPageFactory(title='Topic One: Placeholder Two', parent=topic_1)
-
-    # Topic Two's children
-    detail_page_3 = factories.DetailPageFactory(slug='detail-page-13', parent=topic_2)
-    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder One', parent=topic_2)
-    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder Two', parent=topic_2)
-
-    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder Three', parent=topic_2)
-
-    # Topic Three's children
-    factories.LessonPlaceholderPageFactory(title='Topic Three: Placeholder one', parent=topic_3)
-    detail_page_4 = factories.DetailPageFactory(slug='detail-page-24', parent=topic_3)
-    factories.LessonPlaceholderPageFactory(title='Topic Three: Placeholder Two', parent=topic_3)
-
-    pt_1 = PageTopicHelper(detail_page_1)
-    pt_2 = PageTopicHelper(detail_page_2)
-    pt_3 = PageTopicHelper(detail_page_3)
-
-    assert get_first_lesson(module_1) == detail_page_1
-    assert get_first_lesson(module_2) == detail_page_4  # placeholder skipped
-
-    assert len(get_all_lessons(module_1)) == 3  # placeholders skipped
-    assert len(get_all_lessons(module_2)) == 1  # placeholders skipped
-
-    assert pt_1.get_next_lesson() == detail_page_2
-    assert pt_2.get_next_lesson() == detail_page_3
-    # last page of module should have None as next lesson
-    assert pt_3.get_next_lesson() is None
-
-    client.force_login(user)
-
-    request = HttpRequest()
-    request.user = user
-    request.user.export_plan.data = {}
-    page1_response = detail_page_1.serve(request)
-    page2_response = detail_page_2.serve(request)
-    page3_response = detail_page_3.serve(request)
-    page4_response = detail_page_4.serve(request)
-
-    assert page1_response.context_data['next_lesson'].specific == detail_page_2
-    assert page1_response.context_data['current_module'].specific == module_1
-    assert page1_response.context_data.get('next_module') is None  # only present for final lesson in module
-
-    assert page2_response.context_data['next_lesson'].specific == detail_page_3
-    assert page2_response.context_data['current_module'].specific == module_1
-    assert page2_response.context_data.get('next_module') is None  # only present for final lesson in module
-
-    assert page3_response.context_data['next_lesson'].specific == detail_page_4
-    assert page3_response.context_data['current_module'].specific == module_1
-    assert page3_response.context_data['next_module'].specific == module_2
-
-    assert page4_response.context_data.get('next_lesson') is None
-    assert page4_response.context_data['current_module'] == module_2
-    assert page4_response.context_data.get('next_module') is None  # no next module, even though final lesson
-
-
 @pytest.mark.parametrize(
     'hs_code,country,region,expected_length, expected_filter_dict',
     [
@@ -212,7 +134,7 @@ def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
                 },
                 {
                     'hs_code_tags__name': '123456',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '123456'},
                 {
@@ -221,17 +143,17 @@ def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
                 },
                 {
                     'hs_code_tags__name': '1234',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '1234'},
                 {'hs_code_tags__name': '12', 'country_code_tags__name': 'IN'},
                 {
                     'hs_code_tags__name': '12',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '12'},
                 {'country_code_tags__name': 'IN'},
-                {'country_code_tags__name': 'Asia'},
+                {'region_code_tags__name': 'Asia'},
                 {'trading_bloc_code_tags__name': 'Regional Comprehensive Economic Partnership (RCEP)'},
                 {'trading_bloc_code_tags__name': 'South Asian Association for Regional Cooperation (SAARC)'},
                 {'trading_bloc_code_tags__name': 'South Asia Free Trade Area (SAFTA)'},
@@ -250,17 +172,17 @@ def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
                 },
                 {
                     'hs_code_tags__name': '1234',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '1234'},
                 {'hs_code_tags__name': '12', 'country_code_tags__name': 'IN'},
                 {
                     'hs_code_tags__name': '12',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '12'},
                 {'country_code_tags__name': 'IN'},
-                {'country_code_tags__name': 'Asia'},
+                {'region_code_tags__name': 'Asia'},
                 {'trading_bloc_code_tags__name': 'Regional Comprehensive Economic Partnership (RCEP)'},
                 {'trading_bloc_code_tags__name': 'South Asian Association for Regional Cooperation (SAARC)'},
                 {'trading_bloc_code_tags__name': 'South Asia Free Trade Area (SAFTA)'},
@@ -276,11 +198,11 @@ def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
                 {'hs_code_tags__name': '12', 'country_code_tags__name': 'IN'},
                 {
                     'hs_code_tags__name': '12',
-                    'country_code_tags__name': 'Asia',
+                    'region_code_tags__name': 'Asia',
                 },
                 {'hs_code_tags__name': '12'},
                 {'country_code_tags__name': 'IN'},
-                {'country_code_tags__name': 'Asia'},
+                {'region_code_tags__name': 'Asia'},
                 {'trading_bloc_code_tags__name': 'Regional Comprehensive Economic Partnership (RCEP)'},
                 {'trading_bloc_code_tags__name': 'South Asian Association for Regional Cooperation (SAARC)'},
                 {'trading_bloc_code_tags__name': 'South Asia Free Trade Area (SAFTA)'},
@@ -376,9 +298,87 @@ def test_personalised_filter_condition(
     mock_trading_blocs, hs_code, country, region, expected_length, expected_filter_dict
 ):
     filter_cond = get_personalised_case_study_orm_filter_args(hs_code=hs_code, country=country, region=region)
-
+    print(filter_cond)
     assert filter_cond == expected_filter_dict
     assert len(filter_cond) == expected_length
+
+
+@pytest.mark.django_db
+def test_placeholders_do_not_get_counted(domestic_homepage, client, user):
+    # Almost literally the same test as above, but with some placeholder blocks
+    # mixed in to show that they don't affect lesson counts or 'next' lessons
+
+    list_page = factories.ListPageFactory(parent=domestic_homepage, record_read_progress=True)
+    module_1 = factories.CuratedListPageFactory(
+        title='Module 1',
+        parent=list_page,
+    )
+    module_2 = factories.CuratedListPageFactory(
+        title='Module 2',
+        parent=list_page,
+    )
+    topic_1 = factories.TopicPageFactory(title='Topic 1', parent=module_1)
+    topic_2 = factories.TopicPageFactory(title='Topic 2', parent=module_1)
+    topic_3 = factories.TopicPageFactory(title='Topic 2', parent=module_2)
+
+    # Topic One's children
+    detail_page_1 = factories.DetailPageFactory(slug='detail-page-11', parent=topic_1)
+    factories.LessonPlaceholderPageFactory(title='Topic One: Placeholder One', parent=topic_1)
+    detail_page_2 = factories.DetailPageFactory(slug='detail-page-12', parent=topic_1)
+    factories.LessonPlaceholderPageFactory(title='Topic One: Placeholder Two', parent=topic_1)
+
+    # Topic Two's children
+    detail_page_3 = factories.DetailPageFactory(slug='detail-page-13', parent=topic_2)
+    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder One', parent=topic_2)
+    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder Two', parent=topic_2)
+
+    factories.LessonPlaceholderPageFactory(title='Topic Two: Placeholder Three', parent=topic_2)
+
+    # Topic Three's children
+    factories.LessonPlaceholderPageFactory(title='Topic Three: Placeholder one', parent=topic_3)
+    detail_page_4 = factories.DetailPageFactory(slug='detail-page-24', parent=topic_3)
+    factories.LessonPlaceholderPageFactory(title='Topic Three: Placeholder Two', parent=topic_3)
+
+    pt_1 = PageTopicHelper(detail_page_1)
+    pt_2 = PageTopicHelper(detail_page_2)
+    pt_3 = PageTopicHelper(detail_page_3)
+
+    assert get_first_lesson(module_1) == detail_page_1
+    assert get_first_lesson(module_2) == detail_page_4  # placeholder skipped
+
+    assert len(get_all_lessons(module_1)) == 3  # placeholders skipped
+    assert len(get_all_lessons(module_2)) == 1  # placeholders skipped
+
+    assert pt_1.get_next_lesson() == detail_page_2
+    assert pt_2.get_next_lesson() == detail_page_3
+    # last page of module should have None as next lesson
+    assert pt_3.get_next_lesson() is None
+
+    client.force_login(user)
+
+    request = HttpRequest()
+    request.user = user
+    request.user.export_plan.data = {}
+    page1_response = detail_page_1.serve(request)
+    page2_response = detail_page_2.serve(request)
+    page3_response = detail_page_3.serve(request)
+    page4_response = detail_page_4.serve(request)
+
+    assert page1_response.context_data['next_lesson'].specific == detail_page_2
+    assert page1_response.context_data['current_module'].specific == module_1
+    assert page1_response.context_data.get('next_module') is None  # only present for final lesson in module
+
+    assert page2_response.context_data['next_lesson'].specific == detail_page_3
+    assert page2_response.context_data['current_module'].specific == module_1
+    assert page2_response.context_data.get('next_module') is None  # only present for final lesson in module
+
+    assert page3_response.context_data['next_lesson'].specific == detail_page_4
+    assert page3_response.context_data['current_module'].specific == module_1
+    assert page3_response.context_data['next_module'].specific == module_2
+
+    assert page4_response.context_data.get('next_lesson') is None
+    assert page4_response.context_data['current_module'] == module_2
+    assert page4_response.context_data.get('next_module') is None  # no next module, even though final lesson
 
 
 @pytest.mark.parametrize(
