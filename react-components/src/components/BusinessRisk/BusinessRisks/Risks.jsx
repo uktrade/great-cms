82ecfdc4651785aa115c699ef7cfec9653d25ea9
Jake Hopking <jake@hopking.io>
2021-02-15 17:46:02 +0000

    Update Risks to work with new data format

--- react-components/src/components/BusinessRisk/BusinessRisks/Risks.jsx
+++ react-components/src/components/BusinessRisk/BusinessRisks/Risks.jsx
@@ -1,25 +1,47 @@
 import React, { memo } from 'react'
 // import PropTypes from 'prop-types'
 
-import { Risk } from './Trip'
+import { Risk } from './Risk'
 
-export const Risks = ({ formData, onChange, deleteRisk, addRisk }) => {
+export const Risks = ({
+  formData,
+  onChange,
+  deleteRisk,
+  addRisk,
+  likelihoodOptions,
+  impactOptions,
+}) => {
   return (
     <>
       {formData.length !== 0 && (
         <div className="costs costs--risks bg-blue-deep-10 p-v-s m-b-s">
           <table className="m-v-0">
             <tbody>
-              {formData.map(({ pk, value }, i) => (
-                <Risk
-                  index={i + 1}
-                  key={pk}
-                  id={pk}
-                  value={value}
-                  onChange={onChange}
-                  deleteRisk={deleteRisk}
-                />
-              ))}
+              {formData.map(
+                (
+                  {
+                    pk,
+                    notes,
+                    contingency_notes,
+                    risk_likelihood_option,
+                    risk_impact_option,
+                  },
+                  i
+                ) => (
+                  <Risk
+                    index={i + 1}
+                    key={pk}
+                    id={pk}
+                    notes={notes}
+                    contingency_notes={contingency_notes}
+                    onChange={onChange}
+                    deleteRisk={deleteRisk}
+                    likelihoodOptions={likelihoodOptions}
+                    impactOptions={impactOptions}
+                    selected={{ risk_likelihood_option, risk_impact_option }}
+                  />
+                )
+              )}
             </tbody>
           </table>
         </div>
