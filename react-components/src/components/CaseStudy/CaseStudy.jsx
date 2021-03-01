8062ce6a3e20980ea1e474ccefae65f33fc6ea63
Rafa Garcia <rafa@rafagarcia.info>
2021-03-01 10:39:24 +0000

    Fix tests

--- react-components/src/components/CaseStudy/CaseStudy.jsx
+++ react-components/src/components/CaseStudy/CaseStudy.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import ReactHtmlParser from 'react-html-parser'
 import Slider from 'react-slick'
