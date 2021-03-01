e12e0bdb2f5e9b38a3b67e393e9182282011e219
Steve Jalim <steve@stevejalim.net>
2021-03-01 14:05:08 +0000

    GP2-1686: Broaden test coverage for ported contact view: incl form that uses Notify
    + also lays in the framework for more rapidly testing other contact views we port that use Gov UK Notify

--- tests/unit/contact/test_views.py
+++ tests/unit/contact/test_views.py
@@ -76,6 +76,114 @@ def test_zendesk_submit_success(mock_form_session, client, url, success_url, vie
     )
 
 
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'url,success_url,view_class,agent_template,user_template,agent_email',
+    (
+        (
+            # V1 didn't have an explicit test for thixs
+            reverse('contact:contact-us-enquiries'),
+            reverse('contact:contact-us-domestic-success'),
+            views.DomesticEnquiriesFormView,
+            settings.CONTACT_ENQUIRIES_AGENT_NOTIFY_TEMPLATE_ID,
+            settings.CONTACT_ENQUIRIES_USER_NOTIFY_TEMPLATE_ID,
+            settings.CONTACT_ENQUIRIES_AGENT_EMAIL_ADDRESS,
+        ),
+        # TO BE PORTED IN SUBSEQUENT WORK
+        # (
+        #     reverse('contact-us-events-form'),
+        #     reverse('contact-us-events-success'),
+        #     views.EventsFormView,
+        #     settings.CONTACT_EVENTS_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_EVENTS_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_EVENTS_AGENT_EMAIL_ADDRESS,
+        # ),
+        # (
+        #     reverse('contact-us-dso-form'),
+        #     reverse('contact-us-dso-success'),
+        #     views.DefenceAndSecurityOrganisationFormView,
+        #     settings.CONTACT_DSO_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_DSO_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_DSO_AGENT_EMAIL_ADDRESS,
+        # ),
+        # (
+        #     reverse('contact-us-international'),
+        #     reverse('contact-us-international-success'),
+        #     views.InternationalFormView,
+        #     settings.CONTACT_INTERNATIONAL_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_INTERNATIONAL_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_INTERNATIONAL_AGENT_EMAIL_ADDRESS,
+        # ),
+        # (
+        #     reverse('office-finder-contact', kwargs={'postcode': 'FOO'}),
+        #     reverse('contact-us-office-success', kwargs={'postcode': 'FOO'}),
+        #     views.OfficeContactFormView,
+        #     settings.CONTACT_OFFICE_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_OFFICE_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_DIT_AGENT_EMAIL_ADDRESS,
+        # ),
+        # (
+        #     reverse('contact-us-exporting-to-the-uk-beis'),
+        #     reverse('contact-us-exporting-to-the-uk-beis-success'),
+        #     views.ExportingToUKBEISFormView,
+        #     settings.CONTACT_BEIS_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_BEIS_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_BEIS_AGENT_EMAIL_ADDRESS,
+        # ),
+        # (
+        #     reverse('contact-us-exporting-to-the-uk-defra'),
+        #     reverse('contact-us-exporting-to-the-uk-defra-success'),
+        #     views.ExportingToUKDERAFormView,
+        #     settings.CONTACT_DEFRA_AGENT_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_DEFRA_USER_NOTIFY_TEMPLATE_ID,
+        #     settings.CONTACT_DEFRA_AGENT_EMAIL_ADDRESS,
+        # )
+    ),
+)
+@mock.patch.object(views.FormSessionMixin, 'form_session_class')
+def test_notify_form_submit_success(
+    mock_form_session,
+    client,
+    url,
+    agent_template,
+    user_template,
+    view_class,
+    agent_email,
+    success_url,
+    settings,
+):
+    class Form(forms.SerializeDataMixin, django.forms.Form):
+        email = django.forms.EmailField()
+        save = mock.Mock()
+
+    with mock.patch.object(view_class, 'form_class', Form):
+        response = client.post(url, {'email': 'test@example.com'})
+
+    assert response.status_code == 302
+    assert response.url == success_url
+
+    assert Form.save.call_count == 2
+    assert Form.save.call_args_list == [
+        mock.call(
+            template_id=agent_template,
+            email_address=agent_email,
+            form_url=url,
+            form_session=mock_form_session(),
+            sender={
+                'email_address': 'test@example.com',
+                'country_code': None,
+                'ip_address': None,
+            },
+        ),
+        mock.call(
+            template_id=user_template,
+            email_address='test@example.com',
+            form_url=url,
+            form_session=mock_form_session(),
+        ),
+    ]
+
+
 contact_urls_for_prefill_tests = (
     reverse('contact:contact-us-domestic'),  # DomesticFormView
     reverse('contact:contact-us-enquiries'),  # DomesticEnquiriesFormView
