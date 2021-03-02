c72a505b27b698fec6e860cbb193bb8edfd4949f
Steve Jalim <steve@stevejalim.net>
2021-03-02 09:24:47 +0000

    GP2-1686: Backfilling more test coverage for core.mixins

--- tests/unit/core/test_mixins.py
+++ tests/unit/core/test_mixins.py
@@ -2,8 +2,10 @@ from unittest import mock
 
 import pytest
 
-from core.mixins import GetSnippetContentMixin
+from core.mixins import GetSnippetContentMixin, PrepopulateFormMixin
 from domestic.models import DomesticHomePage
+from sso.models import BusinessSSOUser
+from tests.helpers import create_response
 from tests.unit.domestic import factories
 
 
@@ -22,6 +24,9 @@ def test_get_snippet_content_mixin():
 
     test_view = TestView()
 
+    assert test_view.slug == 'foo-bar-baz'
+    assert test_view.snippet_import_path == 'path.to.models.FakeTestSnippet'
+
     mock_snippet_instance = mock.Mock(name='mock_snippet_instance')
 
     mock_snippet_model = mock.Mock(name='mock_snippet_model')
@@ -35,3 +40,57 @@ def test_get_snippet_content_mixin():
         assert test_view.get_snippet_instance() == mock_snippet_instance
 
     mock_snippet_model.objects.get.assert_called_once_with(slug='foo-bar-baz')
+
+
+@pytest.mark.parametrize(
+    'full_name,first_name,last_name',
+    (
+        ('James Example', 'James', 'Example'),
+        ('James', 'James', None),
+        ('James Earl Jones', 'James', 'Jones'),
+        ('', None, None),
+    ),
+)
+def test_retrieve_company_profile_mixin_name_guessing(
+    rf,
+    full_name,
+    first_name,
+    last_name,
+    mock_get_company_profile,
+):
+    mock_get_company_profile.return_value = {
+        'postal_full_name': full_name,
+    }
+    request = rf.get('/')
+    request.user = BusinessSSOUser(session_id=123)
+    mixin = PrepopulateFormMixin()
+    mixin.request = request
+
+    assert mixin.guess_given_name == first_name
+    assert mixin.guess_family_name == last_name
+
+
+@pytest.mark.parametrize(
+    'full_name,first_name,last_name',
+    (
+        ('James Example', 'James', 'Example'),
+        ('James', 'James', None),
+        ('James Earl Jones', 'James', 'Jones'),
+        ('', None, None),
+    ),
+)
+def test_retrieve_company_profile_mixin_name_guessing_user(
+    rf,
+    full_name,
+    first_name,
+    last_name,
+    company_profile,
+):
+    company_profile.return_value = create_response(status_code=404)
+    request = rf.get('/')
+    request.user = BusinessSSOUser(session_id=123, first_name=first_name, last_name=last_name)
+    mixin = PrepopulateFormMixin()
+    mixin.request = request
+
+    assert mixin.guess_given_name == first_name
+    assert mixin.guess_family_name == last_name
