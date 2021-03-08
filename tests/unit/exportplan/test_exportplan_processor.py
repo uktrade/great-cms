d0fe14893d77b06f73a3657dd0795d7135bb2cb3
depsiatwal <depsi.a@gmail.com>
2021-03-08 12:38:29 +0000

    change progress

--- tests/unit/exportplan/test_exportplan_processor.py
+++ tests/unit/exportplan/test_exportplan_processor.py
@@ -6,22 +6,6 @@ from exportplan.core import data, helpers
 from exportplan.core.processor import ExportPlanProcessor
 
 
-def test_calculate_ep_section_progress(export_plan_data):
-    progress = ExportPlanProcessor(export_plan_data).calculate_ep_section_progress()
-
-    assert progress == {
-        'about_your_business': {'total': 5, 'populated': 1},
-        'objectives': {'total': 1, 'populated': 1},
-        'target_markets_research': {'total': 5, 'populated': 0},
-        'adaptation_target_market': {'total': 10, 'populated': 0},
-        'marketing_approach': {'total': 1, 'populated': 1},
-        'total_cost_and_price': {'total': 8, 'populated': 5},
-        'getting_paid': {'total': 3, 'populated': 3},
-        'funding_and_credit': {'total': 2, 'populated': 2},
-        'travel_business_policies': {'total': 3, 'populated': 3},
-    }
-
-
 @pytest.mark.parametrize(
     'ui_progress_data, expected',
     [
@@ -121,5 +105,4 @@ def test_export_plan_processor_get_current_url_country_required(export_plan_data
 
 def test_export_plan_processor_calculate_ep_section_progress(user, export_plan_data, export_plan_section_progress_data):
     export_plan_parser = ExportPlanProcessor(export_plan_data)
-
     assert export_plan_parser.calculate_ep_section_progress() == export_plan_section_progress_data
