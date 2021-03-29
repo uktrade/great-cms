6af117b4047c26a06c37fcd10f782237d930deb7
Steve Jalim <steve@stevejalim.net>
2021-03-29 16:52:18 +0100

    GP2-1648: Split up georedirect tests to be easier to discover

--- tests/unit/core/test_helpers.py
+++ tests/unit/core/test_helpers.py
@@ -8,6 +8,9 @@ from django.http import HttpRequest
 from requests.exceptions import HTTPError
 
 from core import helpers
+from core.management.commands.download_geolocation_data import (
+    GeolocationLocalFileArchive,
+)
 from directory_api_client import api_client
 from directory_api_client.exporting import url_lookup_by_postcode
 from directory_constants import choices
@@ -98,6 +101,91 @@ def test_store_user_location_success(mock_user_location_create, mock_get_locatio
 
     assert mock_user_location_create.call_count == 1
     assert mock_user_location_create.call_args == mock.call(sso_session_id=user.session_id, data={'country': 'US'})
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=(None, False)))
+def test_geolocation_redirector_unroutable(rf):
+    request = rf.get('/')
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is False
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=('8.8.8.8', True)))
+def test_geolocation_redirector_cookie_set(rf):
+    request = rf.get('/')
+    request.COOKIES[helpers.GeoLocationRedirector.COOKIE_NAME] = True
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is False
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=('8.8.8.8', True)))
+def test_geolocation_redirector_language_param(rf):
+    request = rf.get('/', {'lang': 'en-gb'})
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is False
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=('8.8.8.8', True)))
+@mock.patch('core.helpers.GeoLocationRedirector.country_code', mock.PropertyMock(return_value=None))
+def test_geolocation_redirector_unknown_country(rf):
+    request = rf.get('/', {'lang': 'en-gb'})
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is False
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=('8.8.8.8', True)))
+@mock.patch('core.helpers.GeoLocationRedirector.country_code', new_callable=mock.PropertyMock)
+@pytest.mark.parametrize('country_code', helpers.GeoLocationRedirector.DOMESTIC_COUNTRY_CODES)
+def test_geolocation_redirector_is_domestic(mock_country_code, rf, country_code):
+    mock_country_code.return_value = country_code
+
+    request = rf.get('/', {'lang': 'en-gb'})
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is False
+
+
+@mock.patch('core.helpers.get_client_ip', mock.Mock(return_value=('8.8.8.8', True)))
+@mock.patch('core.helpers.GeoLocationRedirector.country_code', new_callable=mock.PropertyMock)
+@pytest.mark.parametrize('country_code', helpers.GeoLocationRedirector.COUNTRY_TO_LANGUAGE_MAP)
+def test_geolocation_redirector_is_international(mock_country_code, rf, country_code):
+    mock_country_code.return_value = country_code
+
+    request = rf.get('/')
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is True
+
+
+@pytest.mark.parametrize(
+    'ip_address,language',
+    (
+        ('221.194.47.204', 'zh-hans'),
+        ('144.76.204.44', 'de'),
+        ('195.12.50.155', 'es'),
+        ('110.50.243.6', 'ja'),
+    ),
+)
+def test_geolocation_end_to_end(rf, ip_address, language, settings):
+    request = rf.get('/', {'a': 'b'}, REMOTE_ADDR=ip_address)
+
+    archive = GeolocationLocalFileArchive()
+    archive.decompress(
+        file_name=settings.GEOIP_COUNTRY,
+        destination=settings.GEOIP_PATH,
+    )
+
+    redirector = helpers.GeoLocationRedirector(request)
+
+    assert redirector.should_redirect is True
+    url, querysrtring = redirector.get_response().url.split('?')
+    assert url == '/international/'
+    assert 'lang=' + language in querysrtring
+    assert 'a=b' in querysrtring
 
 
 @mock.patch.object(sso_api_client.user, 'create_user_profile')
