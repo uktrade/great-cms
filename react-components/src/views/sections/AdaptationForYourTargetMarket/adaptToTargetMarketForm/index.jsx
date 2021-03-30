28ae7fd0d80f28fc175fe6f585ad6a3358d921b1
Jake Hopking <jake@hopking.io>
2021-03-30 17:46:42 +0100

    Move content from django into component. Add learning component.

--- react-components/src/views/sections/AdaptationForYourTargetMarket/adaptToTargetMarketForm/index.jsx
+++ react-components/src/views/sections/AdaptationForYourTargetMarket/adaptToTargetMarketForm/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
 import { FormElements } from '@src/components/FormElements'
+import { Learning } from '@src/components/Learning/Learning'
 
 export const AdaptToTargetMarketForm = (params) => {
+  const { lessonDetails } = params
   return (
-    <div className="form-table bg-blue-deep-10 radius p-h-s p-v-xs">
-      <div className="target-market-form">
-        <FormElements {...params} />
+    <>
+      <h3 className="h-l">Changes to your product</h3>
+      <p>
+        Write down whether you need to make changes to any of the following and
+        include relevant details.
+      </p>
+      <Learning lesson={lessonDetails} />
+      <br />
+      <div className="form-table bg-blue-deep-10 radius p-h-s p-v-xs">
+        <div className="target-market-form">
+          <FormElements {...params} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
