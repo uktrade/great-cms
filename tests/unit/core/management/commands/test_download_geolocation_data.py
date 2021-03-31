84f3d88258ac42f1359bdd42517eda3f66c471da
Rambowalsh <christopherjwalsh2@gmail.com>
2021-03-31 11:22:32 +0100

    Rewound to what's currently in dev as a lot of what was brought from ui is not required

--- tests/unit/core/management/commands/test_download_geolocation_data.py
+++ tests/unit/core/management/commands/test_download_geolocation_data.py
@@ -1,43 +1,18 @@
 import os
-import shutil
 import tarfile
 from unittest.mock import call, patch
 
 import pytest
-import requests.exceptions
 from django.conf import settings
 from django.core.management import call_command
 
 from core.management.commands.download_geolocation_data import (
-    GeolocationArchiveNegotiator,
-    GeolocationLocalFileArchive,
+    GeolocationRemoteFileArchive,
 )
 
 
-class GeolocationBadLocalFileArchive(GeolocationLocalFileArchive):
-    location = os.path.join(
-        settings.PROJECT_ROOT, '../tests/unit/core/management/commands/tests/missing-database-file.tar.gz'
-    )
-
-
-@pytest.mark.parametrize(
-    'exception_class',
-    (
-        requests.exceptions.HTTPError,
-        requests.exceptions.ConnectionError,
-        requests.exceptions.SSLError,
-        requests.exceptions.Timeout,
-    ),
-)
-def test_falls_back_to_local_file_on_request_error(exception_class, settings, caplog, requests_mock):
-    requests_mock.get(settings.GEOLOCATION_MAXMIND_DATABASE_FILE_URL, exc=exception_class)
-    geolocation_archive = GeolocationArchiveNegotiator()
-
-    assert isinstance(geolocation_archive, GeolocationLocalFileArchive)
-
-    log = caplog.records[0]
-    assert log.levelname == 'ERROR'
-    assert log.msg == GeolocationArchiveNegotiator.MESSAGE_FAILED_TO_DOWNLOAD
+class GeolocationBadLocalFileArchive(GeolocationRemoteFileArchive):
+    location = os.path.join(settings.ROOT_DIR, '../core/geolocation_data/foo.mmdb')
 
 
 def test_handles_remote_invalid_archive(settings, requests_mock):
@@ -46,48 +21,46 @@ def test_handles_remote_invalid_archive(settings, requests_mock):
         status_code=200,
         content=b'hello',
     )
-    geolocation_archive = GeolocationArchiveNegotiator()
+    archive = GeolocationRemoteFileArchive()
 
     with pytest.raises(tarfile.ReadError):
-        geolocation_archive.decompress(
-            file_name=settings.GEOIP_COUNTRY, destination=os.path.join(settings.GEOIP_PATH, 'test')
+        archive.decompress(
+            file_like_object=archive.retrieve_file(edition_id='GeoLite2-Country'),
+            file_name=settings.GEOIP_COUNTRY,
         )
     with pytest.raises(tarfile.ReadError):
-        geolocation_archive.decompress(
-            file_name=settings.GEOIP_CITY, destination=os.path.join(settings.GEOIP_PATH, 'test')
+        archive.decompress(
+            file_like_object=archive.retrieve_file(edition_id='GeoLite2-Country'),
+            file_name=settings.GEOIP_CITY,
         )
 
 
 def test_handles_missing_database_file():
-    geolocation_archive = GeolocationBadLocalFileArchive()
+    archive = GeolocationBadLocalFileArchive()
 
     with pytest.raises(ValueError):
-        geolocation_archive.decompress(
-            file_name=settings.GEOIP_COUNTRY, destination=os.path.join(settings.GEOIP_PATH, 'test')
+        archive.decompress(
+            file_like_object=archive.retrieve_file(edition_id='GeoLite2-Country'),
+            file_name=settings.GEOIP_COUNTRY,
+        )
+
+    with pytest.raises(ValueError):
+        archive.decompress(
+            file_like_object=archive.retrieve_file(edition_id='GeoLite2-City'),
+            file_name=settings.GEOIP_CITY,
         )
 
 
-def test_handles_local_database_file(settings):
-    expected_path = os.path.join(settings.GEOIP_PATH, 'test')
-    expected_file = os.path.join(expected_path, settings.GEOIP_COUNTRY)
-
-    if os.path.exists(expected_path):
-        shutil.rmtree(expected_path)
-
-    geolocation_archive = GeolocationLocalFileArchive()
-    geolocation_archive.decompress(file_name=settings.GEOIP_COUNTRY, destination=expected_path)
-
-    assert os.path.exists(expected_file) is True
-    shutil.rmtree(expected_path)
-
-
-@patch('core.management.commands.download_geolocation_data.' 'GeolocationArchiveNegotiator')
+@patch('core.management.commands.download_geolocation_data.GeolocationRemoteFileArchive')
 def test_call_command(mock_geolocation_navigator, settings):
     call_command('download_geolocation_data')
-    geolocation_archive = mock_geolocation_navigator()
+    archive = mock_geolocation_navigator()
 
-    assert geolocation_archive.decompress.call_count == 1
-    assert geolocation_archive.decompress.call_args == call(
-        file_name=settings.GEOIP_COUNTRY,
-        destination=settings.GEOIP_PATH,
-    )
+    city_file = archive.retrieve_file(edition_id='GeoLite2-City')
+    country_file = archive.retrieve_file(edition_id='GeoLite2-Country')
+
+    assert archive.decompress.call_count == 2
+    assert archive.decompress.call_args_list == [
+        call(file_like_object=city_file, file_name=settings.GEOIP_CITY),
+        call(file_like_object=country_file, file_name=settings.GEOIP_COUNTRY),
+    ]
