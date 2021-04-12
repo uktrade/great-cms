231772d9d6d84b3cdbf3fbe81e1224fd481f5c7b
Jake Hopking <jake@hopking.io>
2021-04-12 15:03:06 +0100

    Add content_icons context var

--- exportplan/views.py
+++ exportplan/views.py
@@ -378,6 +378,7 @@ class PDFDownload(
         context = super().get_context_provider_data(request)
         context['risk_impact_options'] = choices_to_key_value(choices.RISK_IMPACT_OPTIONS)
         context['risk_likelihood_options'] = choices_to_key_value(choices.RISK_LIKELIHOOD_OPTIONS)
+        context['content_icons'] = False
         pdf = render_to_pdf('exportplan/pdf_download.html', context)
         response = HttpResponse(pdf, content_type='application/pdf')
         filename = 'export_plan.pdf'
