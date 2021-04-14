9bac02cb22bd00645d99ca8d6a4d0ee93910482f
Jake Hopking <jake@hopking.io>
2021-04-14 14:42:34 +0100

    Allow user to select dates in the past for 'start date'. Prevent user selecting a 'start date' in future which is past the 'end date'

--- react-components/src/components/ObjectivesList/Objective/index.jsx
+++ react-components/src/components/ObjectivesList/Objective/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import { TextArea } from '@src/components/Form/TextArea'
 import { Input } from '@src/components/Form/Input'
 import { ConfirmModal } from '@src/components/ConfirmModal/ConfirmModal'
-import { objectHasValue, dateNowISO } from '@src/Helpers'
+import { objectHasValue } from '@src/Helpers'
 import ErrorList from '../../ErrorList'
 
 export const Objective = memo(
@@ -15,8 +15,6 @@ export const Objective = memo(
         ...item,
       })
     }
-
-    const ISONow = dateNowISO()
 
     const onDelete = () => {
       deleteObjective(data.pk)
@@ -45,7 +43,7 @@ export const Objective = memo(
                   id="start_date"
                   type="date"
                   label="Start date"
-                  minDate={ISONow}
+                  maxDate={data.end_date}
                   value={data.start_date}
                   onChange={onChange}
                   errors={[]}
