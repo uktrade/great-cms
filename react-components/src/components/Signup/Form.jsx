c5c08a83bb331e44677d983c91cc5e96f6916d0e
Lewis Coulson <lewis@digital.trade.gov.uk>
2021-05-05 14:54:39 +0100

    GP2-2411: Unify label padding on sign in and up

--- react-components/src/components/Signup/Form.jsx
+++ react-components/src/components/Signup/Form.jsx
@@ -25,7 +25,7 @@ const Form = ({
       className="signup__form"
     >
       {showTitle && (
-        <legend className="h-s text-blue-deep-80">Create an account</legend>
+        <legend className="h-s text-blue-deep-80 p-t-xs">Create an account</legend>
       )}
       Already have an account? &nbsp;&nbsp;
       <a
