d7a1024ffaa3a510c421c7d7914bd8e676c3c827
Bob Meredith <bob.meredith@ntlworld.com>
2021-05-18 16:44:17 +0100

    Update

--- react-components/src/components/TargetAgeGroupInsights/AgeGroupData/index.jsx
+++ react-components/src/components/TargetAgeGroupInsights/AgeGroupData/index.jsx
@@ -17,7 +17,7 @@ export const AgeGroupData = memo(
     return (
       <>
         <h3 className="body-l-b m-t-s m-b-xs">
-          More details on {country.name}
+          More details on {country.country_name}
         </h3>
         <div className="stat-group radius-top-xs">
           <div className="grid">
