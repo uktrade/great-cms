41e1c39e012cea96c71161dda0aab1a29f40b208
Steve Jalim <steve@stevejalim.net>
2021-06-16 17:52:08 +0100

    GP2-1653: Porting Contact Us guidance pages from V1 to V2 - core work
    This gets all the functionality for the 'great-account' steps in the form, but there are gaps in behaviour related to other forms that the tests, ideally, are expecting. As such, will try to backfill more of that, too, within reason

--- contact/constants.py
+++ contact/constants.py
@@ -12,3 +12,59 @@ COMPANY_TYPE_OTHER_CHOICES = (
     ('FOREIGN', 'UK branch of foreign company'),
     ('OTHER', 'Other'),
 )
+
+HMRC = 'HMRC'
+DEFRA = 'Defra'
+BEIS = 'BEIS'
+IMPORT_CONTROLS = 'import-controls'
+TRADE_WITH_UK_APP = 'trade-with-uk-app'
+EXPORTING_TO_UK = 'exporting-to-uk'
+
+BUYING = 'buying'
+DOMESTIC = 'domestic'
+DSO = 'dso'
+EUEXIT = 'euexit'
+EVENTS = 'events'
+EXPORT_ADVICE = 'export-advice'
+EXPORT_OPPORTUNITIES = 'export-opportunities'
+EXPORTING = 'exporting'
+SELLING_ONLINE_OVERSEAS = 'selling-online-overseas'
+FINANCE = 'finance'
+GREAT_ACCOUNT = 'great-account'
+GREAT_SERVICES = 'great-services'
+INTERNATIONAL = 'international'
+INVESTING = 'investing'
+CAPITAL_INVEST = 'capital-invest'
+LOCATION = 'location'
+OTHER = 'other'
+TRADE_OFFICE = 'trade-office'
+ALERTS = 'alerts'
+NO_RESPONSE = 'no-response'
+
+NO_VERIFICATION_EMAIL = 'no-email-confirmation'
+PASSWORD_RESET = 'password-reset'
+COMPANIES_HOUSE_LOGIN = 'companies-house-login'
+COMPANY_NOT_FOUND = 'company-not-found'
+VERIFICATION_CODE = 'verification-code'
+NO_VERIFICATION_LETTER = 'no-verification-letter'
+NO_VERIFICATION_MISSING = 'verification-missing'
+
+MARKETING_SOURCES = (
+    (
+        'Print- magazines, newspaper or trade press',
+        'Magazines, newspaper or trade press',
+    ),
+    ('Print - posters or billboards', 'Posters or billboard'),
+    ('Digital - outdoor advertising digital screens', 'Outdoor advertising digital screen'),
+    ('Digital - advertisement / banner', 'Website advertisement'),
+    ('Digital - articles', 'Website article'),
+    (
+        ('Digital - social networking site ' '(e.g. LinkedIn, Xing!, Twitter, Facebook, YouTube)'),
+        'Social media website',
+    ),
+    (
+        'At an event',
+        'At an event',
+    ),
+    ('OTHER', 'Other'),
+)
