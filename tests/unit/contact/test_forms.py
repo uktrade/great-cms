3f0af994497e936001409a8a3d04c0b66561c63c
Steve Jalim <steve@stevejalim.net>
2021-06-17 17:25:19 +0100

    GP2-1653: Backfill Contact triage views so that one can go from /contact/ to the great-account help pages
    This has involved PARTIALLY porting some other forms - eg exopps or international triage forms - so this is NOT a completely working section yet, but future PRs will follow soon to plug the gaps

--- tests/unit/contact/test_forms.py
+++ tests/unit/contact/test_forms.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 import requests_mock
 
-from contact import forms, views
+from contact import constants, forms, views
 from directory_api_client.exporting import url_lookup_by_postcode
 
 routing_steps = [step for step, _ in views.RoutingFormView.form_list]
@@ -269,3 +269,71 @@ def test_postcode_validation(valid_request_export_support_form_data):
     valid_request_export_support_form_data['company_postcode'] = 'W1A W1A'
     form = forms.ExportSupportForm(data=valid_request_export_support_form_data)
     assert form.is_valid() is False
+
+
+def test_location_form_routing():
+    field = forms.LocationRoutingForm.base_fields['choice']
+    # for each of the choices the form supports
+    for choice, _ in field.choices:
+        # the view supports routing the user to that step
+        assert choice in routing_steps
+
+
+def test_domestic_form_routing():
+    field = forms.DomesticRoutingForm.base_fields['choice']
+    choices = set(item for item, _ in field.choices)
+
+    # expect these choices to result in a redirect to a new form
+    choices_expect_redirect = {
+        constants.TRADE_OFFICE,
+        constants.EXPORT_ADVICE,
+        constants.FINANCE,
+        constants.EUEXIT,
+        constants.EVENTS,
+        constants.DSO,
+        constants.OTHER,
+    }
+    mapping = views.RoutingFormView.redirect_mapping[constants.DOMESTIC]
+
+    for choice in choices_expect_redirect:
+        assert choice in choices
+        assert choice in mapping
+        assert choice not in routing_steps
+
+    choices_expect_next_step = (constants.GREAT_SERVICES,)
+    for choice in choices_expect_next_step:
+        assert choice in choices
+        assert choice not in mapping
+        assert choice in routing_steps
+
+    expected_choice_count = len(choices_expect_next_step) + len(choices_expect_redirect)
+
+    assert expected_choice_count == len(choices)
+
+
+def test_great_services_form_routing():
+    field = forms.GreatServicesRoutingForm.base_fields['choice']
+    choices = set(item for item, _ in field.choices)
+
+    choices_expect_redirect = {
+        constants.OTHER,
+    }
+    mapping = views.RoutingFormView.redirect_mapping[constants.GREAT_SERVICES]
+
+    for choice in choices_expect_redirect:
+        assert choice in choices
+        assert choice in mapping
+        assert choice not in routing_steps
+
+    choices_expect_next_step = {
+        constants.EXPORT_OPPORTUNITIES,
+        constants.GREAT_ACCOUNT,
+    }
+    for choice in choices_expect_next_step:
+        assert choice in choices
+        assert choice not in mapping
+        assert choice in routing_steps
+
+    expected_choice_count = len(choices_expect_next_step) + len(choices_expect_redirect)
+
+    assert expected_choice_count == len(choices)
