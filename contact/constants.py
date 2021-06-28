20bfc44572cc75d9982d8203f3cf19e4687751d4
Steve Jalim <steve@stevejalim.net>
2021-06-28 15:38:26 +0100

    GP2-1683: Port more contact views from V1 into V2
    This changeset ports the pages at the following URLs from V1 into V2
    
    https://www.great.gov.uk/contact/export-advice/*
    https://www.great.gov.uk/contact/international/*
    
    It also wires up some more of the routing in the main form, just to get ahead of it for subsequent tickets

--- contact/constants.py
+++ contact/constants.py
@@ -68,3 +68,37 @@ MARKETING_SOURCES = (
     ),
     ('OTHER', 'Other'),
 )
+
+
+CONTACT_FORM_INDUSTRIES = [
+    'Advanced engineering',
+    'Aerospace',
+    'Agriculture, horticulture, fisheries and pets',
+    'Airports',
+    'Automotive',
+    'Chemicals',
+    'Construction',
+    'Consumer and retail',
+    'Creative industries',
+    'Defence',
+    'Education and training',
+    'Energy',
+    'Environment',
+    'Financial and professional services',
+    'Food and drink',
+    'Healthcare services',
+    'Maritime',
+    'Medical devices and equipment',
+    'Mining',
+    'Pharmaceuticals and biotechnology',
+    'Railways',
+    'Security',
+    'Space',
+    'Sports economy',
+    'Technology and smart cities',
+    'Water',
+]
+
+INDUSTRY_CHOICES = [('', 'Please select')] + [(item, item) for item in CONTACT_FORM_INDUSTRIES] + [('OTHER', 'Other')]
+
+INDUSTRY_MAP = dict(INDUSTRY_CHOICES)
