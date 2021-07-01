9846b168f5f0d75b791a51a0671c7547af4f8fe7
Fred Marecesche <fred@froddd.co.uk>
2021-07-01 16:11:43 +0100

    Use updated DB name to allow tests to run against docker DB

--- dockerise/postgres/init-db.sh
+++ dockerise/postgres/init-db.sh
@@ -5,20 +5,11 @@ set -e
 /data_dumps/getS3DataDump.sh
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER debug WITH PASSWORD 'debug';
+    CREATE ROLE debug WITH LOGIN SUPERUSER PASSWORD 'debug';
 
     CREATE DATABASE directory_api_debug;
-    CREATE DATABASE directory_sso_debug;
+    CREATE DATABASE sso_debug;
     CREATE DATABASE directory_forms_api_debug;
     CREATE DATABASE greatcms;
-
-
-    GRANT ALL PRIVILEGES ON DATABASE directory_api_debug TO debug;
-    GRANT ALL PRIVILEGES ON DATABASE directory_forms_api_debug TO debug;
-    GRANT ALL PRIVILEGES ON DATABASE greatcms TO debug;
-    GRANT ALL PRIVILEGES ON DATABASE directory_sso_debug TO debug;
-
-
-
 EOSQL
 psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" greatcms < /data_dumps/$DATABASE_DUMP_FILENAME
