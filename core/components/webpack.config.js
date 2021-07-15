1616819a2569252abf0326048e2634c403a02df7
Le H Ngo <hgle.ngo@gmail.com>
2021-07-15 08:30:06 +0100

    -Updating gulpfile inside componotents.

---
+++ core/components/webpack.config.js
@@ -0,0 +1,19 @@
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// // import { MiniCssExtractPlugin } from 'mini-css-extract-plugin'
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss$/,
+//         use: ['style-loader', 'css-loader', 'sass-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin({
+//       filename: '[name].css',
+//       chunkFilename: '[id].css',
+//     }),
+//   ],
+// }
