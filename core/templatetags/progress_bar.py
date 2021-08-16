d580a3d7c5f717d9024f7fb2fe0b88369bf26a59
Le H Ngo <hgle.ngo@gmail.com>
2021-08-16 18:24:01 +0100

    This commit will fix empty string in progress bar def.

--- core/templatetags/progress_bar.py
+++ core/templatetags/progress_bar.py
@@ -7,8 +7,10 @@ register = template.Library()
 @register.simple_tag
 def progress_bar(total, complete):
     """
-    Renders a progress bar given total abnd completed
+    Renders a progress bar given total and completed
     """
+    if total == "":
+        total = 0
 
     percentage = int((complete / total) * 100) if (total or 0) > 0 else 0
 
