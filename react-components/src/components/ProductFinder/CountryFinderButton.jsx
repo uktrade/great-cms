b47bc2a95ebfa23776bf8145bf2b4f6981175380
Le H Ngo <hgle.ngo@gmail.com>
2021-10-25 12:59:19 +0100

    - Rebase

--- react-components/src/components/ProductFinder/CountryFinderButton.jsx
+++ react-components/src/components/ProductFinder/CountryFinderButton.jsx
@@ -7,6 +7,7 @@ import { useUserMarkets } from '@src/components/hooks/useUserData'
 import { sortMapBy } from '@src/Helpers'
 import CountryFinderModal from './CountryFinderModal'
 import BasketViewer from './BasketView'
+import { Confirmation } from '@src/components/ConfirmModal/Confirmation'
 
 export const CountryFinderButton = () => {
   const [modalIsOpen, setIsOpen] = useState(false)
@@ -14,10 +15,28 @@ export const CountryFinderButton = () => {
 
   const sortMap = sortMapBy(markets || [], 'country_name')
 
+  const [deleteConfirm, setDeleteConfirm] = useState({
+    delete: null,
+    index: null,
+    name: null
+  })
+
+  const confirmDelete = (name, index) => {
+    setDeleteConfirm({
+      delete: true,
+      index: index,
+      name: name
+    })
+  }
+
   const deleteMarket = (index) => {
     const reduced = [...markets]
     reduced.splice(index, 1)
     setMarkets(reduced)
+    setDeleteConfirm({
+      ...confirmDelete,
+      delete: false
+    })
   }
 
   const selectCountry = (country) => {
@@ -25,10 +44,10 @@ export const CountryFinderButton = () => {
       addMarketItem(country)
     }
   }
-
   return (
     <span>
       <BasketViewer label="My markets" onOpen={loadMarkets}>
+        {sortMap.length === 0 ? <p class="body-l-b text-center">My markets is empty</p>: null}
         <ul className="list m-v-0 body-l-b">
           {sortMap.map((marketIdx) => {
             const market = markets[marketIdx]
@@ -37,9 +56,10 @@ export const CountryFinderButton = () => {
                 <button
                   type="button"
                   className="f-r button button--small button--only-icon button--tertiary"
-                  onClick={() => deleteMarket(marketIdx)}
+                  // onClick={() => deleteMarket(marketIdx)}
+                  onClick={() => confirmDelete(market.country_name, marketIdx)}
                 >
-                  <i className="fas fa-trash-alt" />
+                  <i className="fas fa-times fa-lg" />
                   <span className="visually-hidden">
                     Remove market {market.country_name}
                   </span>
@@ -49,15 +69,14 @@ export const CountryFinderButton = () => {
             )
           })}
         </ul>
-        <button
-          type="button"
-          className="button button--primary button--icon m-t-xs button--full-width"
-          onClick={() => setIsOpen(true)}
-        >
-          <i className="fas fa-plus" />
-          Add market
-        </button>
       </BasketViewer>
+      {deleteConfirm.delete && <Confirmation
+        title={`Are you sure you want to remove ${deleteConfirm.name}?`}
+        yesLabel="Remove"
+        yesIcon="fa-trash-alt"
+        onYes={() => deleteMarket(deleteConfirm.index)}
+        onNo={() => setDeleteConfirm({...deleteConfirm, delete: false})}
+      />}
       {modalIsOpen && (
       <CountryFinderModal
         modalIsOpen
--- react-components/src/components/ProductFinder/ProductFinderButton.jsx
+++ react-components/src/components/ProductFinder/ProductFinderButton.jsx
@@ -10,23 +10,43 @@ import { sortMapBy } from '@src/Helpers'
 import ProductFinderModal from './ProductFinderModal'
 import BasketViewer from './BasketView'
 
+import { Confirmation } from '@src/components/ConfirmModal/Confirmation'
+
 function ProductFinderButton() {
   const [modalIsOpen, setIsOpen] = useState(false)
   const {products, setProducts, loadProducts} = useUserProducts(
     false
   )
 
-  const sortMap = sortMapBy(products || [],'commodity_name')
+  const sortMap = sortMapBy(products || [], 'commodity_name')
+  const [deleteConfirm, setDeleteConfirm] = useState({
+    delete: null,
+    index: null,
+    name: null
+  })
+
+  const confirmDelete = (name, index) => {
+    setDeleteConfirm({
+      delete: true,
+      index: index,
+      name: name
+    })
+  }
 
   const deleteProduct = (index) => {
     const reduced = [...products]
     reduced.splice(index, 1)
     setProducts(reduced)
+    setDeleteConfirm({
+      ...confirmDelete,
+      delete: false
+    })
   }
 
   return (
     <>
       <BasketViewer label="My products" onOpen={loadProducts}>
+        {sortMap.length === 0 ? <p class="body-l-b text-center">My products is empty</p>: null}
         <ul className="list m-v-0 body-l-b">
           {sortMap.map((mapIndex) => {
             const product = products[mapIndex]
@@ -38,9 +58,9 @@ function ProductFinderButton() {
                 <button
                   type="button"
                   className="button button--small button--only-icon button--tertiary"
-                  onClick={() => deleteProduct(mapIndex)}
+                  onClick={() => confirmDelete(product.commodity_name, mapIndex)}
                 >
-                  <i className="fas fa-trash-alt" />
+                  <i className="fas fa-times fa-lg" />
                   <span className="visually-hidden">
                     Remove product {ReactHtmlParser(product.commodity_name)}
                   </span>
@@ -50,15 +70,14 @@ function ProductFinderButton() {
             )
           })}
         </ul>
-        <button
-          type="button"
-          className="button button--primary button--icon m-t-xs button--full-width"
-          onClick={() => setIsOpen(true)}
-        >
-          <i className="fas fa-plus" />
-          Add product
-        </button>
       </BasketViewer>
+      {deleteConfirm.delete && <Confirmation
+        title={`Are you sure you want to remove ${deleteConfirm.name}?`}
+        yesLabel="Remove"
+        yesIcon="fa-trash-alt"
+        onYes={() => deleteProduct(deleteConfirm.index)}
+        onNo={() => setDeleteConfirm({...deleteConfirm, delete: false})}
+      />}
       <ProductFinderModal modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />
     </>
   )
