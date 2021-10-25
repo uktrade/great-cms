d7287784716d6929821c44e984812af3d3fea0b3
Rafa Garcia <rafa@rafagarcia.info>
2021-10-25 09:32:21 +0100

    Add management command to purge admin users

---
+++ core/management/commands/purge_admin_users.py
@@ -0,0 +1,27 @@
+import sys
+
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand, call_command
+
+
+class Command(BaseCommand):
+    help = 'Purge admin users'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.UserModel = get_user_model()
+
+    def handle(self, *args, **options):
+        proceed = input('This operation will purge the users table, proceed anyway? [Y/n]: ') or 'y'
+        if proceed[0].lower() == 'y':
+            self.UserModel.objects.all().delete()
+        else:
+            self.stderr.write('Operation cancelled.')
+            sys.exit(1)
+
+        create_superuser = input('Create superuser? [Y/n]: ') or 'y'
+
+        if create_superuser[0].lower() == 'y':
+            call_command('createsuperuser')
+
+        self.stdout.write(self.style.SUCCESS('All done, bye!'))
