1cd3eaf2af1bbd13dfdade9b0b2b1c5e1d095a1e
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2022-02-02 11:47:56 +0000

    Switching to opensearch from elasticsearch

--- config/settings.py
+++ config/settings.py
@@ -394,9 +394,9 @@ if ELASTIC_APM_ENABLED:
 ELASTICSEARCH_PROVIDER = env.str('ELASTICSEARCH_PROVIDER', 'aws').lower()
 
 if ELASTICSEARCH_PROVIDER == 'govuk-paas':
-    services = {item['instance_name']: item for item in VCAP_SERVICES['elasticsearch']}
+    services = {item['instance_name']: item for item in VCAP_SERVICES['opensearch']}
     ELASTICSEARCH_INSTANCE_NAME = env.str(
-        'ELASTICSEARCH_INSTANCE_NAME', VCAP_SERVICES['elasticsearch'][0]['instance_name']
+        'ELASTICSEARCH_INSTANCE_NAME', VCAP_SERVICES['opensearch'][0]['instance_name']
     )
     connections.create_connection(
         alias='default',
