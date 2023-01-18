53a7e19252bcd99fc89512ad8b52bebb1eaff48d
Rafa Garcia <rafa@rafagarcia.info>
2023-01-18 10:53:06 +0000

    Rename app to 'export_academy'

--- config/urls.py
+++ config/urls.py
@@ -51,7 +51,7 @@ urlpatterns += [
     path('', include(domestic.urls, namespace='domestic')),
     path('', include(core.urls, namespace='core')),
     path('', include(contact.urls)),  # No prefix because not all of them start with /contact/
-    path('', include(events.urls, namespace='events')),
+    path('', include(export_academy.urls, namespace='export_academy')),
 ]
 
 urlpatterns += [
--- events/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import ListView
-
-from events import models
-
-
-class EventListView(ListView):
-    model = models.Event
