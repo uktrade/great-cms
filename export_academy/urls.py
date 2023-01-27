3be0ee6a14647ec8e6f57b37fb01c3caa1eff388
Abhilash Nair <abhilash.nair@digital.trade.gov.uk>
2023-01-27 12:38:39 +0000

    Registrations

--- export_academy/urls.py
+++ export_academy/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
+from core.urls import SIGNUP_URL
 from export_academy import views
 
 app_name = 'export_academy'
@@ -9,5 +11,18 @@ urlpatterns = [
         'upcoming-events/',
         views.EventListView.as_view(),
         name='upcoming-events',
+    ),
+    path(
+        'registration/', login_required(views.RegistrationFormView.as_view(), login_url=SIGNUP_URL), name='registration'
+    ),
+    path(
+        'registration-success/',
+        views.RegistrationSuccessPageView.as_view(),
+        name='registration-success',
+    ),
+    path(
+        'booking-success/',
+        views.BookingSuccessPageView.as_view(),
+        name='booking-success',
     ),
 ]
--- export_academy/views.py
+++ export_academy/views.py
@@ -1,7 +1,55 @@
-from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView
 
+from config import settings
+from contact.views import BaseNotifyFormView
+from core.datastructures import NotifySettings
 from export_academy import models
+from export_academy.forms import EARegistration
 
 
 class EventListView(ListView):
     model = models.Event
+
+
+class RegistrationFormView(BaseNotifyFormView):
+    template_name = 'registration_form.html'
+    form_class = EARegistration
+    success_url = reverse_lazy('export_academy:registration-success')
+    notify_settings = NotifySettings(
+        agent_template=settings.UKEF_CONTACT_AGENT_NOTIFY_TEMPLATE_ID,
+        agent_email=settings.UKEF_CONTACT_AGENT_EMAIL_ADDRESS,
+        user_template=settings.UKEF_CONTACT_USER_NOTIFY_TEMPLATE_ID,
+    )
+
+    def form_valid(self, form):
+        user_email = form.cleaned_data['email']
+        self.request.session['user_email'] = user_email
+        return super().form_valid(form)
+
+
+class RegistrationSuccessPageView(TemplateView):
+    template_name = 'registration_form_success.html'
+
+    def get(self, *args, **kwargs):
+        if not self.request.session.get('user_email'):
+            return HttpResponseRedirect(reverse_lazy('export_academy:upcoming-events'))
+        return super().get(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_email'] = self.request.session.get('user_email')
+        return super().get_context_data(**kwargs)
+
+
+class BookingSuccessPageView(TemplateView):
+    template_name = 'booking_success.html'
+
+    def get(self, *args, **kwargs):
+        if not self.request.session.get('user_email'):
+            return HttpResponseRedirect(reverse_lazy('export_academy:upcoming-events'))
+        return super().get(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_email'] = self.request.session.get('user_email')
+        return super().get_context_data(**kwargs)
