025da400d340ed3afd5ca0b2c0bb9655913982a8
Rafa Garcia <rafa@rafagarcia.info>
2023-02-10 20:04:18 +0000

    Refactor registration middleware & add more tests

--- export_academy/middleware.py
+++ export_academy/middleware.py
@@ -1,16 +1,10 @@
 from django.utils.deprecation import MiddlewareMixin
 
-from export_academy.models import Registration
-from sso.models import BusinessSSOUser
+from export_academy.helpers import is_export_academy_registered
 
 
 class ExportAcademyRegistrationMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if request.path.startswith('export_academy'):
+        if request.path.startswith('/export-academy'):
             user = request.user
-            if user.is_authenticated and isinstance(user, BusinessSSOUser):
-                try:
-                    Registration.objects.get(pk=user.email)
-                    request.user.is_export_academy_user = True
-                except Registration.DoesNotExist:
-                    pass
+            request.is_export_academy_registered = is_export_academy_registered(user)
