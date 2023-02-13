d670b1a3c752ccac03ee93e82f8cb9f6b0c911f0
Rafa Garcia <rafa@rafagarcia.info>
2023-02-13 11:47:27 +0000

    Clean up unused items

--- export_academy/apps.py
+++ export_academy/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class ExportAcademyConfig(AppConfig):
     name = 'export_academy'
-
-    def ready(self):
-        from export_academy import context  # noqa F401
