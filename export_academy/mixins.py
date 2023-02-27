d2ccf09071b0ef3446cede8a1bd1e27d209e6d9b
Abhilash Nair <abhilash.nair@digital.trade.gov.uk>
2023-02-27 15:11:47 +0000

    remove redundant code

--- export_academy/mixins.py
+++ export_academy/mixins.py
@@ -1,22 +1,6 @@
 from directory_forms_api_client import actions
 from directory_forms_api_client.forms import GovNotifyEmailActionMixin
 
-from config import settings
-from export_academy import models
-
-# class SaveAndSendNotifyMixin(GovNotifyEmailActionMixin):
-#     def save(self, form):
-#         user_email = self.request.user.email
-#         super(GovNotifyEmailActionMixin, self).save(
-#             template_id=self.notify_template,
-#             email_address=user_email,
-#             form_url=self.request.get_full_path(),
-#         )
-#         return super(SaveAndSendNotifyMixin, self).save()
-#
-#     def save_registration(self, data):
-#         self.model.save(data)
-
 
 class SaveAndSendNotifyMixin(GovNotifyEmailActionMixin):
     def save_model(self, data):
@@ -33,29 +17,3 @@ class SaveAndSendNotifyMixin(GovNotifyEmailActionMixin):
         )
         response = action.save(data)
         response.raise_for_status()
-
-
-class ApplyRegistrationMixin:
-    def send_gov_notify(self, form):
-        user_email = self.request.user.email
-        form.save(
-            template_id=settings.EXPORT_ACADEMY_NOTIFY_REGISTRATION_TEMPLATE_ID,
-            email_address=user_email,
-            form_url=self.request.get_full_path(),
-        )
-
-    def save_registration(self, form):
-        cleaned_data = form.cleaned_data
-        user_email = self.request.user.email
-        reg = models.Registration(
-            first_name=cleaned_data.get('first_name'),
-            last_name=cleaned_data.get('last_name'),
-            email=user_email,
-            data=cleaned_data,
-        )
-        reg.save()
-
-    def form_valid(self, form):
-        self.save_registration(form)
-        self.send_gov_notify(form)
-        return super().form_valid(form)
