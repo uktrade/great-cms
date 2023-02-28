19a656315b4c3c80e48cbf20a2c961cd29985769
Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
2023-02-28 11:01:07 +0000

    PR modifications

--- international_online_offer/forms.py
+++ international_online_offer/forms.py
@@ -28,8 +28,20 @@ class IntentForm(forms.Form):
     ]
     intent = forms.fields.MultipleChoiceField(
         label='',
-        required=True,
+        required=False,
         widget=forms.CheckboxSelectInlineLabelMultiple(attrs={'id': 'intent-select'}),
         choices=CHOICES,
     )
     intent_other = forms.CharField(label='', min_length=2, max_length=50, required=False)
+
+    def clean(self):
+        data = self.cleaned_data
+        intent = data.get('intent')
+        intent_other = data.get('intent_other')
+        if not intent:
+            self.add_error('intent', 'This field is required')
+            return data
+        if any("Other" in s for s in intent) and not intent_other:
+            self.add_error('intent_other', 'This field is required')
+        else:
+            return data
