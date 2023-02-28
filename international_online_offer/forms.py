b98d398d707b631cb75f597d156f4b75329774f2
Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
2023-02-28 11:18:57 +0000

    Modify intent validation to make more simple

--- international_online_offer/forms.py
+++ international_online_offer/forms.py
@@ -28,7 +28,7 @@ class IntentForm(forms.Form):
     ]
     intent = forms.fields.MultipleChoiceField(
         label='',
-        required=False,
+        required=True,
         widget=forms.CheckboxSelectInlineLabelMultiple(attrs={'id': 'intent-select'}),
         choices=CHOICES,
     )
@@ -38,10 +38,7 @@ class IntentForm(forms.Form):
         data = self.cleaned_data
         intent = data.get('intent')
         intent_other = data.get('intent_other')
-        if not intent:
-            self.add_error('intent', 'This field is required')
-            return data
-        if any('Other' in s for s in intent) and not intent_other:
+        if intent and any('Other' in s for s in intent) and not intent_other:
             self.add_error('intent_other', 'This field is required')
         else:
             return data
