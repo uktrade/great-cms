11bb753ad42004beea14683d6e39c865253a6477
Abhilash Nair <abhilash.nair@digital.trade.gov.uk>
2023-03-03 12:21:56 +0000

    Update event remind email format

--- export_academy/tasks.py
+++ export_academy/tasks.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from directory_forms_api_client import actions
 from django.conf import settings
@@ -11,8 +11,8 @@ from export_academy.models import Booking, Event
 def send_automated_notification():
     time_delay = settings.EXPORT_ACADEMY_AUTOMATED_NOTIFY_TIME_DELAY_MINUTES
     events = Event.objects.filter(
-        start_date__gte=datetime.now() + timedelta(minutes=time_delay),
-        start_date__lt=datetime.now() + timedelta(minutes=time_delay * 2),
+        start_date__gte=datetime.now(timezone.utc) + timedelta(minutes=time_delay),
+        start_date__lt=datetime.now(timezone.utc) + timedelta(minutes=time_delay * 2),
     )
 
     for event in events:
@@ -22,9 +22,15 @@ def send_automated_notification():
 
             action = actions.GovNotifyEmailAction(
                 email_address=email,
-                template_id=settings.EXPORT_ACADEMY_NOTIFY_BOOKING_TEMPLATE_ID,
+                template_id=settings.EXPORT_ACADEMY_NOTIFY_EVENT_REMINDER_TEMPLATE_ID,
                 form_url=str(),
             )
-            notify_data = dict(first_name=booking.registration.first_name, event_names=booking.event.name)
+            event_time = f'{event.start_date.strftime("%H:%M")} - {event.end_date.strftime("%H:%M")}'
+            notify_data = dict(
+                first_name=booking.registration.first_name,
+                event_name=booking.event.name,
+                event_date=event.start_date.strftime("%-d %B %Y"),
+                event_time=event_time,
+            )
 
             action.save(notify_data)
