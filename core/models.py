701a4ce4d04b64f3f1dfe007ee382cd61b04218a
LuptonM <marc.lupton100@gmail.com>
2023-03-13 11:04:04 +0000

    save nav changes

--- core/models.py
+++ core/models.py
@@ -1471,16 +1471,17 @@ class MicrositePage(cms_panels.MicrositePanels, Page):
             {
                 'url': child.get_url(),
                 'title': child.title,
+                'children': self.get_secondary_pages(child)
             }
             for child in parent_page.get_children().live()
         ]
 
     # Return the children of the MicrositePage at the top of current user journey
-    def get_secondary_pages(self):
-        if type(self.get_parent().specific) == Microsite:
-            return [{'title': child.title, 'url': child.get_url()} for child in self.get_children()]
+    def get_secondary_pages(self, page):
+        if type(page.get_parent().specific) == Microsite:
+            return [{'title': child.title, 'url': child.get_url()} for child in page.get_children()]
         else:
-            parent_page = self.get_ancestors().live().type(MicrositePage).first().specific
+            parent_page = page.get_ancestors().live().type(MicrositePage).first().specific
             return [{'title': child.title, 'url': child.get_url()} for child in parent_page.get_children()]
 
     # Return the children of a child or grandchild page
