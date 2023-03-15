4b0ed650cf5ed2cfca9e8021f01a23ba1180fc48
Abhilash Nair <abhilash.nair@digital.trade.gov.uk>
2023-03-15 17:08:59 +0000

    Try to make celery work

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ import sentry_sdk
 from django.urls import reverse_lazy
 from elasticsearch import RequestsHttpConnection
 from elasticsearch_dsl.connections import connections
+from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 
 import healthcheck.backends
@@ -329,7 +330,9 @@ else:
 # Sentry
 if env.str('SENTRY_DSN', ''):
     sentry_sdk.init(
-        dsn=env.str('SENTRY_DSN'), environment=env.str('SENTRY_ENVIRONMENT'), integrations=[DjangoIntegration()]
+        dsn=env.str('SENTRY_DSN'),
+        environment=env.str('SENTRY_ENVIRONMENT'),
+        integrations=[DjangoIntegration(), CeleryIntegration()],
     )
 
 USE_X_FORWARDED_HOST = True
