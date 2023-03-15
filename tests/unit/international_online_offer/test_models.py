56add18843ac28f14a816a1acd6f0532f22cdaca
Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
2023-03-15 08:50:42 +0000

    Reverting change

--- tests/unit/international_online_offer/test_models.py
+++ tests/unit/international_online_offer/test_models.py
@@ -1,36 +1,38 @@
+from wagtail.tests.utils import WagtailPageTests
+
 from international_online_offer.models import IOOGuidePage, IOOLandingPage
 
 
-def test_ioo_landing_page_allowed_parents(self):
-    self.assertAllowedParentPageTypes(
-        IOOLandingPage,
-        {
+class IOOLandingPageTests(WagtailPageTests):
+    def test_ioo_landing_page_allowed_parents(self):
+        self.assertAllowedParentPageTypes(
             IOOLandingPage,
-        },
-    )
+            {
+                IOOLandingPage,
+            },
+        )
+
+    def test_ioo_landing_page_allowed_children(self):
+        self.assertAllowedSubpageTypes(
+            IOOLandingPage,
+            {
+                IOOLandingPage,
+                IOOGuidePage,
+            },
+        )
 
 
-def test_ioo_landing_page_allowed_children(self):
-    self.assertAllowedSubpageTypes(
-        IOOLandingPage,
-        {
-            IOOLandingPage,
+class IOOGuidePageTests(WagtailPageTests):
+    def test_ioo_guide_page_allowed_parents(self):
+        self.assertAllowedParentPageTypes(
             IOOGuidePage,
-        },
-    )
+            {
+                IOOLandingPage,
+            },
+        )
 
-
-def test_ioo_guide_page_allowed_parents(self):
-    self.assertAllowedParentPageTypes(
-        IOOGuidePage,
-        {
-            IOOLandingPage,
-        },
-    )
-
-
-def test_ioo_guide_page_allowed_children(self):
-    self.assertAllowedSubpageTypes(
-        IOOGuidePage,
-        {},
-    )
+    def test_ioo_guide_page_allowed_children(self):
+        self.assertAllowedSubpageTypes(
+            IOOGuidePage,
+            {},
+        )
