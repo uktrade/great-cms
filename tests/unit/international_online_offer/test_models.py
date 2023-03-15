0ade488cf3098c35d0760a9d6f27d2a73e42b832
Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
2023-03-15 08:33:55 +0000

    Removing class structure for ioo models

--- tests/unit/international_online_offer/test_models.py
+++ tests/unit/international_online_offer/test_models.py
@@ -1,38 +1,36 @@
-from wagtail.tests.utils import WagtailPageTests
-
 from international_online_offer.models import IOOGuidePage, IOOLandingPage
 
 
-class IOOLandingPageTests(WagtailPageTests):
-    def test_ioo_landing_page_allowed_parents(self):
-        self.assertAllowedParentPageTypes(
+def test_ioo_landing_page_allowed_parents(self):
+    self.assertAllowedParentPageTypes(
+        IOOLandingPage,
+        {
             IOOLandingPage,
-            {
-                IOOLandingPage,
-            },
-        )
+        },
+    )
 
-    def test_ioo_landing_page_allowed_children(self):
-        self.assertAllowedSubpageTypes(
+
+def test_ioo_landing_page_allowed_children(self):
+    self.assertAllowedSubpageTypes(
+        IOOLandingPage,
+        {
             IOOLandingPage,
-            {
-                IOOLandingPage,
-                IOOGuidePage,
-            },
-        )
-
-
-class IOOGuidePageTests(WagtailPageTests):
-    def test_ioo_guide_page_allowed_parents(self):
-        self.assertAllowedParentPageTypes(
             IOOGuidePage,
-            {
-                IOOLandingPage,
-            },
-        )
+        },
+    )
 
-    def test_ioo_guide_page_allowed_children(self):
-        self.assertAllowedSubpageTypes(
-            IOOGuidePage,
-            {},
-        )
+
+def test_ioo_guide_page_allowed_parents(self):
+    self.assertAllowedParentPageTypes(
+        IOOGuidePage,
+        {
+            IOOLandingPage,
+        },
+    )
+
+
+def test_ioo_guide_page_allowed_children(self):
+    self.assertAllowedSubpageTypes(
+        IOOGuidePage,
+        {},
+    )
