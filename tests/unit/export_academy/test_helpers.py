8a653d000b51245d05f5137463be317dc0f3db54
Tim Patterson <timpatterson@Tim-Pattersons-MacBook-Pro.local>
2023-03-17 08:47:49 +0000

    unit test for upcoming event booking

--- tests/unit/export_academy/test_helpers.py
+++ tests/unit/export_academy/test_helpers.py
@@ -1,8 +1,28 @@
+from datetime import datetime, timedelta
+
 import pytest
 from django.contrib.auth.models import AnonymousUser
 
-from export_academy.helpers import is_export_academy_registered
+from export_academy.helpers import EventButtonHelper, is_export_academy_registered
 from tests.unit.export_academy import factories
+
+
+@pytest.mark.django_db
+def book_button_shown_for_upcoming_event(user):
+    now = datetime.now()
+    factories.RegistrationFactory(email=user.email)
+    event = factories.EventFactory(start_date=now + timedelta(hours=6), end_date=now + timedelta(hours=7))
+
+    buttons = EventButtonHelper().get_buttons_for_obj(user, event)
+
+    assert buttons['form_event_booking_buttons'] is [
+        {
+            'label': 'Book',
+            'classname': 'link',
+            'value': 'Confirmed',
+            'type': 'submit',
+        },
+    ]
 
 
 @pytest.mark.django_db
