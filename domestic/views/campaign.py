f716cd95353e74f20bceedf49f566755a8b2a96b
LuptonM <marc.lupton100@gmail.com>
2023-03-19 10:14:37 +0000

    saving progress

--- domestic/views/campaign.py
+++ domestic/views/campaign.py
@@ -5,28 +5,28 @@ from contact.views import BaseNotifyFormView
 from core.datastructures import NotifySettings
 from domestic.forms import CampaignLongForm, CampaignShortForm
 from domestic.models import ArticlePage
-
-
+from contact import constants, forms as contact_forms, helpers
+from django.shortcuts import render
 class CampaignView(BaseNotifyFormView):
-   
+    
+
+    def setup(self, request, *args, **kwargs):
+        page_slug =  kwargs['page_slug']
+        self.success_url = reverse_lazy('domestic:campaigns', kwargs = {'page_slug': page_slug})
+        return super().setup(request, *args, **kwargs)
+
     template_name = 'domestic/article_page.html'
-    success_url = reverse_lazy('contact:contact-free-trade-agreements-success')
     notify_settings = NotifySettings(
-        user_template=settings.SUBSCRIBE_TO_FTA_UPDATES_NOTIFY_TEMPLATE_ID,
+        agent_template=settings.UKEF_CONTACT_AGENT_NOTIFY_TEMPLATE_ID,
+        agent_email=settings.UKEF_CONTACT_AGENT_EMAIL_ADDRESS,
+        user_template=settings.UKEF_CONTACT_USER_NOTIFY_TEMPLATE_ID,
     )
- 
+        
     def get_current_page(self):
         try:
             return ArticlePage.objects.live().get(slug=self.kwargs['page_slug'])
         except ObjectDoesNotExist:
             return None
-           
-    def get_sectors(self):
-        current_page = self.get_current_page()
-        if current_page:
-            return ArticlePage.get_sector_choices(current_page, self.request)
-        else:
-            return []
 
     def get_form_value(self, ArticlePage):
         values = [block.value for block in ArticlePage.article_body if block.block_type == 'form']
@@ -46,18 +46,7 @@ class CampaignView(BaseNotifyFormView):
         else:
             return None 
 
-    def get_form_context(self):
-        page = self.get_current_page()
-        if page:
-            form_type = self.get_form_value(page)
-            if form_type == 'Short':
-                return CampaignShortForm()
-            elif form_type == 'Long':
-                sector_choices = self.get_sectors()
-                return CampaignLongForm(sector_choices)       
-        return None
-
+        
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs, page=self.get_current_page() if self.get_current_page() else None)
-        # context['form'] = self.get_form_context()   
-        return context
+        return super().get_context_data(**kwargs, page=self.get_current_page() if self.get_current_page() else None)
+    
