bdf9889a326329c888c61fe01724bb8d07125e5b
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2023-03-29 13:50:00 +0100

    Feature/ioo 422 login (#2073)
    * IOO directory SSO integration for sign up and sign in. Use of sessions for storing form data and move away from local storage
    
    ---------
    
    Co-authored-by: Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
    Co-authored-by: Stuart Mindt <stuartmindt@stuartmindtsmbp.lan>

--- tests/unit/international_online_offer/test_views.py
+++ tests/unit/international_online_offer/test_views.py
@@ -19,11 +19,28 @@ def test_ioo_sector(client, settings):
 
 
 @pytest.mark.django_db
+def test_triage_sector_session(client, settings):
+    settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
+    url = reverse('international_online_offer:sector')
+    client.post(url, {'sector': 'Food and Drink'})
+    assert client.session['sector'] == 'Food and Drink'
+
+
+@pytest.mark.django_db
 def test_ioo_intent(client, settings):
     settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
     url = reverse('international_online_offer:intent')
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_triage_intent_session(client, settings):
+    settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
+    url = reverse('international_online_offer:intent')
+    client.post(url, {'intent': ['Set up new premises']})
+    assert client.session['intent'] == ['Set up new premises']
+    assert client.session['intent_other'] == ''
 
 
 @pytest.mark.django_db
@@ -35,6 +52,15 @@ def test_ioo_location(client, settings):
 
 
 @pytest.mark.django_db
+def test_triage_location_session(client, settings):
+    settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
+    url = reverse('international_online_offer:location')
+    client.post(url, {'location': 'Wales'})
+    assert client.session['location'] == 'Wales'
+    assert client.session['location_none'] is False
+
+
+@pytest.mark.django_db
 def test_ioo_hiring(client, settings):
     settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
     url = reverse('international_online_offer:hiring')
@@ -43,11 +69,28 @@ def test_ioo_hiring(client, settings):
 
 
 @pytest.mark.django_db
+def test_triage_hiring_session(client, settings):
+    settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
+    url = reverse('international_online_offer:hiring')
+    client.post(url, {'hiring': '11-50'})
+    assert client.session['hiring'] == '11-50'
+
+
+@pytest.mark.django_db
 def test_ioo_spend(client, settings):
     settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
     url = reverse('international_online_offer:spend')
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_triage_spend_session(client, settings):
+    settings.FEATURE_INTERNATIONAL_ONLINE_OFFER = True
+    url = reverse('international_online_offer:spend')
+    client.post(url, {'spend': '2000001-5000000'})
+    assert client.session['spend'] == '2000001-5000000'
+    assert client.session['spend_other'] is None
 
 
 @pytest.mark.django_db
