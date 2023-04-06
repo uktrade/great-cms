3d3feab05b8c37ce4e7f75d1492d0ea998340536
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2023-04-06 09:49:20 +0100

    Feature/ioo 458 save triage and contact data to db (#2076)
    * IOO Login / sign up, saving triage and contact to session or db if logged in and initial filtering by tag for articles on dashboard
    
    ---------
    
    Co-authored-by: Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
    Co-authored-by: Stuart Mindt <stuartmindt@stuartmindtsmbp.lan>

--- international_online_offer/models.py
+++ international_online_offer/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.models import ParentalKey
@@ -12,6 +13,75 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from core.blocks import ColumnsBlock
 from core.models import CMSGenericPage
 from domestic.models import BaseContentPage
+from international_online_offer.core import helpers
+
+
+def get_triage_data(hashed_uuid):
+    try:
+        return TriageData.objects.get(hashed_uuid=hashed_uuid)
+    except TriageData.DoesNotExist:
+        return None
+
+
+def get_user_data(hashed_uuid):
+    try:
+        return UserData.objects.get(hashed_uuid=hashed_uuid)
+    except UserData.DoesNotExist:
+        return None
+
+
+def get_triage_data_from_db_or_session(request):
+    if hasattr(request, 'user') and request.user.is_authenticated:
+        triage_data = get_triage_data(request.user.hashed_uuid)
+        if triage_data:
+            return triage_data
+    if hasattr(request, 'session'):
+        return type(
+            'obj',
+            (object,),
+            {
+                'sector': request.session.get('sector') if request.session.get('sector') else '',
+                'intent': request.session.get('intent') if request.session.get('intent') else [],
+                'intent_other': request.session.get('intent_other') if request.session.get('intent_other') else '',
+                'location': request.session.get('location') if request.session.get('location') else '',
+                'location_none': request.session.get('location_none') if request.session.get('location_none') else '',
+                'hiring': request.session.get('hiring') if request.session.get('hiring') else '',
+                'spend': request.session.get('spend') if request.session.get('spend') else '',
+                'spend_other': request.session.get('spend_other') if request.session.get('spend_other') else '',
+            },
+        )
+
+
+def get_user_data_from_db_or_session(request):
+    if hasattr(request, 'user') and request.user.is_authenticated:
+        user_data = get_user_data(request.user.hashed_uuid)
+        if user_data:
+            return user_data
+
+    if hasattr(request, 'session'):
+        return type(
+            'obj',
+            (object,),
+            {
+                'company_name': request.session.get('company_name') if request.session.get('company_name') else '',
+                'company_location': request.session.get('company_location')
+                if request.session.get('company_location')
+                else '',
+                'full_name': request.session.get('full_name') if request.session.get('full_name') else '',
+                'role': request.session.get('role') if request.session.get('role') else '',
+                'email': request.session.get('email' if request.session.get('email') else ''),
+                'telephone_number': request.session.get('telephone_number')
+                if request.session.get('telephone_number')
+                else '',
+                'agree_terms': request.session.get('agree_terms') if request.session.get('agree_terms') else False,
+                'agree_info_email': request.session.get('agree_info_email')
+                if request.session.get('agree_info_email')
+                else False,
+                'agree_info_telephone': request.session.get('agree_info_telephone')
+                if request.session.get('agree_info_telephone')
+                else False,
+            },
+        )
 
 
 class IOOIndexPage(BaseContentPage):
@@ -38,12 +108,22 @@ class IOOGuidePage(BaseContentPage):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
+        triage_data = get_triage_data_from_db_or_session(request)
+        user_data = get_user_data_from_db_or_session(request)
+        get_to_know_market_articles = []
+        if triage_data:
+            get_to_know_market_articles = helpers.find_get_to_know_market_articles(
+                self.get_children().live(), triage_data.sector, triage_data.intent
+            )
         context.update(
             complete_contact_form_message=self.LOW_VALUE_INVESTOR_CONTACT_FORM_MESSAGE,
             complete_contact_form_link='international_online_offer:contact',
             complete_contact_form_link_text='Complete form',
             contact_form_success_message=self.CONTACT_FORM_SUCCESS_MESSAGE,
             submit_contact_details_success=request.GET.get('success'),
+            triage_data=triage_data,
+            user_data=user_data,
+            get_to_know_market_articles=get_to_know_market_articles,
         )
         return context
 
@@ -116,3 +196,128 @@ class IOOArticlePage(BaseContentPage):
         StreamFieldPanel('article_body'),
         FieldPanel('tags'),
     ]
+
+
+class TriageData(models.Model):
+    hashed_uuid = models.CharField(max_length=200)
+
+    SECTOR_CHOICES = (
+        ('Advanced engineering', 'Advanced engineering'),
+        ('Aerospace', 'Aerospace'),
+        ('Agriculture, Horticulture, Fisheries and pets', 'Agriculture, Horticulture, Fisheries and pets'),
+        ('Airports', 'Airports'),
+        ('Automotive', 'Automotive'),
+        ('Biotech and Pharmaceuticals', 'Biotech and Pharmaceuticals'),
+        ('Business and consumer services', 'Business and consumer services'),
+        ('Chemicals', 'Chemicals'),
+        ('Construction', 'Construction'),
+        ('Consumer and retail', 'Consumer and retail'),
+        ('Creative industries', 'Creative industries'),
+        ('Defense and Security', 'Defense and Security'),
+        ('Education and Training', 'Education and Training'),
+        ('Energy', 'Energy'),
+        ('Environment', 'Environment'),
+        ('Financial and Professional Services', 'Financial and Professional Services'),
+        ('Food and Drink', 'Food and Drink'),
+        ('Healthcare and Medical', 'Healthcare and Medical'),
+        ('Infrastructure Air and Sea', 'Infrastructure Air and Sea'),
+        ('Leisure', 'Leisure'),
+        ('Logistics', 'Logistics'),
+        ('Manufacturing', 'Manufacturing'),
+        ('Marine', 'Marine'),
+        ('Maritime Services', 'Maritime Services'),
+        ('Medical devices and equipment', 'Medical devices and equipment'),
+        ('Mining', 'Mining'),
+        ('Nuclear', 'Nuclear'),
+        ('Oil and Gas', 'Oil and Gas'),
+        ('Rail', 'Rail'),
+        ('Renewable', 'Renewable'),
+        ('Retail', 'Retail'),
+        ('Security', 'Security'),
+        ('Space', 'Space'),
+        ('Sports Events', 'Sports Events'),
+        ('Technology and Smart Cities', 'Technology and Smart Cities'),
+    )
+
+    sector = models.CharField(max_length=255, choices=SECTOR_CHOICES)
+
+    INTENT_CHOICES = (
+        ('Set up new premises', 'Set up new premises'),
+        ('Set up a new distribution centre', 'Set up a new distribution centre'),
+        ('Onward sales and exports from the UK', 'Onward sales and exports from the UK'),
+        ('Research, develop and collaborate', 'Research, develop and collaborate'),
+        ('Find people with specialist skills', 'Find people with specialist skills'),
+        ('Other', 'Other'),
+    )
+
+    intent = ArrayField(
+        ArrayField(
+            models.CharField(max_length=255, choices=INTENT_CHOICES),
+            size=6,
+        ),
+        size=1,
+        default=list,
+    )
+    intent_other = models.CharField(max_length=255)
+
+    LOCATION_CHOICES = (
+        ('East', 'East'),
+        ('East Midlands', 'East Midlands'),
+        ('London', 'London'),
+        ('North East', 'North East'),
+        ('North West', 'North West'),
+        ('Northern Ireland', 'Northern Ireland'),
+        ('Scotland', 'Scotland'),
+        ('South East', 'South East'),
+        ('South West', 'South West'),
+        ('Wales', 'Wales'),
+        ('West Midlands', 'West Midlands'),
+        ('Yorkshire and the Humber', 'Yorkshire and the Humber'),
+    )
+
+    location = models.CharField(max_length=255, choices=LOCATION_CHOICES)
+    location_none = models.BooleanField(default=False)
+
+    HIRING_CHOICES = (
+        ('1-10', '1 to 10'),
+        ('11-50', '11 to 50'),
+        ('51-100', '51 to 100'),
+        ('101+', 'More than 100'),
+        ('No plans to hire yet', 'No plans to hire yet'),
+    )
+
+    hiring = models.CharField(max_length=255, choices=HIRING_CHOICES)
+
+    SPEND_CHOICES = (
+        ('10000-500000', '£10,000 - £500,000'),
+        ('500001-1000000', '£500,000 - £1,000,000'),
+        ('1000001-2000000', '£1,000,001 - £2,000,000'),
+        ('2000001-5000000', '£2,000,001 - £5,000,000'),
+        ('5000001-10000000', '£5,000,001 - £10,000,000'),
+        ('10000000+', 'More than £10 million'),
+        ('Specific amount', 'Specific amount'),
+    )
+
+    spend = models.CharField(max_length=255, choices=SPEND_CHOICES)
+    spend_other = models.CharField(max_length=255, null=True)
+
+
+class UserData(models.Model):
+    hashed_uuid = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=255)
+    LOCATION_CHOICES = (
+        ('France', 'France'),
+        ('Germany', 'Germany'),
+        ('India', 'India'),
+        ('Italy', 'Italy'),
+        ('Spain', 'Spain'),
+        ('United States', 'United States'),
+    )
+    company_location = models.CharField(max_length=255, choices=LOCATION_CHOICES)
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    telephone_number = models.CharField(max_length=255)
+    agree_terms = models.BooleanField(default=False)
+    agree_info_email = models.BooleanField(default=False)
+    agree_info_telephone = models.BooleanField(default=False)
