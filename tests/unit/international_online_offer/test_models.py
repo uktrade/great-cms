18dca2aabffc4df8935e7cce4fa94b784bd0549e
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2023-04-06 09:49:20 +0100

    Feature/ioo 458 save triage and contact data to db (#2076)
    * IOO Login / sign up, saving triage and contact to session or db if logged in and initial filtering by tag for articles on dashboard
    
    ---------
    
    Co-authored-by: Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>
    Co-authored-by: Stuart Mindt <stuartmindt@stuartmindtsmbp.lan>

--- tests/unit/international_online_offer/test_models.py
+++ tests/unit/international_online_offer/test_models.py
@@ -1,8 +1,19 @@
 import pytest
+from django.contrib.sessions.middleware import SessionMiddleware
 from wagtail.tests.utils import WagtailPageTests
 
 from domestic.models import StructuralPage
-from international_online_offer.models import IOOArticlePage, IOOGuidePage, IOOIndexPage
+from international_online_offer.models import (
+    IOOArticlePage,
+    IOOGuidePage,
+    IOOIndexPage,
+    TriageData,
+    UserData,
+    get_triage_data,
+    get_triage_data_from_db_or_session,
+    get_user_data,
+    get_user_data_from_db_or_session,
+)
 
 
 class IOOIndexPageTests(WagtailPageTests):
@@ -49,6 +60,105 @@ def test_ioo_guide_page_content(rf):
     assert context['complete_contact_form_link_text'] == 'Complete form'
     assert context['contact_form_success_message'] == IOOGuidePage.CONTACT_FORM_SUCCESS_MESSAGE
     assert context['complete_contact_form_link'] == 'international_online_offer:contact'
+    assert context['get_to_know_market_articles'] == []
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_triage_data_none(rf):
+    data = get_triage_data('testId')
+    assert data is None
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_triage_data(rf):
+    TriageData.objects.update_or_create(
+        hashed_uuid='testId',
+        defaults={'spend': 'spend', 'spend_other': 'spend_other'},
+    )
+    data = get_triage_data('testId')
+    assert data is not None
+    assert data.hashed_uuid == 'testId'
+    assert data.spend == 'spend'
+    assert data.spend_other == 'spend_other'
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_user_data_none(rf):
+    data = get_user_data('testId')
+    assert data is None
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_user_data(rf):
+    UserData.objects.update_or_create(
+        hashed_uuid='testId',
+        defaults={'full_name': 'Joe', 'company_name': 'DBT'},
+    )
+    data = get_user_data('testId')
+    assert data is not None
+    assert data.hashed_uuid == 'testId'
+    assert data.full_name == 'Joe'
+    assert data.company_name == 'DBT'
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_triage_from_db_not_session(rf, user):
+    TriageData.objects.update_or_create(
+        hashed_uuid='123',
+        defaults={'sector': 'sector'},
+    )
+    guide_page = IOOGuidePage(title='Guide')
+    request = rf.get(guide_page.url)
+    request.user = user
+    request.user.hashed_uuid = '123'
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    request.session.save()
+    triage_data = get_triage_data_from_db_or_session(request)
+    assert triage_data is not None
+    assert triage_data.sector == 'sector'
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_triage_from_session_not_db(rf):
+    guide_page = IOOGuidePage(title='Guide')
+    request = rf.get(guide_page.url)
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    request.session.save()
+    request.session['sector'] = 'sector'
+    triage_data_session = get_triage_data_from_db_or_session(request)
+    assert triage_data_session is not None
+    assert triage_data_session.sector == 'sector'
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_user_from_session_not_db(rf):
+    guide_page = IOOGuidePage(title='Guide')
+    request = rf.get(guide_page.url)
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    request.session.save()
+    request.session['full_name'] = 'full_name'
+    user_data_session = get_user_data_from_db_or_session(request)
+    assert user_data_session is not None
+    assert user_data_session.full_name == 'full_name'
+
+
+@pytest.mark.django_db
+def test_ioo_guide_get_user_from_db_not_session(rf, user):
+    UserData.objects.update_or_create(
+        hashed_uuid='123',
+        defaults={'full_name': 'Joe', 'company_name': 'DBT'},
+    )
+    guide_page = IOOGuidePage(title='Guide')
+    request = rf.get(guide_page.url)
+    request.user = user
+    request.user.hashed_uuid = '123'
+    user_data = get_user_data_from_db_or_session(request)
+    assert user_data is not None
+    assert user_data.full_name == 'Joe'
+    assert user_data.company_name == 'DBT'
 
 
 class IOOArticlePageTests(WagtailPageTests):
