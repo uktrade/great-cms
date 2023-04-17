9d3b661cd7bf729010f66db74588a907eee5b4af
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2023-04-17 13:26:05 +0100

    Added support and incentives as well as opportunities section to dashboard (#2105)
    * Added support and opportunies sections to dashboard
    
    ---------
    
    Co-authored-by: Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>

--- international_online_offer/core/helpers.py
+++ international_online_offer/core/helpers.py
@@ -1,6 +1,15 @@
+def concat_filters(*filters):
+    filters_out = []
+    for filter in filters:
+        if type(filter) is str:
+            filter = [filter]
+        filters_out = filters_out + filter
+    return filters_out
+
+
 def find_get_to_know_market_articles(articles, sector_filter, intent_filters):
-    filtered_pages = []
     filters = concat_filters(sector_filter, intent_filters)
+    filtered_pages = []
     for page in articles:
         all_tags = page.specific.tags.all() if hasattr(page.specific.tags, 'all') else page.specific.tags
         tag_match_count = 0
@@ -15,6 +24,35 @@ def find_get_to_know_market_articles(articles, sector_filter, intent_filters):
     return filtered_pages
 
 
-def concat_filters(sector_filter, intent_filters):
-    sector_filter = [sector_filter]
-    return sector_filter + intent_filters
+def find_get_support_and_incentives_articles(articles):
+    filters = ['Support and Incentives']
+    filtered_pages = []
+    for page in articles:
+        all_tags = page.specific.tags.all() if hasattr(page.specific.tags, 'all') else page.specific.tags
+        tag_match_count = 0
+        for tag in all_tags:
+            for filter in filters:
+                if tag.name == filter:
+                    tag_match_count += 1
+
+        if len(all_tags) == tag_match_count and tag_match_count > 0:
+            filtered_pages.append(page.specific)
+
+    return filtered_pages
+
+
+def find_opportunities_articles(articles, sector_filter):
+    filters = concat_filters(sector_filter, ['Opportunity'])
+    filtered_pages = []
+    for page in articles:
+        all_tags = page.specific.tags.all() if hasattr(page.specific.tags, 'all') else page.specific.tags
+        tag_match_count = 0
+        for tag in all_tags:
+            for filter in filters:
+                if tag.name == filter:
+                    tag_match_count += 1
+
+        if len(all_tags) == tag_match_count and len(all_tags) == len(filters) and tag_match_count > 0:
+            filtered_pages.append(page.specific)
+
+    return filtered_pages
--- tests/unit/international_online_offer/test_helpers.py
+++ tests/unit/international_online_offer/test_helpers.py
@@ -1,7 +1,7 @@
 from international_online_offer.core import helpers
 
 
-def test_find_get_to_know_market_articles():
+def test_find_articles_based_on_tags():
     tag = type(
         'obj',
         (object,),
@@ -12,11 +12,37 @@ def test_find_get_to_know_market_articles():
         (object,),
         {'name': 'tag2'},
     )
+
+    tag3 = type(
+        'obj',
+        (object,),
+        {'name': 'Support and Incentives'},
+    )
+
+    tag4 = type(
+        'obj',
+        (object,),
+        {'name': 'Opportunity'},
+    )
+
     specific = type(
         'obj',
         (object,),
         {'tags': [tag, tag2]},
     )
+
+    specific2 = type(
+        'obj',
+        (object,),
+        {'tags': [tag3]},
+    )
+
+    specific3 = type(
+        'obj',
+        (object,),
+        {'tags': [tag, tag4]},
+    )
+
     article = type(
         'obj',
         (object,),
@@ -29,11 +55,30 @@ def test_find_get_to_know_market_articles():
         {'specific': specific},
     )
 
+    article3 = type(
+        'obj',
+        (object,),
+        {'specific': specific2},
+    )
+
+    article4 = type(
+        'obj',
+        (object,),
+        {'specific': specific3},
+    )
+
     articles = []
     articles.append(article)
     articles.append(article2)
+    articles.append(article3)
+    articles.append(article4)
+
     assert len(helpers.find_get_to_know_market_articles(articles, 'tag1', ['tag2'])) == 2
+    assert len(helpers.find_get_support_and_incentives_articles(articles)) == 1
+    assert len(helpers.find_opportunities_articles(articles, 'tag1')) == 1
     assert helpers.find_get_to_know_market_articles([], '', []) == []
+    assert helpers.find_get_support_and_incentives_articles([]) == []
+    assert helpers.find_opportunities_articles([], '') == []
 
 
 def test_concat_filters():
