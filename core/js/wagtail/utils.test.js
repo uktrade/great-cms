9b7f67f81ab8f9cca2d7400b3bebc6a08c201af6
Lewis Coulson <lewiscoulson@Lewis-Coulsons-MacBook-Pro.local>
2023-04-18 12:42:08 +0100

    KLS-514: Status messaging

--- core/js/wagtail/utils.test.js
+++ core/js/wagtail/utils.test.js
@@ -1,8 +1,13 @@
-import { isAddVideoPage } from './utils';
+import { isAddVideoPage, isEditVideoPage } from './utils'
 
 describe('Wagtail utils', () => {
-    it('isAddVideoPage()', () => {
-      expect(isAddVideoPage("/admin/media/video/add/")).toEqual(true);
-      expect(isAddVideoPage("/admin/media/image/add/")).toEqual(false);
-    })
-});
+  it('isAddVideoPage()', () => {
+    expect(isAddVideoPage('/admin/media/video/add/')).toEqual(true)
+    expect(isAddVideoPage('/admin/media/image/add/')).toEqual(false)
+  })
+
+  it('isEditVideoPage()', () => {
+    expect(isEditVideoPage('/admin/media/edit/101/')).toEqual(true)
+    expect(isEditVideoPage('/admin/media/something-else/101/')).toEqual(false)
+  })
+})
