89502e2536a3943c3e39f150120f8767887816a9
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2023-05-05 10:57:56 +0100

    Login / Signup flow changes, navbar updates and article locking added (#2138)
    * Login / Signup flow changes, navbar updates and article locking added
    
    ---------
    
    Co-authored-by: Stuart Mindt <stuartmindt@Stuart-Mindts-MacBook-Pro.local>

---
+++ international_online_offer/core/constants.py
@@ -0,0 +1,3 @@
+LOW_VALUE_INVESTOR_SIGNUP_MESSAGE = 'Sign up to keep up to date with our personalised service.'
+HIGH_VALUE_INVESTOR_SIGNUP_MESSAGE = """Your business qualifies for 1 to 1 support from specialist UK
+        government advisors. Sign up to access this and your full personalised online guides."""
