149d306299a1e2008d23a1a937abad0cf8bb7426
LuptonM <marc.lupton100@gmail.com>
2023-05-22 14:42:51 +0100

    update tests

--- tests/unit/core/test_models.py
+++ tests/unit/core/test_models.py
@@ -876,8 +876,8 @@ class MicrositePageTests(SetUpLocaleMixin, WagtailPageTests):
         root = MicrositeFactory(title='root')
         home = MicrositePageFactory(page_title='home', title='microsite-title', parent=root, use_domestic_logo=True)
         home_child = MicrositePageFactory(page_title='home-child', title='home-child', parent=home)
-        self.assertEqual(home_child.use_domestic_logo(), True)
+        self.assertEqual(home_child.get_use_domestic_logo(), True)
 
         home = DetailPageFactory()
         home_child = MicrositePageFactory(page_title='home-child', title='home-child', parent=home)
-        self.assertEqual(home_child.use_domestic_logo(), True)
+        self.assertEqual(home_child.get_use_domestic_logo(), True)
