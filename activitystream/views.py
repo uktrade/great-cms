403bd803e1f05a730acb45ac620584a4b8b601ee
Rafa Garcia <rafa@rafagarcia.info>
2023-06-19 12:59:04 +0100

    Enable endpoint authentication

--- activitystream/views.py
+++ activitystream/views.py
@@ -156,11 +156,11 @@ class TestSearchAPIView(TemplateView):
 
 
 class ActivityStreamBaseView(ListAPIView):
-    # authentication_classes = (ActivityStreamAuthentication,)
+    authentication_classes = (ActivityStreamAuthentication,)
     permission_classes = ()
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
-    # @decorator_from_middleware(ActivityStreamHawkResponseMiddleware)
+    @decorator_from_middleware(ActivityStreamHawkResponseMiddleware)
     def list(self, request, *args, **kwargs):
         """A single page of activities to be consumed by activity stream."""
         return super().list(request, *args, **kwargs)
