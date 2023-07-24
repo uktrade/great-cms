dd0c1b324c6ab81b6975e9ce4446eedcbc4b0dec
Stuart Mindt <stuart.mindt@madetech.com>
2023-07-24 08:30:50 +0100

    Moving choices to for feedback form to choices.py

--- international_online_offer/forms.py
+++ international_online_offer/forms.py
@@ -184,13 +184,7 @@ class LocationSelectForm(forms.Form):
 class FeedbackForm(forms.Form):
     satisfaction = ChoiceField(
         label='1. Overall, how do you feel about your use of the expand your business in the UK service today?',
-        choices=(
-            ('VERY_SATISFIED', 'Very satisfied'),
-            ('SATISFIED', 'Satisfied'),
-            ('NEITHER', 'Neither satisfied nor dissatisfied'),
-            ('DISSATISFIED', 'Dissatisfied'),
-            ('VERY_DISSATISFIED', 'Very dissatisfied'),
-        ),
+        choices=choices.SATISFACTION_CHOICES,
         widget=RadioSelect(attrs={'class': 'govuk-radios__input'}),
         error_messages={
             'required': 'You must select a level of satisfaction',
@@ -199,13 +193,7 @@ class FeedbackForm(forms.Form):
     experience = MultipleChoiceField(
         label='2. Did you experience any of the following issues?',
         help_text='Tick all that apply.',
-        choices=(
-            ('I_DID_NOT_EXPERIENCE_ANY_ISSUE', 'I did not experience any issue'),
-            ('PROCESS_IS_NOT_CLEAR', 'Process is not clear'),
-            ('NOT_ENOUGH_GUIDANCE', 'Not enough guidance'),
-            ('I_WAS_ASKED_FOR_INFORMATION_I_DID_NOT_HAVE', 'I was asked for information I did not have'),
-            ('I_DID_NOT_GET_THE_INFORMATION_I_EXPECTED', 'I did not get the information I expected'),
-        ),
+        choices=choices.EXPERIENCE_CHOICES,
         widget=CheckboxSelectMultiple(attrs={'class': 'govuk-checkboxes__input'}),
         error_messages={
             'required': 'You must select one or more issues',
@@ -220,14 +208,7 @@ class FeedbackForm(forms.Form):
     )
     likelihood_of_return = ChoiceField(
         label='4. What is the likelihood of you returning to this site?',
-        choices=(
-            ('EXTREMELY_LIKELY', 'Extremely likely'),
-            ('LIKELY', 'Likely'),
-            ('NEITHER_LIKELY_NOR_UNLIKELY', 'Neither likely nor unlikely'),
-            ('UNLIKELY', 'Unlikely'),
-            ('EXTREMELY_UNLIKELY', 'Extremely unlikely'),
-            ('DONT_KNOW_OR_PREFER_NOT_TO_SAY', "Don't know / prefer not to say"),
-        ),
+        choices=choices.LIKELIHOOD_CHOICES,
         widget=RadioSelect(attrs={'class': 'govuk-radios__input'}),
         error_messages={
             'required': 'You must select one likelihood of returning options',
@@ -236,17 +217,7 @@ class FeedbackForm(forms.Form):
     site_intentions = MultipleChoiceField(
         label='5. What will your business use this site for?',
         help_text='Tick all that apply.',
-        choices=(
-            ('DECIDE_IF_WE_SHOULD_SET_UP_IN_THE_UK', 'Decide if we should set up in the UK'),
-            ('HELP_US_SET_UP_IN_THE_UK', 'Help us set up in the UK'),
-            ('UNDERSTAND_THE_UK_LEGAL_SYSTEM', 'Understand the UK legal system such as tax and employment regulations'),
-            ('PUT_US_IN_TOUCH_WITH_EXPERTS', 'Put us in touch with experts to help us set up'),
-            ('ACCESS_TRUSTED_INFORMATION', 'Access trusted information'),
-            ('LEARN_ABOUT_AVAILABLE_INCENTIVES', 'Learn about available incentives'),
-            ('OTHER', 'Other'),
-            ('DONT_KNOW_OR_PREFER_NOT_TO_SAY', "Don't know / prefer not to say"),
-            ('MY_BUSINESS_WILL_NOT_USE_THE_SITE', 'My business will not use the site'),
-        ),
+        choices=choices.INTENSION_CHOICES,
         widget=CheckboxSelectMultiple(attrs={'class': 'govuk-checkboxes__input'}),
         error_messages={
             'required': 'You must select one or more site use options',
