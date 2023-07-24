956d00b6ac96d7d4b5035adc2fd33d5bdeef70fa
Stuart Mindt <stuart.mindt@madetech.com>
2023-07-24 14:16:45 +0100

    Rent data and view updated

--- international_online_offer/core/helpers.py
+++ international_online_offer/core/helpers.py
@@ -182,31 +182,20 @@ def get_salary_data(entry_salary, mid_salary, executive_salary):
 
 
 def get_rent_data(large_warehouse_rent, small_warehouse_rent, shopping_centre, high_street_retail, work_office):
-    large_warehouse_rent = large_warehouse_rent.get('value_converted__avg')
-    small_warehouse_rent = small_warehouse_rent.get('value_converted__avg')
-    shopping_centre = shopping_centre.get('value_converted__avg')
-    high_street_retail = high_street_retail.get('value_converted__avg')
-    work_office = work_office.get('value_converted__avg')
-
     if large_warehouse_rent:
-        large_warehouse_rent = large_warehouse_rent * 340000
-        large_warehouse_rent = int(large_warehouse_rent)
+        large_warehouse_rent = int(large_warehouse_rent.gbp_per_month) if large_warehouse_rent.gbp_per_month else None
 
     if small_warehouse_rent:
-        small_warehouse_rent = small_warehouse_rent * 5000
-        small_warehouse_rent = int(small_warehouse_rent)
+        small_warehouse_rent = int(small_warehouse_rent.gbp_per_month) if small_warehouse_rent.gbp_per_month else None
 
     if shopping_centre:
-        shopping_centre = shopping_centre * 204
-        shopping_centre = int(shopping_centre)
+        shopping_centre = int(shopping_centre.gbp_per_month) if shopping_centre.gbp_per_month else None
 
     if high_street_retail:
-        high_street_retail = high_street_retail * 204
-        high_street_retail = int(high_street_retail)
+        high_street_retail = int(high_street_retail.gbp_per_month) if high_street_retail.gbp_per_month else None
 
     if work_office:
-        work_office = work_office * 16671
-        work_office = int(work_office)
+        work_office = int(work_office.gbp_per_month) if work_office.gbp_per_month else None
 
     return large_warehouse_rent, small_warehouse_rent, shopping_centre, high_street_retail, work_office
 
