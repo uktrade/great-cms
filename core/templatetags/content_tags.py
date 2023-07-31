452fdb5a0d0efcf04b55921aeac21df0d39a58c7
LuptonM <marc.lupton100@gmail.com>
2023-07-31 17:16:43 +0100

    stop putting all embedded videos in a div

--- core/templatetags/content_tags.py
+++ core/templatetags/content_tags.py
@@ -232,7 +232,7 @@ def add_govuk_classes(value):
         ({'tag': 'input', 'class': 'form-control'}, 'govuk-form-control'),
         ({'tag': 'label', 'class': 'form-label'}, 'govuk-form-label'),
         ({'tag': 'div', 'class': 'form-group-error'}, 'govuk-form-group-error'),
-        ({'tag': 'iframe', 'wrap': True}, 'great-video-embed-16-9'),
+        ({'tag': 'iframe', 'wrap': False}, 'great-video-embed-16-9'),
     ]
     for tag_name, class_name in mapping:
         if 'wrap' in tag_name:
