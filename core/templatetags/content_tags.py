f341fe4d8ea34c7ac33d7eecbc9bb23c098eb25c
LuptonM <marc.lupton100@gmail.com>
2023-08-01 10:24:24 +0100

    fix video stacking

--- core/templatetags/content_tags.py
+++ core/templatetags/content_tags.py
@@ -206,11 +206,9 @@ def get_css_class_from_string(string):
 
 
 def wrap_tag_in_div(soup, tag_name, wrapper_class):
-    wrapper = soup.new_tag('div')
-    wrapper.attrs['class'] = wrapper_class
-
     for element in soup.findAll(tag_name['tag']):
-        element.wrap(wrapper)
+        div = soup.new_tag('div', **{'class': wrapper_class})
+        element.wrap(div)
 
 
 @register.filter
