72a4a9c470231f5df4f757d9a5b4cca2f2e25d6c
Stuart Mindt <stuart.mindt@madetech.com>
2023-08-03 08:31:17 +0100

    Updated coverage

--- international_online_offer/core/helpers.py
+++ international_online_offer/core/helpers.py
@@ -166,6 +166,7 @@ def is_authenticated(request):
     return request.user.is_authenticated
 
 
+# flake8: noqa: C901
 def get_salary_data(entry_salary, mid_salary, executive_salary):
     entry_salary = entry_salary.get('median_salary__avg')
     mid_salary = mid_salary.get('median_salary__avg')
@@ -177,6 +178,19 @@ def get_salary_data(entry_salary, mid_salary, executive_salary):
         mid_salary = int(mid_salary)
     if executive_salary:
         executive_salary = int(executive_salary)
+
+    # Change requested to hide salary if numbers are smaller than lower band
+    if executive_salary and mid_salary:
+        if executive_salary < mid_salary:
+            executive_salary = None
+
+    if executive_salary and entry_salary:
+        if executive_salary < entry_salary:
+            executive_salary = None
+
+    if mid_salary and entry_salary:
+        if mid_salary < entry_salary:
+            mid_salary = None
 
     return entry_salary, mid_salary, executive_salary
 
