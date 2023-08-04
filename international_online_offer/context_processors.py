11fcd79ceb7fa21c4666015309e655cd2c8782a2
Stuart Mindt <stuart.mindt@madetech.com>
2023-08-04 08:41:32 +0100

    Context processor update for tests

--- international_online_offer/context_processors.py
+++ international_online_offer/context_processors.py
@@ -2,11 +2,14 @@ from international_online_offer.models import UserData
 
 
 def eyb_user(request):
-    if hasattr(request, 'user'):
-        if hasattr(request.user, 'is_authenticated'):
-            if request.user.is_authenticated:
-                return {'eyb_user': UserData.objects.filter(hashed_uuid=request.user.hashed_uuid).first()}
-    return {}
+    return (
+        {'eyb_user': UserData.objects.filter(hashed_uuid=request.user.hashed_uuid).first()}
+        if hasattr(request, 'user')
+        and hasattr(request.user, 'is_authenticated')
+        and request.user.is_authenticated
+        and hasattr(request.user, 'hashed_uuid')
+        else {}
+    )
 
 
 def feedback_next_url(request):
