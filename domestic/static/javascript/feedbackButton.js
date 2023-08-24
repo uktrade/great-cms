734d095a66b8fe9ef2e67d01d9748d13395e05c1
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2023-08-24 11:37:43 +0100

    Wrap feedback js in an IIFE

--- domestic/static/javascript/feedbackButton.js
+++ domestic/static/javascript/feedbackButton.js
@@ -1,16 +1,18 @@
-const feedbackButton = document.getElementById('feedback_button')
+;(function () {
+  const feedbackButton = document.getElementById('feedback_button')
 
-const toggleButtonTextContainer = () => {
-  const text = document.getElementById('feedback-text')
-  const feedbackContainer = document.getElementById('feedback_container')
-  feedbackButton.classList.toggle('great-hidden')
-  text.classList.toggle('great-hidden')
-  feedbackContainer.classList.toggle('great-box-shadow')
-  return
-}
+  const toggleButtonTextContainer = () => {
+    const text = document.getElementById('feedback-text')
+    const feedbackContainer = document.getElementById('feedback_container')
+    feedbackButton.classList.toggle('great-hidden')
+    text.classList.toggle('great-hidden')
+    feedbackContainer.classList.toggle('great-box-shadow')
+    return
+  }
 
-feedbackButton.addEventListener('click', toggleButtonTextContainer)
+  feedbackButton.addEventListener('click', toggleButtonTextContainer)
 
-document
-  .getElementById('no_thanks')
-  .addEventListener('click', toggleButtonTextContainer)
+  document
+    .getElementById('no_thanks')
+    .addEventListener('click', toggleButtonTextContainer)
+})()
