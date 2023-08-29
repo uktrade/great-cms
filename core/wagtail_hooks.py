fffd0a7e12533187cd45c0bc5ad840113585eea9
LuptonM <marc.lupton100@gmail.com>
2023-08-29 11:20:12 +0100

    add icons

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -541,14 +541,14 @@ def register_image_chooser_viewset():
     )
     
     
-@hooks.register("register_icons")
+@hooks.register('register_icons')
 def register_icons(icons):
     return icons + [
-        'wagtailfontawesomesvg/solid/link.svg',
-        'wagtailfontawesomesvg/solid/image.svg',
-        'wagtailfontawesomesvg/solid/crosshairs.svg',
-        'wagtailfontawesomesvg/solid/gear.svg',
-        'wagtailfontawesomesvg/solid/quote-left.svg',
         'wagtailfontawesomesvg/solid/video.svg',
-        'wagtailfontawesomesvg/solid/bookmark.svg',
+        # 'static/wagtailfontawesomesvg/solid/image.svg',
+        # 'static/wagtailfontawesomesvg/solid/crosshairs.svg',
+        # 'static/wagtailfontawesomesvg/solid/gear.svg',
+        # 'static/wagtailfontawesomesvg/solid/quote-left.svg',
+        # 'static/wagtailfontawesomesvg/solid/video.svg',
+        # 'static/wagtailfontawesomesvg/solid/bookmark.svg',
     ]
