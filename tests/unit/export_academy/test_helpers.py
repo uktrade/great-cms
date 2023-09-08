26f75ace90404327527d1bf14fab62c588cd97ba
LuptonM <marc.lupton100@gmail.com>
2023-09-08 13:24:15 +0100

    align with warning

--- tests/unit/export_academy/test_helpers.py
+++ tests/unit/export_academy/test_helpers.py
@@ -41,7 +41,7 @@ def test_book_button_returned_for_upcoming_event_registered_user(user, test_futu
 
     assert buttons['form_event_booking_buttons'] == [
         {
-            'label': 'Book<span class="great-visually-hidden"> Test event name</span>',
+            'label': 'Book event<span class="great-visually-hidden"> Test event name</span>',
             'classname': 'govuk-button govuk-!-margin-bottom-0 ukea-ga-tracking',
             'value': 'Confirmed',
             'type': 'submit',
