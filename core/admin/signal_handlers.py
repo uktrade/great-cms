80ce21cdc2b3ccc80b0172edc56c487d00ecee98
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-09-28 12:58:06 +0100

    create email backend for moderation emails

---
+++ core/admin/signal_handlers.py
@@ -0,0 +1,29 @@
+# <project>/signal_handlers.py
+
+from wagtail.admin.mail import (
+    GroupApprovalTaskStateSubmissionEmailNotifier as dis,
+    WorkflowStateSubmissionEmailNotifier as dis2,
+)
+from wagtail.models import TaskState, WorkflowState
+from wagtail.signals import task_submitted, workflow_submitted
+
+from .mail import GroupApprovalTaskStateSubmissionEmailNotifier
+
+
+def register_signal_handlers():
+    task_submission_email_notifier = dis()
+    workflow_submission_email_notifier = dis2()
+    task_submission_email_notifier = GroupApprovalTaskStateSubmissionEmailNotifier((TaskState, WorkflowState))
+    task_submitted.disconnect(
+        task_submission_email_notifier,
+        sender=TaskState,
+        dispatch_uid="group_approval_task_submitted_email_notification",
+    )
+    workflow_submitted.disconnect(
+        workflow_submission_email_notifier,
+        sender=WorkflowState,
+        dispatch_uid="workflow_state_submitted_email_notification",
+    )
+    task_submitted.connect(
+        task_submission_email_notifier, dispatch_uid='user_approval_task_submitted_email_notification'
+    )
