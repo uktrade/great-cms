969bdc8e43883b3b8d408e55567bb98a54e35be7
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-10-04 12:05:01 +0100

    remove core handler

--- core/apps.py
+++ core/apps.py
@@ -6,7 +6,5 @@ class CoreConfig(AppConfig):
 
     def ready(self) -> None:
         import core.wagtail_documents_patch  # noqa
-        from core.admin.signal_handlers import register_signal_handlers
 
-        register_signal_handlers()
         return super().ready()
