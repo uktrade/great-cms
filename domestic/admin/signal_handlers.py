b2d9da476dea78dcfd539959cfeaf2bb9e6808b2
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-10-05 18:19:33 +0100

    tidy up

--- domestic/admin/signal_handlers.py
+++ domestic/admin/signal_handlers.py
@@ -1,9 +1,12 @@
-from domestic.admin.mail import ModerationTaskStateSubmissionEmailNotifier
 from wagtail.models import TaskState, WorkflowState
 from wagtail.signals import task_submitted
 
-my_notifier = ModerationTaskStateSubmissionEmailNotifier()
+from domestic.admin.mail import ModerationTaskStateSubmissionEmailNotifier
+
+moderation_task_state_submission_email_notifier = ModerationTaskStateSubmissionEmailNotifier()
 
 
 def register_signal_handlers():
-    task_submitted.connect(receiver=my_notifier, sender=TaskState, dispatch_uid='my_unique_id')
+    task_submitted.connect(
+        receiver=moderation_task_state_submission_email_notifier, sender=TaskState, dispatch_uid='my_receiver_id'
+    )
