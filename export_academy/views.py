e7ce2525c734f7f4c0bc0ce2f375a6940fd98604
LuptonM <marc.lupton100@gmail.com>
2023-10-20 09:18:31 +0100

    fix get thumbnail

--- export_academy/views.py
+++ export_academy/views.py
@@ -799,5 +799,4 @@ class EventVideoOnDemandView(DetailView):
         ctx['series'] = event.get_course()[0] if len(event.get_course()) else None
         ctx['slug'] = kwargs['object'].slug
         ctx['video_page_slug'] = event.get_past_event_recording_slug()
-        ctx['event_video']['video'].thumbnail
         return ctx
