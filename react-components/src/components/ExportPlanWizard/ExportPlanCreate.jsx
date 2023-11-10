d63ea7476646a4e57ae0f1cbb97d6bfd463191a5
LuptonM <marc.lupton100@gmail.com>
2023-11-10 11:43:32 +0000

    save changes

--- react-components/src/components/ExportPlanWizard/ExportPlanCreate.jsx
+++ react-components/src/components/ExportPlanWizard/ExportPlanCreate.jsx
@@ -65,7 +65,7 @@ export function ExportPlanWizard({ exportPlan }) {
               href={config.exportPlanBaseUrl}
               className="back-link h-m body-m-b m-t-xs"
             >
-              <span className="great-icon fas fa-arrow-circle-left" />
+              <span role='img' className="great-icon fas fa-arrow-circle-left" />
               <span className="m-f-xs">Back</span>
             </a>
           </div>
