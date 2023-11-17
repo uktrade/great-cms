44326b670784cd7cee9a004fffbfdd46525cfbb5
Timothy Patterson <tim.patterson@digital.trade.gov.uk>
2023-11-17 09:59:37 +0000

    add review attributes to Microsite page model

--- core/models.py
+++ core/models.py
@@ -1388,6 +1388,9 @@ class Microsite(Page):
 
     subpage_types = ['core.MicrositePage']
 
+    six_month_review_reminder_sent = models.DateTimeField(blank=True, null=True)
+    twelve_month_review_reminder_sent = models.DateTimeField(blank=True, null=True)
+
     class Meta:
         verbose_name = 'Campaign site'
         verbose_name_plural = 'Campaign sites'
