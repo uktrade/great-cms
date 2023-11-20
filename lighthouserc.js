3b348a078118f4eed4b561ece4c9db824a93eb11
Halidu Abubakar <halidu.abubakar@digital.trade.gov.uk>
2023-11-20 13:41:36 +0000

    spike lighthouse ci

---
+++ lighthouserc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 5,
+      startServerCommand: 'make webserver',
+      url: ['http://localhost:8020/', 'http://localhost:8020/services/'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['uses-http2'],
+        chromeFlags: '--no-sandbox',
+        extraHeaders: JSON.stringify({
+          Cookie: 'customCookie=1;foo=bar',
+        }),
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median-run' },
+        ],
+        'categories:accessibility': [
+          'error',
+          { minScore: 1, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:best-practices': [
+          'error',
+          { minScore: 1, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:seo': [
+          'error',
+          { minScore: 1, aggregationMethod: 'pessimistic' },
+        ],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
