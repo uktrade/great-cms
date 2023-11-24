6b1d0e42d143876d3f746cdfc1a44b00380b691b
LuptonM <marc.lupton100@gmail.com>
2023-11-24 14:01:36 +0000

    fix merge issues

--- react-components/src/components/CaseStudy/CaseStudy.jsx
+++ react-components/src/components/CaseStudy/CaseStudy.jsx
@@ -75,13 +75,13 @@ const CaseStudy = memo(({ content: { heading, company, blocks } }) => {
             <h2 className="govuk-heading-l cast-study__lead_title govuk-heading-m govuk-!-margin-bottom-3 govuk-!-padding-0">
               {ReactHtmlParser(heading)}
             </h2>
-            <h3 className="case-study__company text-blue-deep-60 govuk-heading-s govuk-!-padding-0">
+            <h3 className="case-study__company govuk-heading-s govuk-!-padding-0">
               {ReactHtmlParser(company)}
             </h3>
             {/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
             {image}
             {/* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
-            <details class="govuk-details" data-module="govuk-details">
+            <details class="govuk-details govuk-!-margin-top-4" data-module="govuk-details">
               <summary class="govuk-details__summary">
                 <span class="govuk-details__summary-text">
                   Show case study details
