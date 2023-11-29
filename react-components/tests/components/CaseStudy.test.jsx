7e552871829fe7270c9cce363cd49def5394483b
Marc Lupton <56481191+LuptonM@users.noreply.github.com>
2023-11-29 17:03:47 +0000

    Kls 1579 (#2749)
    * save changes
    
    * save progress
    
    * save changes
    
    * save changes
    
    * rebased
    
    * rebased
    
    * update tests
    
    * fix merge issues
    
    * save changes
    
    * updated
    
    * fix issues
    
    * pm comments
    
    * undo settings changes
    
    * rebased
    
    * fix styling
    
    * add govuk filters around step
    
    * add govuk filters around step
    
    * make numbers centred
    
    * make numbers centred
    
    * updates
    
    * update tests
    
    * fix test
    
    * rebased
    
    * add test to fix coverage
    
    * fix black and flake8 error

--- react-components/tests/components/CaseStudy.test.jsx
+++ react-components/tests/components/CaseStudy.test.jsx
@@ -3,8 +3,7 @@ import { render, waitFor, fireEvent } from '@testing-library/react'
 
 import { CaseStudy } from '../../src/components/CaseStudy/CaseStudy'
 
-const openButtonText = 'Expand case study'
-const closeButtonText = 'Collapse case study'
+const openButtonText = 'Show case study details'
 const bodyText = 'example body content'
 const mediaBlock = { type: 'media', content: `<div>${bodyText}</div>` }
 const quoteBlock = { type: 'quote', content: `<div>${bodyText}</div>` }
@@ -33,19 +32,5 @@ describe('CaseStudy', () => {
   it('Should have an open case study button', () => {
     const { getByText } = setup(props)
     expect(getByText(openButtonText)).toBeInTheDocument()
-  })
-
-  it('Should toggle body content when buttons clicked', async () => {
-    const { getByText, queryByText } = setup(props)
-
-    await waitFor(() => {
-      expect(getByText(props.company)).toBeInTheDocument()
-    })
-
-    fireEvent.click(getByText(openButtonText))
-
-    await waitFor(() => {
-      expect(getByText(closeButtonText)).toBeInTheDocument()
-    })
   })
 })
