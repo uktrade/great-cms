e395c06c053d48c3a4416faa3bf7a0612791b3f8
LuptonM <marc.lupton100@gmail.com>
2023-11-29 16:54:06 +0000

    fix black and flake8 error

--- tests/unit/core/test_models.py
+++ tests/unit/core/test_models.py
@@ -60,6 +60,7 @@ def test_object_hash():
     hash = AbstractObjectHash.generate_content_hash(mocked_file)
     assert hash == 'acbd18db4cc2f85cedef654fccc4a4d8'  # /PS-IGNORE
 
+
 @pytest.mark.django_db
 def test_detail_page_get_lesson_category_name(client, domestic_homepage, user, domestic_site, mock_get_user_profile):
     # given the user has not read a lesson
@@ -71,7 +72,8 @@ def test_detail_page_get_lesson_category_name(client, domestic_homepage, user, d
     detail_page = factories.DetailPageFactory(parent=topic_page)
 
     assert detail_page.get_lesson_category_name() == 'Topic page'
-    
+
+
 @pytest.mark.django_db
 def test_detail_page_can_mark_as_read(client, domestic_homepage, user, domestic_site, mock_get_user_profile):
     # given the user has not read a lesson
