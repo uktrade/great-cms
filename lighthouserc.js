5d8f5a4dbeb1236a01a4b24b4c451ed751ac65af
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2023-11-30 15:07:52 +0000

    KLS-1565: Add lighthouseci

---
+++ lighthouserc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      startServerCommand: 'DEBUG=False make webserver',
+      url: ['http://localhost:8020/'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['uses-http2'],
+        chromeFlags: '--no-sandbox',
+        extraHeaders: JSON.stringify({
+          Cookie: 'customCookie=1;foo=bar',
+        }),
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': [
+          'error',
+          { minScore: 0.6, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:accessibility': [
+          'error',
+          { minScore: 0.89, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:best-practices': [
+          'error',
+          { minScore: 0.89, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:seo': [
+          'error',
+          { minScore: 0.89, aggregationMethod: 'pessimistic' },
+        ],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
