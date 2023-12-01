8e0ce17ea2bb6ad8cd7e4e272263cb7d64aefc76
LuptonM <marc.lupton100@gmail.com>
2023-12-01 14:45:33 +0000

    fix exception

--- domestic/views/campaign.py
+++ domestic/views/campaign.py
@@ -43,11 +43,10 @@ class CampaignView(BaseNotifyUserFormView):
         try:
             current_language_code = get_language()
             current_locale = Locale.objects.get(language_code=current_language_code)
-            return self.get_correct_page(current_locale)
         except ObjectDoesNotExist:
             from config.settings import LANGUAGE_CODE
-            default_locale = Locale.objects.get(language_code=LANGUAGE_CODE)
-            return self.get_correct_page(default_locale)
+            current_locale = Locale.objects.get(language_code=LANGUAGE_CODE)
+        return self.get_correct_page(current_locale)
 
     def get_correct_page(self, current_locale):
         if self.page_class.objects.live().filter(slug=self.page_slug).count() > 0:
