9cb62dac4f9645acb3656d2a2c9c1c5c6a9ef6c0
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2024-02-08 08:57:46 +0000

    ADDED Tests.

--- tests/unit/export_academy/test_models.py
+++ tests/unit/export_academy/test_models.py
@@ -83,4 +83,4 @@ def test_event_model_save_fails_when_completed_but_not_closed():
     # Ensure saving event raises a validation Error
     with pytest.raises(ValidationError) as excinfo:
         event.clean()
-    assert str(excinfo.value) == "Event must be marked 'Closed for Bookings' before it can be marked 'Completed'"
+    assert str(excinfo.value) == ["Event must be marked 'Closed for Bookings' before it can be marked 'Completed'"]
