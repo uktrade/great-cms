2074154695a7177aa07d64f20d510d2e58362cd6
stuart-madetech <75611506+stuart-madetech@users.noreply.github.com>
2024-02-14 15:04:27 +0000

    First pass of international home in great-cms (#2897)
    * First pass of international home in great-cms
    
    * Styling updates
    
    * Refactor styles
    
    * Updating model test in EYB index cms page class
    
    * Removing views and test_views as no longer used
    
    * Adding test_models for international
    
    * Adding test coverage to getting international icons on homepage

--- international/urls.py
+++ international/urls.py
@@ -1,13 +1,3 @@
-from django.urls import path
-
-from international import views
-
 app_name = 'international'
 
-urlpatterns = [
-    path(
-        '',
-        views.IndexView.as_view(),
-        name='index',
-    ),
-]
+urlpatterns = []
--- tests/unit/international/test_views.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.urls import reverse
-
-
-@pytest.mark.django_db
-def test_index(client, settings):
-    url = reverse('international:index')
-    response = client.get(url)
-    assert response.status_code == 200
