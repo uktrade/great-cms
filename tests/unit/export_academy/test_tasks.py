ef7566efd4004c616fc7c99f5948b54409e67096
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2024-02-15 09:06:43 +0000

    ADDED prototype test

--- tests/unit/export_academy/test_tasks.py
+++ tests/unit/export_academy/test_tasks.py
@@ -71,6 +71,54 @@ def test_notify_event_complete_email_sent(mock_complete_action, user):
     assert mock_complete_action.call_count == 1
 
 
+@mock.patch('directory_forms_api_client.actions.GovNotifyEmailAction')
+@pytest.mark.django_db
+def test_notify_event_complete_expired_email_not_sent(mock_complete_action, user):
+    """
+    We want to create and event that has marked as completed more than 15 minutes ago, and ensure an email notification
+    is NOT sent.
+    """
+
+    mock_complete_action().save.return_value = create_response(status_code=201)
+
+    # Create event with one booking
+    event = factories.EventFactory(
+        name='Event name',
+        completed=timezone.now() - timedelta(minutes=30),
+    )
+    registration = factories.RegistrationFactory(email=user.email, first_name=user.first_name)
+    factories.BookingFactory(event=event, registration=registration, status='Confirmed')
+
+    # Run task - we are looking for a single email being sent
+    assert mock_complete_action.call_count == 0
+    send_automated_event_complete_notification()
+    assert mock_complete_action.call_count == 0
+
+
+@mock.patch('directory_forms_api_client.actions.GovNotifyEmailAction')
+@pytest.mark.django_db
+def test_notify_event_not_complete_email_not_sent(mock_complete_action, user):
+    """
+    We want to create and event that has marked as NOT completed, and ensure an email notification
+    is NOT sent.
+    """
+
+    mock_complete_action().save.return_value = create_response(status_code=201)
+
+    # Create event with one booking
+    event = factories.EventFactory(
+        name='Event name',
+        completed=None,
+    )
+    registration = factories.RegistrationFactory(email=user.email, first_name=user.first_name)
+    factories.BookingFactory(event=event, registration=registration, status='Confirmed')
+
+    # Run task - we are looking for a single email being sent
+    assert mock_complete_action.call_count == 0
+    send_automated_event_complete_notification()
+    assert mock_complete_action.call_count == 0
+
+
 @pytest.mark.django_db
 def test_remove_video(user):
     delay_days = settings.EXPORT_ACADEMY_REMOVE_EVENT_MEDIA_AFTER_DAYS + 1
