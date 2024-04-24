57466c0d75b0cad47ea17ad109571e8a2ca1d1b2
boxadesign <info@boxadesign.co.uk>
2024-04-24 16:33:50 +0100

    Fix tests

--- tests/unit/core/test_wagtail_hooks.py
+++ tests/unit/core/test_wagtail_hooks.py
@@ -333,7 +333,7 @@ def test_estimated_read_time_calculation__updates_only_draft_if_appropriate(rf, 
 
     detail_page.refresh_from_db()
 
-    expected_duration = timedelta(seconds=32)  # NB just the read time of a skeleton DetailPage
+    expected_duration = timedelta(seconds=33)  # NB just the read time of a skeleton DetailPage
 
     # show the live version is not updated yet
     assert detail_page.has_unpublished_changes is True
@@ -369,7 +369,7 @@ def test_estimated_read_time_calculation__updates_only_draft_if_appropriate(rf, 
     # NOTE: for a reason unrelated to the point of _this_ test, the readtime
     # of the published page CAN BE calculated as slightly longer than the draft.
     # This may be in part due to the page having a very small amount of content.
-    assert detail_page.estimated_read_duration == timedelta(seconds=32)
+    assert detail_page.estimated_read_duration == timedelta(seconds=33)
 
 
 @pytest.mark.django_db
@@ -412,7 +412,7 @@ def test_estimated_read_time_calculation__forced_update_of_live(rf, domestic_hom
 
     detail_page.refresh_from_db()
 
-    expected_duration = timedelta(seconds=32)  # NB just the read time of a skeleton DetailPage
+    expected_duration = timedelta(seconds=33)  # NB just the read time of a skeleton DetailPage
 
     # show the live version is updated yet
     assert detail_page.estimated_read_duration == expected_duration
