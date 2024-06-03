b48f06db0306809f837229523137228a7b240ef0
bobby-didcoding <bobby@didcoding.com>
2024-06-03 16:44:08 +0100

    Added new task test

--- export_academy/forms.py
+++ export_academy/forms.py
@@ -23,8 +23,8 @@ from wagtail.admin.forms import WagtailAdminModelForm
 from contact import constants, widgets as contact_widgets
 from core.validators import is_valid_uk_phone_number, is_valid_uk_postcode
 from directory_constants.choices import COUNTRY_CHOICES
-from export_academy.widgets import GreatRadioSelectWithOtherText
 from export_academy import choices
+from export_academy.widgets import GreatRadioSelectWithOtherText
 
 COUNTRIES = COUNTRY_CHOICES.copy()
 COUNTRIES.insert(0, ('', 'Select a country'))
