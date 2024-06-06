93513085b3ea0fb588fcbd2c902663b263ed4327
boxadesign <info@boxadesign.co.uk>
2024-06-06 11:57:43 +0100

    Improve js

--- domestic/static/javascript/hcsat-feedback-form.js
+++ domestic/static/javascript/hcsat-feedback-form.js
@@ -5,32 +5,32 @@ class CsatFormHandler {
         this.stepTwo = document.getElementById('csat-step-2');
         this.stepOneSuccessMessage = document.getElementById('csat-step-1-submission-confirmation');
         this.stepTwoSuccessMessage = document.getElementById('csat-step-2-submission-confirmation');
+        this.infoMsg = document.getElementById('info-message')
         this.errorSummary = document.getElementById('error-summary');
         this.errorSummaryTitle = this.errorSummary.querySelector('.govuk-error-summary__title');
         this.errorList = this.errorSummary.querySelector('.govuk-error-summary__list');
         this.submitButton = this.form.querySelectorAll('button[type="submit"]')[0];
         this.cancelButton = this.form.querySelectorAll('button[type="submit"]')[1];
-        this.infoMsg = document.getElementById('infoMessage')
         this.currentStep = 1;
+        this.hideClass = 'great-hidden';
         this.initializeEventListeners();
     }
 
     initializeEventListeners() {
         this.form.addEventListener('submit', async (event) => {
             event.preventDefault();
-
-            if (event.submitter.name=='cancelButton'){
-                this.stepTransition(this.stepTwo, this.stepOne, this.stepTwoSuccessMessage, '');
-                this.form.classList.add('great-hidden');
-                this.stepOneSuccessMessage.classList.add('great-hidden');
-                this.infoMsg.classList.add('great-hidden')
-                this.currentStep=1
-                return
-            }
-
             const formData = new FormData(this.form);
             formData.append('step', this.currentStep);
             const url = this.form.action
+            
+            if (event.submitter.name=='cancelButton'){
+                this.stepTransition(this.stepTwo, this.stepOne, this.stepTwoSuccessMessage, '');
+                this.form.classList.add(this.hideClass);
+                this.stepOneSuccessMessage.classList.add(this.hideClass);
+                this.infoMsg.classList.add(this.hideClass)
+                this.currentStep=1
+                return
+            }
 
             try {
                 const response = await fetch(`${url}?js_enabled=True`, {
@@ -47,52 +47,52 @@ class CsatFormHandler {
                 this.handleStepTransition(response, data);
             } catch (error) {
                 console.error('There was a problem with the fetch operation:', error);
-                this.showErrorSummary();
+                this.showErrors();
             }
         });
     }
 
     handleStepTransition(response, data) {
-        this.checkExistingErrors();
+        this.clearExistingErrors(data);
         if (response.status==200) {
             if (this.currentStep === 1) {
-                this.infoMsg.classList.remove('great-hidden')
+                this.infoMsg.classList.remove(this.hideClass);
+                this.cancelButton.classList.remove('great-hidden');
                 this.stepTransition(this.stepOne, this.stepTwo, this.stepOneSuccessMessage, 'Submit feedback');
-                this.cancelButton.classList.remove('great-hidden')
-
                 this.currentStep = 2;
             } else {
                 this.stepTransition(this.stepTwo, this.stepOne, this.stepTwoSuccessMessage, '');
-                this.form.classList.add('great-hidden');
-                this.stepOneSuccessMessage.classList.add('great-hidden');
+                this.form.classList.add(this.hideClass);
+                this.stepOneSuccessMessage.classList.add(this.hideClass);
 
-                this.infoMsg.classList.add('great-hidden')
-                this.currentStep=1
+                this.infoMsg.classList.add(this.hideClass);
+                this.currentStep=1;
             }
         } else if (data) {
-            this.showErrorSummary(data);
+            this.showErrors(data);
         }
     }
 
     stepTransition(hideStep, showStep, showMessage, buttonLabel) {
-        hideStep.classList.add('great-hidden');
+        hideStep.classList.add(this.hideClass);
         if (showStep) {
-            showStep.classList.remove('great-hidden');
+            showStep.classList.remove(this.hideClass);
         }
-        showMessage.classList.remove('great-hidden');
+        showMessage.classList.remove(this.hideClass);
         showMessage.focus();
-        this.errorSummary.classList.add('great-hidden');
+        this.errorSummary.classList.add(this.hideClass);
         if (buttonLabel) {
             this.submitButton.textContent = buttonLabel;
         }
     }
+    
+    showErrors(errors) {
+        this.stepOneSuccessMessage.classList.add(this.hideClass);
+        this.stepTwoSuccessMessage.classList.add(this.hideClass);
 
-    showErrorSummary(errors) {
         if (!errors) {
             this.errorSummaryTitle.textContent = 'There is a problem with the network. Please try again.';
         } else {
-            this.stepOneSuccessMessage.classList.add('great-hidden');
-            this.stepTwoSuccessMessage.classList.add('great-hidden');
             this.errorList.innerHTML = '';
             Object.keys(errors).forEach(field => {
                 const fieldGroup = document.getElementById(field + '_group');
@@ -100,62 +100,28 @@ class CsatFormHandler {
                     fieldGroup.classList.add('govuk-form-group--error');
                     const inlineError = fieldGroup.querySelector('.govuk-error-message');
                     if (inlineError) {
-                        inlineError.classList.remove('great-hidden');
+                        inlineError.classList.remove(this.hideClass);
                     }
-                }
-
-                if (field == 'experience_other'){
-                    const OtherField = document.getElementById('conditional-experience-4');
-                    const errorField = OtherField.children[0]
-                    if (OtherField.getElementsByClassName('govuk-error-message').length<1){
-                        var inlineError = document.createElement("p");
-                        inlineError.innerHTML='Enter the issue you experienced'
-                        inlineError.classList.add('govuk-error-message')
-
-                        errorField.insertBefore(inlineError,errorField.querySelector('#id_experience_other'))
-                        errorField.classList.add('govuk-form-group--error');
-                    }
-
-                }
-                if (field == 'feedback_text'){
-                    const errorGroup = document.getElementById('id_feedback_text')
-                    errorGroup.classList.add('govuk-form-group--error')
-                    const errorField = document.getElementById('exceeding-characters-error');
-                    errorField.classList.remove('great-hidden')
-                }
-
-                if (!Object.keys(errors).includes('feedback_text')){
-                    const errorGroup = document.getElementById('id_feedback_text')
-                    errorGroup.classList.remove('govuk-form-group--error')
-                    const errorField = document.getElementById('exceeding-characters-error');
-                    errorField.classList.add('great-hidden')
                 }
 
                 errors[field].forEach(error => {
                     const listItem = document.createElement('li');
-                    if (field == 'feedback_text'){
-                        listItem.innerHTML = `<a href="#with-hint">${'Your feedback must be 1200 characters or less'}</a>`;
-                    }
-                    else{
                     listItem.innerHTML = `<a href="#id_${field}">${error}</a>`;
-                    }
                     this.errorList.appendChild(listItem);
                 });
             });
         }
-        this.errorSummary.classList.remove('great-hidden');
+        this.errorSummary.classList.remove(this.hideClass);
         this.errorSummary.focus();
     }
 
-    checkExistingErrors() {
+    clearExistingErrors(hasErrors) {
         const errorFields = document.querySelectorAll('.govuk-form-group--error');
         errorFields.forEach(fieldGroup => {
-            const inputs = fieldGroup.querySelectorAll('input[type="checkbox"], input[type="radio"]');
-            if (inputs.length) {
-                const isChecked = Array.from(inputs).some(input => input.checked);
-                if (isChecked) {
-                    this.removeErrorClasses(fieldGroup);
-                }
+            const fieldName = fieldGroup.id.replace('_group', '');
+    
+            if (!hasErrors[fieldName]) {
+                this.removeErrorClasses(fieldGroup);
             }
         });
     }
@@ -165,7 +131,7 @@ class CsatFormHandler {
             fieldGroup.classList.remove('govuk-form-group--error');
             const inlineError = fieldGroup.querySelector('.govuk-error-message');
             if (inlineError) {
-                inlineError.classList.add('great-hidden');
+                inlineError.classList.add(this.hideClass);
             }
         }
     }
