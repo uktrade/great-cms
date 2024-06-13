b69f0923b9342e2a33060e91a569cb986e7a2f29
Stuart Mindt <stuart.mindt@madetech.com>
2024-06-13 13:36:51 +0100

    Adding feature flags for general election and eyb home to hide / show functionality

--- international_online_offer/views.py
+++ international_online_offer/views.py
@@ -1,4 +1,5 @@
 from directory_forms_api_client import actions
+from django.conf import settings
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpResponseRedirect
@@ -51,6 +52,9 @@ def calculate_and_store_is_high_value(request):
 
 class IndexView(GA360Mixin, TemplateView):
     template_name = 'eyb/index.html'
+    # TODO remove after general election AND sign up to front branch merged
+    if settings.FEATURE_PRE_ELECTION and settings.FEATURE_EYB_HOME:
+        template_name = 'eyb/index-new.html'
 
     def __init__(self):
         super().__init__()
@@ -60,14 +64,14 @@ class IndexView(GA360Mixin, TemplateView):
             site_section='index',
         )
 
-    # def get_context_data(self, **kwargs):
-    #     breadcrumbs = [
-    #         {'name': 'Home', 'url': '/international/'},
-    #     ]
-    #     return super().get_context_data(
-    #         **kwargs,
-    #         breadcrumbs=breadcrumbs,
-    #     )
+    def get_context_data(self, **kwargs):
+        breadcrumbs = [
+            {'name': 'Home', 'url': '/international/'},
+        ]
+        return super().get_context_data(
+            **kwargs,
+            breadcrumbs=breadcrumbs,
+        )
 
 
 class SectorView(GA360Mixin, FormView):
