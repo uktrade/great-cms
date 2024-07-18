d2d772e3bb4908eb1ddbf661379d0b513b43089e
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-07-18 12:32:56 +0100

    Refactor domestic_canonical_url_tags

--- domestic/templatetags/domestic_canonical_url_tags.py
+++ domestic/templatetags/domestic_canonical_url_tags.py
@@ -7,7 +7,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_canonical_url(context):
-    request = context['request']
-    if 'microsite' in request.path:
-        request.path.replace('microsite', 'campaign-site')
-    return derive_canonical_url(request)
+    request = context.get('request', None)
+    if request:
+        if 'microsite' in request.path:
+            request.path.replace('microsite', 'campaign-site')
+        return derive_canonical_url(request)
+    else:
+        return ''
