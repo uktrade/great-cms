6c501a66e10f061549cb7791d21399cf7db1fc12
bobby-didcoding <bobby@didcoding.com>
2024-07-19 11:09:39 +0100

    fixed context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -106,7 +106,6 @@ def feature_flags(request):
     retval['features']['FEATURE_PRE_ELECTION'] = settings.FEATURE_PRE_ELECTION
     retval['features']['FEATURE_FAB_MIGRATION'] = settings.FEATURE_FAB_MIGRATION
     retval['features']['FEATURE_FAB_HCSAT'] = settings.FEATURE_FAB_HCSAT
-
     
     retval['features']['FEATURE_DESIGN_SYSTEM'] = settings.FEATURE_DESIGN_SYSTEM
 
