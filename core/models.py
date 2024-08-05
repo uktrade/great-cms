13b48522331c30615837865a44eb36f1200f0fd8
divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>
2024-08-05 15:19:41 +0100

    Update to add a view for learn to export articlePage

--- core/models.py
+++ core/models.py
@@ -7,8 +7,7 @@ from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.forms import Select
-from django.http import Http404, HttpResponseRedirect, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.functional import cached_property
@@ -47,8 +46,7 @@ from wagtail.utils.decorators import cached_classmethod
 from wagtailmedia.models import Media
 from wagtailseo.models import SeoMixin as WagtailSeoMixin, TwitterCard
 
-from core import blocks as core_blocks, cms_panels, constants, forms, mixins, snippet_slugs
-from core import context
+from core import blocks as core_blocks, cms_panels, constants, mixins, snippet_slugs
 from core.blocks import (
     LinkBlockWithHeading,
     MicrositeColumnBlock,
@@ -1158,93 +1156,7 @@ class DetailPage(settings.FEATURE_DEA_V2 and CMSGenericPageAnonymous or CMSGener
 
     def serve(self, request, *args, **kwargs):
         self.handle_page_view(request)
-        if request.method == 'POST':
-            form = forms.CsatUserFeedbackForm(request.POST)
-            if form.is_valid():
-                return self.form_valid(request, form)
-            else:
-                return self.form_invalid(request, form)
-        else:
-            initial_data = self.get_initial(request)
-            form = forms.CsatUserFeedbackForm(initial=initial_data)
-            return render(request, self.template_choices, {'form': form, 'context': self.get_context_data(request)})
-
-    def get_csat(self):
-        csat_id = self.request.session('learn_to_export_csat_id')
-        if csat_id:
-            return CsatUserFeedback.objects.get(id=csat_id)
-        return None
-
-    def get_initial(self):
-        csat = self.get_csat()
-        if csat:
-            satisfaction = csat.satisfaction_rating
-            if satisfaction and self.request.session.get('learn_to_export_csat_stage', 0) == 1:
-                return {'satisfaction': satisfaction}
-        return {'satisfaction': ''}
-
-    def get_context_data(self, request, **kwargs):
-        stage = request.session.get('learn_to_export_csat_stage', 0)
-        context['csat_stage'] = stage
-        if stage == 2:
-            del request.session['learn_to_export_csat_stage']
-        return context
-
-    def form_invalid(self, request, form):
-        if 'cancelButton' in request.POST:
-            request.session['learn_to_export_csat_stage'] = 2
-            return HttpResponseRedirect(self.get_success_url())
-        js_enabled = 'js_enabled' in request.get_full_path()
-        if js_enabled:
-            return JsonResponse(form.errors, status=400)
-        return self.render_to_response(self.get_context_data(form=form))
-
-    def form_valid(self, form):
-        if 'cancelButton' in self.request.POST:
-            self.request.session['learn_to_export_csat_stage'] = 2
-            return HttpResponseRedirect(self.get_success_url())
-
-        super().form_valid(form)
-        csat = self.get_csat()
-        if csat:
-            csat_feedback, created = CsatUserFeedback.objects.update_or_create(
-                id=csat.id,
-                defaults={
-                    'experienced_issues': form.cleaned_data['experience'],
-                    'other_detail': form.cleaned_data['experience_other'],
-                    'likelihood_of_return': form.cleaned_data['likelihood_of_return'],
-                    'service_improvements_feedback': form.cleaned_data['feedback_text'],
-                },
-            )
-            csat_stage = self.request.session.get('learn_to_export_csat_stage', 0)
-
-            if csat_stage == 0:
-                self.request.session['learn_to_export_csat_stage'] = 1
-            else:
-                self.request.session['learn_to_export_csat_stage'] = 2
-
-        else:
-            csat_feedback = CsatUserFeedback.objects.create(
-                satisfaction_rating=form.cleaned_data['satisfaction'],
-                experienced_issues=form.cleaned_data['experience'],
-                other_detail=form.cleaned_data['experience_other'],
-                likelihood_of_return=form.cleaned_data['likelihood_of_return'],
-                service_improvements_feedback=form.cleaned_data['feedback_text'],
-                user_journey='ARTICLE_PAGE',
-            )
-            self.request.session['learn_to_export_csat_id'] = csat_feedback.id
-            self.request.session['learn_to_export_csat_stage'] = 1
-
-        data = {
-            'pk': csat_feedback.pk,
-        }
-        js_enabled = 'js_enabled' in self.request.get_full_path()
-        if js_enabled:
-            csat_stage = self.request.session.get('learn_to_export_csat_stage', 0)
-            if csat_stage == 1:
-                del self.request.session['learn_to_export_csat_stage']
-            return JsonResponse(data)
-        return HttpResponseRedirect(self.get_success_url())
+        return super().serve(request, **kwargs)
 
     @cached_property
     def topic_title(self):
