403af7f1a69d88912e575778e40ce5a59f9d9808
bobby-didcoding <bobby@didcoding.com>
2024-08-07 13:54:06 +0100

    added csat logic to DetailPage

--- core/models.py
+++ core/models.py
@@ -67,6 +67,8 @@ from exportplan.core.data import (
     SECTIONS as EXPORTPLAN_URL_MAP,
 )
 
+from learn.forms import CsatUserFeedbackForm
+
 # If we make a Redirect appear as a Snippet, we can sync it via Wagtail-Transfer
 register_snippet(Redirect)
 
@@ -1201,10 +1203,27 @@ class DetailPage(settings.FEATURE_DEA_V2 and CMSGenericPageAnonymous or CMSGener
         if backlink_path and len(backlink_path.split('/')) > 3:
             _path = backlink_path.split('/')[3]
             return self._export_plan_url_map.get(_path)
+        
+    def get_csat(self):
+        csat_id = self.request.session.get('learn_to_export_csat_id')
+        if csat_id:
+            return CsatUserFeedback.objects.get(id=csat_id)
+        return None
+
+    def get_initial(self):
+        csat = self.get_csat()
+        if csat:
+            satisfaction = csat.satisfaction_rating
+            if satisfaction and self.request.session.get('learn_to_export_csat_id', 0) == 1:
+                return {'satisfaction': satisfaction}
+        return {'satisfaction': ''}
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request)
         context['refresh_on_market_change'] = True
+
+        context['form'] = CsatUserFeedbackForm(data=self.get_initial())
+        
         # Prepare backlink to the export plan if we detect one and can validate it
         _backlink = self._get_backlink(request)
         if _backlink:
