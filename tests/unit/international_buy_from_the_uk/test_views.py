6b69192d9cff8cfa3a0ce00dc60be0c7848449a0
Stuart Mindt <stuart.mindt@madetech.com>
2024-08-16 15:30:07 +0100

    Fixing tests

--- tests/unit/international_buy_from_the_uk/test_views.py
+++ tests/unit/international_buy_from_the_uk/test_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_buy_from_the_uk_contact(client, settings):
+def test_buy_from_the_uk_contact_view(client, settings):
     settings.FEATURE_INTERNATIONAL_INVESTMENT = True
     url = reverse('international_buy_from_the_uk:contact')
     response = client.get(url)
