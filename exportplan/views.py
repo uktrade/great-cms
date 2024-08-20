859d40f19d9f8b50ff92f422caed2c0e88d3c046
divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>
2024-08-20 16:12:16 +0100

    Add models and forms for csat export plan

--- exportplan/views.py
+++ exportplan/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import sentry_sdk
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.functional import cached_property
@@ -17,6 +17,7 @@ from core.utils import choices_to_key_value
 from directory_api_client.client import api_client
 from directory_constants import choices
 from exportplan import forms
+from exportplan.models import CsatUserFeedback
 from exportplan.context import (
     CountryDataContextProvider,
     FactbookDataContextProvider,
@@ -459,6 +460,7 @@ class ExportPlanUpdate(GA360Mixin, TemplateView):
 class ExportPlanDashBoard(
     GA360Mixin,
     TemplateView,
+    FormView,
 ):
     export_plan = None
 
@@ -471,6 +473,7 @@ class ExportPlanDashBoard(
         )
 
     template_name = 'exportplan/dashboard_page.html'
+    form_class = forms.CsatUserFeedbackForm
 
     def dispatch(self, request, *args, **kwargs):
         id = int(self.kwargs['id'])
@@ -489,4 +492,75 @@ class ExportPlanDashBoard(
         context['export_plan_download_link'] = reverse_lazy(
             'exportplan:pdf-download', kwargs={'id': self.export_plan.get('pk')}
         )
+        stage = self.request.session.get('exportplan_csat_stage', 0)
+        context['csat_stage'] = stage
+        if stage == 2:
+            del self.request.session['exportplan_csat_stage']
         return context
+
+    def get_success_url(self):
+        return reverse_lazy('exportplan:update', kwargs={'id': id})
+
+    def get_csat(self):
+        csat_id = self.request.session.get('exportplan_csat_id')
+        if csat_id:
+            return CsatUserFeedback.objects.get(id=csat_id)
+        return None
+
+    def form_invalid(self, form):
+        if 'cancelButton' in self.request.POST:
+            self.request.session['exportplan_csat_stage'] = 2
+            return HttpResponseRedirect(self.get_success_url())
+        super().form_invalid(form)
+        js_enabled = 'js_enabled' in self.request.get_full_path()
+        if js_enabled:
+            return JsonResponse(form.errors, status=400)
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_valid(self, form):
+        if 'cancelButton' in self.request.POST:
+            self.request.session['exportplan_csat_stage'] = 2
+            return HttpResponseRedirect(self.get_success_url())
+
+        super().form_valid(form)
+        csat = self.get_csat()
+        if csat:
+            csat_feedback, created = CsatUserFeedback.objects.update_or_create(
+                id=csat.id,
+                defaults={
+                    'experienced_issues': form.cleaned_data['experience'],
+                    'other_detail': form.cleaned_data['experience_other'],
+                    'likelihood_of_return': form.cleaned_data['likelihood_of_return'],
+                    'service_improvements_feedback': form.cleaned_data['feedback_text'],
+                },
+            )
+            csat_stage = self.request.session.get('exportplan_csat_stage', 0)
+
+            if csat_stage == 0:
+                self.request.session['exportplan_csat_stage'] = 1
+            else:
+                self.request.session['exportplan_csat_stage'] = 2
+
+        else:
+            csat_feedback = CsatUserFeedback.objects.create(
+                satisfaction_rating=form.cleaned_data['satisfaction'],
+                experienced_issues=form.cleaned_data['experience'],
+                other_detail=form.cleaned_data['experience_other'],
+                likelihood_of_return=form.cleaned_data['likelihood_of_return'],
+                service_improvements_feedback=form.cleaned_data['feedback_text'],
+                URL=reverse_lazy('exportplan:update', kwargs={'id': id}),
+                user_journey='EXPORT_PLAN',
+            )
+            self.request.session['exportplan_csat_id'] = csat_feedback.id
+            self.request.session['exportplan_csat_stage'] = 1
+
+        data = {
+            'pk': csat_feedback.pk,
+        }
+        js_enabled = 'js_enabled' in self.request.get_full_path()
+        if js_enabled:
+            csat_stage = self.request.session.get('exportplan_csat_stage', 0)
+            if csat_stage == 1:
+                del self.request.session['exportplan_csat_stage']
+            return JsonResponse(data)
+        return HttpResponseRedirect(self.get_success_url())
