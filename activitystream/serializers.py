a298fa0af9882b1b60fe1ae3bb97b76dbc223e72
divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>
2024-08-21 09:53:33 +0100

    Data workspace endpoints for Make an export pan CSAT

--- activitystream/serializers.py
+++ activitystream/serializers.py
@@ -21,6 +21,7 @@ from export_academy.models import (
 from find_a_buyer.models import CsatUserFeedback as FindABuyerCsatUserFeedback
 from international_online_offer.models import CsatFeedback, TriageData, UserData
 from learn.models import CsatUserFeedback as LearnToExportCsatUserFeedback
+from exportplan.models import CsatUserFeedback as ExportPlanCsatUserFeedback
 
 logger = logging.getLogger(__name__)
 
@@ -725,6 +726,46 @@ class ActivityStreamLearnToExportCsatUserFeedbackDataSerializer(serializers.Mode
         Prefix field names to match activity stream format
         """
         prefix = 'dit:learnToExport:csatFeedbackData'
+        type = 'Update'
+
+        return {
+            'id': f'{prefix}:{instance.id}:{type}',
+            'type': f'{type}',
+            'object': {
+                'id': f'{prefix}:{instance.id}',
+                'type': prefix,
+                **{f'{k}': v for k, v in super().to_representation(instance).items()},
+            },
+        }
+
+
+class ActivityStreamExportPlantCsatUserFeedbackDataSerializer(serializers.ModelSerializer):
+    """
+    Make an Export Plan CSAT Feedback Data serializer for activity stream.
+    """
+
+    feedback_submission_date = serializers.DateTimeField(source='created')  # noqa: N815
+    url = serializers.CharField(source='URL')  # noqa: N815
+
+    class Meta:
+        model = ExportPlanCsatUserFeedback
+        fields = [
+            'id',
+            'feedback_submission_date',
+            'url',
+            'user_journey',
+            'satisfaction_rating',
+            'experienced_issues',
+            'other_detail',
+            'service_improvements_feedback',
+            'likelihood_of_return',
+        ]
+
+    def to_representation(self, instance):
+        """
+        Prefix field names to match activity stream format
+        """
+        prefix = 'dit:exportPlan:csatFeedbackData'
         type = 'Update'
 
         return {
