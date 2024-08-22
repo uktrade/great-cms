fc3a2f5f803cdb0f28c3fef409be3a7f8f79d3c5
Stuart Mindt <stuart.mindt@madetech.com>
2024-08-22 14:27:28 +0100

    Adding tests to check for views in find a supplier

--- tests/unit/international_buy_from_the_uk/test_views.py
+++ tests/unit/international_buy_from_the_uk/test_views.py
@@ -3,8 +3,21 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_buy_from_the_uk_contact_view(client, settings):
-    settings.FEATURE_INTERNATIONAL_INVESTMENT = True
+def test_buy_from_the_uk_contact_view(client):
     url = reverse('international_buy_from_the_uk:contact')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_buy_from_the_uk_search_view(client):
+    url = reverse('international_buy_from_the_uk:find-a-supplier')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_buy_from_the_uk_company_profile_view(client):
+    url = reverse('international_buy_from_the_uk:find-a-supplier-profile')
     response = client.get(url)
     assert response.status_code == 200
