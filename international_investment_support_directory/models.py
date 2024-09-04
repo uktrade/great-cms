6cac4765c841737b5c1908c550b8d921a12bb20f
stuart-mindt <75611506+stuart-mindt@users.noreply.github.com>
2024-09-04 12:51:31 +0100

    Iguk 325 investment support directory (#3371)
    * Adding new service for investment support directory to great-cms

--- international_investment_support_directory/models.py
+++ international_investment_support_directory/models.py
@@ -1,24 +1,32 @@
+from directory_constants import expertise
 from domestic.models import BaseContentPage
 
 
-class BuyFromTheUKIndexPage(BaseContentPage):
+class InvestmentSupportDirectoryIndexPage(BaseContentPage):
     parent_page_types = [
         'international.GreatInternationalHomePage',
     ]
     subpage_types = []
-    template = 'buy_from_the_uk/index.html'
+    template = 'investment_support_directory/index.html'
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         breadcrumbs = [
             {'name': 'Home', 'url': '/international/'},
         ]
+
         context.update(
             breadcrumbs=breadcrumbs,
+            CHOICES_FINANCIAL=expertise.FINANCIAL,
+            CHOICES_HUMAN_RESOURCES=expertise.HUMAN_RESOURCES,
+            CHOICES_LEGAL=expertise.LEGAL,
+            CHOICES_PUBLICITY=expertise.PUBLICITY,
+            CHOICES_BUSINESS_SUPPORT=expertise.BUSINESS_SUPPORT,
+            CHOICES_MANAGEMENT_CONSULTING=expertise.MANAGEMENT_CONSULTING,
         )
         self.set_ga360_payload(
             page_id='Index',
-            business_unit='Buy from the UK',
+            business_unit='Investment Support Directory',
             site_section='index',
         )
         self.add_ga360_data_to_payload(request)
--- react-components/webpack.config.js
+++ react-components/webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     expand_your_business_styles: './international_online_offer/sass/main.scss',
     investment_styles: './international_investment/sass/main.scss',
     buy_from_the_uk_styles: './international_buy_from_the_uk/sass/main.scss',
+    investment_support_directory_styles: './international_investment_support_directory/sass/main.scss',
     microsite_styles: './styles/microsite/main.scss',
     fab_styles: './find_a_buyer/static/sass/main.scss',
   },
