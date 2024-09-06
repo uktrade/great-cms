653d3669282e89e851d1d39fb7f10d694f7dd656
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-09-06 10:13:44 +0100

    add hreflang and x-default links if appropriate

--- contact/templatetags/contact_canonical_url_tags.py
+++ contact/templatetags/contact_canonical_url_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from core.utils import derive_canonical_url
+from core.utils import derive_canonical_url, hreflang_and_x_default_link
 
 register = template.Library()
 
@@ -9,3 +9,13 @@ register = template.Library()
 def get_canonical_url(context):
     request = context['request']
     return derive_canonical_url(request)
+
+
+@register.simple_tag(takes_context=True)
+def hreflang_tags(context):
+    canonical_url = get_canonical_url(context)
+    request = context['request']
+    absolute_url = request.get_absolute_url()
+    if absolute_url == canonical_url:
+        return hreflang_and_x_default_link(canonical_url, 'en-gb')
+    return ''
--- domestic/templatetags/domestic_canonical_url_tags.py
+++ domestic/templatetags/domestic_canonical_url_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from core.utils import derive_canonical_url
+from core.utils import derive_canonical_url, hreflang_and_x_default_link
 
 register = template.Library()
 
@@ -14,3 +14,15 @@ def get_canonical_url(context):
         return derive_canonical_url(request)
     else:
         return ''
+
+
+@register.simple_tag(takes_context=True)
+def hreflang_tags(context):
+    request = context['request']
+    canonical_url = derive_canonical_url(request)
+    absolute_url = request.get_absolute_url()
+    if canonical_url == absolute_url:
+        if 'microsite' in canonical_url:
+            canonical_url.replace('microsite', 'campaign-site')
+        return hreflang_and_x_default_link(canonical_url, 'en-gb')
+    return ''
