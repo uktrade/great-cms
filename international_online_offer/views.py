2f1988ba2b0dd360890b7e5de81c5adaade24e76
divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>
2024-09-11 12:20:12 +0100

    Flake8

--- international_online_offer/views.py
+++ international_online_offer/views.py
@@ -8,13 +8,10 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from great_components.mixins import GA360Mixin
 
-from core.forms import HCSATWithIntensionsForm
 from core.helpers import (
     check_url_host_is_safelisted,
-    hcsat_get_initial,
-    hcsat_update_or_create,
 )
-from core.models import HCSAT
+
 from directory_sso_api_client import sso_api_client
 from international_online_offer import forms
 from international_online_offer.core import (
