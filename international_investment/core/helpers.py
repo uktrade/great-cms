d20e80ef41fa92d2c53b66cf7e17492f1019d037
stuart-mindt <75611506+stuart-mindt@users.noreply.github.com>
2024-09-19 12:07:03 +0100

    Investment opportunities migration from international UI to great-cms (#3404)
    * Investment opportunities migration from international UI to great-cms

--- international_investment/core/helpers.py
+++ international_investment/core/helpers.py
@@ -20,3 +20,32 @@ def get_spend_choices_by_currency(currency):
     elif currency == 'USD':
         spend_choices = SPEND_CHOICES_USD
     return spend_choices
+
+
+def get_investment_opportunities_search_filters(opportunities):
+    sector_filters = []
+    investment_type_filters = []
+    region_filters = []
+
+    # Collecting unique filter values from available opportunities
+    for opportunity in opportunities:
+        if opportunity.sector not in sector_filters:
+            sector_filters.append(opportunity.sector)
+        if opportunity.region not in region_filters:
+            region_filters.append(opportunity.region)
+        if opportunity.investment_type not in investment_type_filters:
+            investment_type_filters.append(opportunity.investment_type)
+
+    sector_filters.sort()
+    region_filters.sort()
+    investment_type_filters.sort()
+
+    sector_choices = tuple((sector_filter, sector_filter) for sector_filter in sector_filters)
+
+    region_choices = tuple((region_filter, region_filter) for region_filter in region_filters)
+
+    investment_type_choices = tuple(
+        (investment_type_choice, investment_type_choice) for investment_type_choice in investment_type_filters
+    )
+
+    return sector_choices, region_choices, investment_type_choices
