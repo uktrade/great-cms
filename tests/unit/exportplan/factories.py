7c2440dd1d10bd8002b91508055273d98070ecc6
bobby-didcoding <bobby@didcoding.com>
2024-09-20 12:39:28 +0100

    fixed import error

--- tests/unit/exportplan/factories.py
+++ tests/unit/exportplan/factories.py
@@ -1,19 +1,19 @@
 import factory
 import factory.fuzzy
 
-from exportplan import choices
+from core import constants
 from exportplan.models import CsatUserFeedback
 
 
 class HCSATFactory(factory.django.DjangoModelFactory):
 
     URL = factory.fuzzy.FuzzyText(length=100)
-    user_journey = factory.fuzzy.FuzzyChoice(choices.USER_JOURNEY_CHOICES, getter=lambda choice: choice[0])
-    satisfaction_rating = factory.fuzzy.FuzzyChoice(choices.SATISFACTION_CHOICES, getter=lambda choice: choice[0])
-    experienced_issues = factory.fuzzy.FuzzyChoice(choices.EXPERIENCE_CHOICES, getter=lambda choice: choice[0])
+    user_journey = factory.fuzzy.FuzzyChoice(constants.USER_JOURNEY_CHOICES, getter=lambda choice: choice[0])
+    satisfaction_rating = factory.fuzzy.FuzzyChoice(constants.SATISFACTION_CHOICES, getter=lambda choice: choice[0])
+    experienced_issues = factory.fuzzy.FuzzyChoice(constants.EXPERIENCE_CHOICES, getter=lambda choice: choice[0])
     other_detail = factory.fuzzy.FuzzyText(length=255)
     service_improvements_feedback = factory.fuzzy.FuzzyText(length=255)
-    likelihood_of_return = factory.fuzzy.FuzzyChoice(choices.LIKELIHOOD_CHOICES, getter=lambda choice: choice[0])
+    likelihood_of_return = factory.fuzzy.FuzzyChoice(constants.LIKELIHOOD_CHOICES, getter=lambda choice: choice[0])
 
     class Meta:
         model = CsatUserFeedback
