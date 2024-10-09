1de80ee9088918c456e0ab15fd0a029e3609b0fc
Kudrotey <kudrotey.khuda@digital.trade.gov.uk>
2024-10-09 12:17:15 +0100

    wip

--- config/settings.py
+++ config/settings.py
@@ -829,7 +829,8 @@ DNB_API_PASSWORD = env.str('DNB_API_PASSWORD', '')
 DNB_API_RENEW_ACCESS_TOKEN_SECONDS_REMAINING = env.int('DNB_API_RENEW_ACCESS_TOKEN_SECONDS_REMAINING', 20)
 
 # geo location
-GEOIP_PATH = os.path.join(ROOT_DIR, 'core/geolocation_data')
+TMP_DIR = Path(__file__).resolve().parents[2]
+GEOIP_PATH = os.path.join(TMP_DIR, 'tmp/core/geolocation_data')
 GEOIP_COUNTRY = 'GeoLite2-Country.mmdb'
 GEOIP_CITY = 'GeoLite2-City.mmdb'
 MAXMIND_LICENCE_KEY = env.str('MAXMIND_LICENCE_KEY')
