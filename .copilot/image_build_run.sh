d3241ee02f9a911addab6cabf1ce47694b54db98
Kudrotey <kudrotey.khuda@digital.trade.gov.uk>
2024-10-10 16:16:46 +0100

    wip

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -5,6 +5,9 @@ set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 
+# shellcheck disable=SC2046
+export $(grep -v '^#' ./config/test | xargs)
+
 echo "Running image_build_run.sh"
 
 echo "Running collectstatic"
