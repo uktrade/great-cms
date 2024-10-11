b92c4ad901914c8fe7a44aa760219b1458befb47
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-10-11 16:28:23 +0100

    WAGTAIL_CACHE_IGNORE_COOKIES = False

--- export_academy/views.py
+++ export_academy/views.py
@@ -20,7 +20,6 @@ from django.shortcuts import get_list_or_404, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.utils.text import get_valid_filename
-from django.views.decorators.vary import vary_on_cookie
 from django.views.generic import (
     DetailView,
     FormView,
@@ -35,7 +34,6 @@ from great_components.helpers import get_is_authenticated, get_user
 from great_components.mixins import GA360Mixin
 from icalendar import Alarm, Calendar, Event
 from rest_framework.generics import GenericAPIView
-from wagtailcache.cache import WagtailCacheMixin, cache_page, nocache_page
 
 from config import settings
 from core import mixins as core_mixins
@@ -87,11 +85,7 @@ class BespokeBreadcrumbMixin(TemplateView):
         return super().get_context_data(bespoke_breadcrumbs=bespoke_breadcrumbs, **kwargs)
 
 
-@method_decorator(cache_page, name='dispatch')
-@method_decorator(vary_on_cookie, name='dispatch')
-class EventListView(
-    WagtailCacheMixin, GetBreadcrumbsMixin, GA360Mixin, core_mixins.GetSnippetContentMixin, FilterView, ListView
-):
+class EventListView(GetBreadcrumbsMixin, GA360Mixin, core_mixins.GetSnippetContentMixin, FilterView, ListView):
     model = models.Event
     queryset = model.upcoming
     filterset_class = filters.EventFilter
@@ -611,7 +605,6 @@ class JoinBookingView(RedirectView):
         return super().get(request, *args, **kwargs)
 
 
-@method_decorator(nocache_page, name='dispatch')
 class SignUpView(HandleNewAndExistingUsersMixin, VerificationLinksMixin, sso_mixins.SignUpMixin, FormView):
     def get_template_names(self):
         if self.get_ea_user():
@@ -820,9 +813,7 @@ class SignInView(HandleNewAndExistingUsersMixin, sso_mixins.SignInMixin, FormVie
         return ctx
 
 
-@method_decorator(cache_page, name='dispatch')
-@method_decorator(vary_on_cookie, name='dispatch')
-class EventsDetailsView(WagtailCacheMixin, DetailView):
+class EventsDetailsView(DetailView):
     template_name = 'export_academy/event_details.html'
     model = models.Event
 
