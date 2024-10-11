176cb33fe2e2a61323750f52e95c2e76aeb924da
Jamie Fox <134952460+FoxJamie16@users.noreply.github.com>
2024-10-11 11:08:59 +0100

    domestic GDS footer in great now matches prod footer

--- core/context_processors.py
+++ core/context_processors.py
@@ -166,28 +166,54 @@ def domestic_footer(request):
     return {
         'isInternational': False,
         'currentYear': 2024,
-        'footerLinks': [{ 
-            'href': '/support/export-support',
-            'title': 'Export support for UK businesses',
-            'text': 'Export support for UK businesses'
-        },
-        { 
-            'href': '/contact/triage/great-account',
-            'title': 'Get help with your account',
-            'text': 'Get help with your account'
-        },
-        { 
-            'href': 'https://'+settings.DIT_ON_GOVUK,
-            'title': 'Department for Business and Trade on GOV.UK',
-            'text': 'Department for Business and Trade on GOV.UK',
-            'target':'_blank',
-            'rel': 'noopener'
-        },
-        { 
-            'href': '/support/export-support',
-            'title': 'Privacy',
-            'text': 'Privacy'
-        }],
-
+        'footerLinks': [
+            { 
+                'href': '/support/export-support',
+                'title': 'Export support for UK businesses',
+                'text': 'Export support for UK businesses',
+            },
+            { 
+                'href': '/contact/triage/great-account',
+                'title': 'Get help with your account',
+                'text': 'Get help with your account',
+            },
+            { 
+                'href': 'https://'+settings.DIT_ON_GOVUK,
+                'title': 'Department for Business and Trade on GOV.UK',
+                'text': 'Department for Business and Trade on GOV.UK',
+                'target':'_blank',
+                'rel': 'noopener',
+            },
+            { 
+                'href': '/privacy',
+                'title': 'Privacy',
+                'text': 'Privacy',
+            },
+            { 
+                'href': '/cookies',
+                'title': 'Cookies',
+                'text': 'Cookies',
+            },
+            { 
+                'href': '/terms-and-conditions',
+                'title': 'Terms and conditions',
+                'text': 'Terms and conditions',
+            },
+            { 
+                'href': '/accessibility-statement',
+                'title': 'Accessibility',
+                'text': 'Accessibility',
+            },
+            { 
+                'href': '/performance-dashboard',
+                'title': 'Performance',
+                'text': 'Performance',
+            },
+            { 
+                'href': '/international',
+                'title': 'International business',
+                'text': 'Go to the page for international business',
+            }
+        ]
     }
 
