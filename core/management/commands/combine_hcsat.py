bb6f125aab62c2d670510063d7731d702c8da664
Jamie Fox <134952460+FoxJamie16@users.noreply.github.com>
2024-10-14 14:30:27 +0100

    Code cleanup grtlv 309 combine hcsat code to share functionality (#3485)
    * Added new HCSAT model
    
    * Created new modelform, model and mixin to handle HCSAT in basic django views
    
    * Fixed model form clean function
    
    * Fixed model form clean function
    
    * Fixed html form validation
    
    * fixed migration conflicts
    
    * Altered export academy and export plan hcsat views
    
    * wip- where to export
    
    * satisfaction no longer required, naming changes for error feedback
    
    * point to the right template
    
    * update the form class
    
    * Cancel button function fixed for javascript, typo fix. Cancel works for non-js, but 'thank you for submitting' message is not shown due to rendering change - to be fixed
    
    * Update to use the passed variable
    
    * [wip]
    
    * Fix tests and flake8
    
    * Do not display the content the second time
    
    * Update tests
    
    * Reset the stage when loading a second time to update the same entry in the table
    
    * Update tests
    
    * Flake8
    
    * WIP - form progression working well on non-js versions. Error validation needs work on all forms for non-js, especially LTE but seems to work on js. is-js-active check needs to be fixed for LTE. Small js glitches in js-enabled scenario. Will continue in morning
    
    * prints removed
    
    * Error feedback re-enabled for non-js users, small error colour override for feedback on one field
    
    * template conditional removed for loading javascript, javascript forms now progress correctly and show fresh form after refresh
    
    * fixed bug where satisfaction was lost if second form was submitted, fixed bug picking up js, fixed js form progression, added auto-scroll back to csat after submission, FAB now only shows after publish and spacing fix
    
    * linting fixes
    
    * unused feature_fab_hcsat variable removed, wagtail read time tests fixed, fab test fix
    
    * flake8 fix, extra function added to mixin for DRY
    
    * static files recompiled
    
    * flake8 import fix
    
    * New management command to move data added along with new activitystream endpoint
    
    * fixed failing test
    
    * fixed import error
    
    * fixed import error
    
    * Fixed serializer test
    
    * fixed command test
    
    * static files recompiled
    
    * GRTLV-309-FEAT-mixin created to tailor initial csat heading depending on service
    
    * Heading size and spacing changed to reflect figma
    
    * design feedback changes, initial button text fix, fix for js form when user refreshes mid-form, TODO stop users from seeing learn hcsat on second article
    
    * linting fixes, test fixed, learn hcsat now only shows on first article of session
    
    * migrations and static files updated post-merge
    
    * linting fixes post merge
    
    * cleanup of unused hcsat code from previous implementation
    
    * international fields removed from domestic hcsat model and references, combine_hcsat fix to include WTE
    
    * migrations fixed post-merge to avoid core conflict
    
    * Small constant cleanup
    
    ---------
    
    Co-authored-by: bobby-didcoding <bobby@didcoding.com>
    Co-authored-by: divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>

---
+++ core/management/commands/combine_hcsat.py
@@ -0,0 +1,33 @@
+from django.core.management import BaseCommand
+
+from core.models import HCSAT, CsatUserFeedback as WhereToExportHCSAT
+from export_academy.models import CsatUserFeedback as ExportAcademyHCSAT
+from exportplan.models import CsatUserFeedback as ExportPlanHCSAT
+from find_a_buyer.models import CsatUserFeedback as FindABuyerHCSAT
+from learn.models import CsatUserFeedback as LearnHCSAT
+
+
+def serialize_hcsat(hcsat):
+
+    serialize_hcsat = hcsat.__dict__
+    del serialize_hcsat['id']
+    del serialize_hcsat['_state']
+
+    return serialize_hcsat
+
+
+class Command(BaseCommand):
+    help = 'Used to combine data from hcsat implementations into one model'
+
+    def handle(self, *args, **options):
+
+        hcsat_models = [ExportAcademyHCSAT, WhereToExportHCSAT, ExportPlanHCSAT, LearnHCSAT, FindABuyerHCSAT]
+
+        for model in hcsat_models:
+            for hcsat in model.objects.all():
+                serialized_obj = serialize_hcsat(hcsat)
+                object, created = HCSAT.objects.get_or_create(**serialized_obj)
+                if created:
+                    self.stdout.write(self.style.SUCCESS('New obj created'))
+                else:
+                    self.stdout.write(self.style.ERROR(f'Found object: {object.id}'))
