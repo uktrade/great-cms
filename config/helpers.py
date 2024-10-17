7d4aada2c829345b9fee58ba4098926d1949b973
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-10-17 16:21:35 +0100

    missing files

---
+++ config/helpers.py
@@ -0,0 +1,13 @@
+import os
+
+
+def is_local():
+    return os.getenv('APP_ENVIRONMENT') is None
+
+
+def is_circleci():
+    return 'IS_CIRCLECI' in os.environ
+
+
+def get_env_files():
+    return ['config/env/' + filename for filename in os.getenv('ENV_FILES', '').split(',')]
