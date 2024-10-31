de718331040c27c99ecb4c8c63884b59146103ec
boxadesign <info@boxadesign.co.uk>
2024-10-31 14:02:51 +0000

    Black CI fix

--- domestic/views/campaign.py
+++ domestic/views/campaign.py
@@ -178,7 +178,9 @@ class CampaignView(BaseNotifyUserFormView):
         use_domestic_logo = page.get_use_domestic_header_logo() if hasattr(page, 'get_use_domestic_header_logo') else ''
         site_title = page.get_site_title() if hasattr(page, 'get_site_title') else ''
         menu_items = page.get_menu_items() if hasattr(page, 'get_menu_items') else ''
-        great_logo_href = page.get_menu_items()[0]['href'] if hasattr(page, 'get_menu_items') and page.get_menu_items() else ''
+        great_logo_href = (
+            page.get_menu_items()[0]['href'] if hasattr(page, 'get_menu_items') and page.get_menu_items() else ''
+        )
         microsite_context = microsite_header(self.request)
         microsite_context['include_link_to_great'] = include_link_to_great
         microsite_context['use_domestic_logo'] = use_domestic_logo
