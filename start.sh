22c80ddf2554c91aead23922f44874afe5ecd370
bobby-didcoding <bobby@didcoding.com>
2024-11-05 10:56:21 +0000

    management comand added to start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # sleep infinity
 python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
+python /app/manage.py clear_wagtail_cache
 python /app/manage.py runserver 0.0.0.0:8020
