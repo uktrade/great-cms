75ab0cc9b37912b8de05a98535b5228c4d597e9b
Halidu Abubakar <haliduabubakar@DBT000649.local>
2024-11-22 08:17:36 +0000

    GREATUK-1351 Mitigate security risk of using Allure

--- tests/browser/test_lessons.py
+++ tests/browser/test_lessons.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 from random import choice
 from typing import List
 from unittest import mock
 
-import allure
 import pytest
 from pytest_django.live_server_helper import LiveServer
 from selenium.webdriver.common.by import By
@@ -35,21 +35,23 @@ pytestmark = [
     pytest.mark.lesson,
 ]
 
+logger = logging.getLogger(__name__)
 
-@allure.step('Visit lesson listing page: {page_name}')
+
 def visit_lesson_listing_page(live_server: LiveServer, browser: WebDriver, page_name: str, endpoint: str):
+    logger.info('Visit lesson listing page: %s', page_name)
     visit_page(live_server, browser, '', page_name, endpoint=endpoint)
     should_see_all_elements(browser, TopicLessonListing)
 
 
-@allure.step('Visit {page_name} page')
 def visit_lesson_page(live_server: LiveServer, browser: WebDriver, page_name: str, endpoint: str):
+    logger.info('Visit %s page', page_name)
     visit_page(live_server, browser, '', page_name, endpoint=endpoint)
     should_see_all_elements(browser, LessonPage)
 
 
-@allure.step('Open random lesson on lesson listing page')
 def open_random_lesson(browser: WebDriver):
+    logger.info('Open random lesson on lesson listing page')
     lesson_links = browser.find_elements(
         by=TopicLessonListing.LESSON_LINKS.by,
         value=TopicLessonListing.LESSON_LINKS.selector,
@@ -62,8 +64,8 @@ def open_random_lesson(browser: WebDriver):
     attach_jpg_screenshot(browser, 'Lesson page')
 
 
-@allure.step('Check topics reading progress')
 def check_topic_read_progress(browser: WebDriver, topic: ListPageFactory, lessons: List[DetailPageFactory]):
+    logger.info('Check topics reading progress')
     attach_jpg_screenshot(browser, 'Topics reading progress', selector=DashboardReadingProgress.YOUR_PROGRESS_CARD)
     count_element = browser.find_element(By.CSS_SELECTOR, '#your-progress-card .progress-bar-text')
     count_text = count_element.text
