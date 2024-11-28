e228262e40ebbda72e9df89db52b39db0dada87c
boxadesign <info@boxadesign.co.uk>
2024-11-28 11:09:53 +0000

    Tweaks to cta banner

--- core/templatetags/content_tags.py
+++ core/templatetags/content_tags.py
@@ -720,7 +720,15 @@ def is_cheg_excluded_country(country_code):
 
 
 @register.inclusion_tag('_cta_banner.html')
-def render_signup_cta():
+def render_signup_cta(background=None, link=None):
+    background_class = 'great-ds-cta-banner--bg-white'
+    if background:
+        background_class = f'great-ds-cta-banner--bg-{background}'
+
+    link_color = ''
+    if link:
+        link_color = f'great-ds-action-link--{link}'
+
     return {
         'headingText': 'Accelerate your learning',
         'leadingText': "Sign up to Great.gov.uk and you'll be able to:",
@@ -729,7 +737,8 @@ def render_signup_cta():
             'Join live events from the UK Export Academy',
             'Compare markets using live export data',
         ],
-        'backgroundClass': 'great-ds-cta-banner--bg-white',
+        'backgroundClass': background_class,
+        'actionLinkClass': link_color,
         'signInLink': {'href': '/login', 'preLinkText': 'Already signed up?', 'linkText': 'Sign in'},
         'signUpLink': {'href': '/signup', 'linkText': 'Sign up to get started'},
         'landscapeImagePath': '/static/images/lte-signup-promo-landscape.png',
