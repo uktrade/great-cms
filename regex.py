1d4603efee6d6bb116effd42a2808b35264be729
bobby-didcoding <bobby@didcoding.com>
2024-12-10 11:01:02 +0000

    Run black on /Users/bobbystearman/development/great-cms/regex.py

--- regex.py
+++ regex.py
@@ -11,5 +11,6 @@ PHONE_NUMBER_REGEX_SIGNUP = re.compile(
         ?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)\
         ?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]\
         ?(?:x|ext\.?|\#)\d{3,4})?$\
-    ')
+    '
+)
 EMAIL_ADDRESS_REGEX = re.compile(r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$')
