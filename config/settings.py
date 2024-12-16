b9bb26e2edc15fa0bd27fd5e554239e1fff9ade2
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2024-12-16 10:57:06 +0000

    UPDATED feature flags ADDED Logging

--- config/settings.py
+++ config/settings.py
@@ -875,6 +875,7 @@ FEATURE_GUIDED_JOURNEY = env.feature_guided_journey
 FEATURE_UNGUIDED_JOURNEY = env.feature_unguided_journey
 FEATURE_OPENSEARCH = env.feature_opensearch
 FEATURE_SEARCH_PREVIEW = env.feature_search_preview
+FEATURE_ACTIVITY_STREAM = env.feature_activity_stream
 FEATURE_GUIDED_JOURNEY_EXTRAS = env.feature_guided_journey_extras
 FEATURE_GUIDED_JOURNEY_ENHANCED_SEARCH = env.feature_guided_journey_enhanced_search
 
