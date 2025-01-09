72bd0477d6b9c05e25fe78be3d1f1c9aa3005901
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-01-09 14:24:56 +0000

    add template tag for Guide document

---
+++ international_buy_from_the_uk/templatetags/document_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.conf import settings
+from wagtail.documents.models import Document
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_document_link(document_title: str) -> str:
+    try:
+        document = Document.objects.get(title=document_title)
+    except Document.DoesNotExist:
+        return ''
+    else:
+        return f'{settings.BASE_URL}/documents/{document.id}/{document_title}.pdf'
