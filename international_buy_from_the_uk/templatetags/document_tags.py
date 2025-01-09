49f539d07280363ebc93d5cbaf56fc42ab1aa3e2
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-01-09 15:49:05 +0000

    revise document link template tag

--- international_buy_from_the_uk/templatetags/document_tags.py
+++ international_buy_from_the_uk/templatetags/document_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.conf import settings
 from wagtail.documents.models import Document
 
 register = template.Library()
@@ -12,4 +11,4 @@ def get_document_link(document_title: str, document_type) -> str:
     except Document.DoesNotExist:
         return ''
     else:
-        return f'{settings.BASE_URL}/documents/{document.id}/{document_title}.{document_type}'
+        return f'/documents/{document.id}/{document_title}.{document_type}'
