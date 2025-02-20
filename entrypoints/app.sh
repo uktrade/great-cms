7732a200e6b6b80e10e584f2794ec64a9df663dc
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2025-02-20 11:19:07 +0000

    feature: add datadog logging.

---
+++ entrypoints/app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py migrate --noinput
+
+if [[ "$COPILOT_ENVIRONMENT_NAME" == "dev" ]]; then
+   ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT
+else
+   gunicorn config.wsgi --bind 0.0.0.0:$PORT
+fi
---
+++ entrypoints/celery-beat.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ "$COPILOT_ENVIRONMENT_NAME" == "dev" ]]; then
+   ddtrace-run celery -A config beat -l info -S django
+else
+   gunicorn celery -A config beat -l info -S django
+fi
