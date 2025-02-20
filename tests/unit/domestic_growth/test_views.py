b08b1fa11aa26cca357317b7bac6b46b0885a7a6
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2025-02-20 12:45:56 +0000

    BGST-12: Add domestic growth app

---
+++ tests/unit/domestic_growth/test_views.py
@@ -0,0 +1,42 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'page_url,form_data,redirect_url',
+    (
+        (
+            reverse('domestic_growth:domestic-growth-starting-a-business'),
+            {
+                'sector': 'Aerospace',
+                'postcode': 'SW1A 1AA',  # /PS-IGNORE
+            },
+            reverse('domestic_growth:domestic-growth-starting-a-business-results'),
+        ),
+        (
+            reverse('domestic_growth:domestic-growth-scaling-a-business'),
+            {
+                'country': 'uk',
+                'sector': 'Aerospace',
+                'business_stage': 'startup',
+                'postcode': 'SW1A 1AA',  # /PS-IGNORE
+            },
+            reverse('domestic_growth:domestic-growth-scaling-a-business-results'),
+        ),
+    ),
+)
+@pytest.mark.django_db
+def test_business_growth_triage(
+    page_url,
+    form_data,
+    redirect_url,
+    client,
+):
+    response = client.post(
+        page_url,
+        form_data,
+    )
+
+    assert response.status_code == 302
+    assert response.url == redirect_url
