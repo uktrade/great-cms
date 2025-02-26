cc26d514db628c9eb58cce324fc309d38b6da07c
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2025-02-26 09:01:23 +0000

    feature: add datadog logging.

--- entrypoints/app.sh
+++ entrypoints/app.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-python manage.py migrate --noinput && ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT
+python manage.py migrate --noinput
 
-#if [[ "$COPILOT_ENVIRONMENT_NAME" == "dev" ]]; then
-#   ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT
-#else
-#   gunicorn config.wsgi --bind 0.0.0.0:$PORT
-#fi
+if [[ "$COPILOT_ENVIRONMENT_NAME" == "dev" ]]; then
+   echo "Dev Environment Detected"
+   echo "$COPILOT_ENVIRONMENT_NAME"
+   ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT
+else
+   echo "Dev Environment Not Detected"
+   echo "$COPILOT_ENVIRONMENT_NAME"
+   gunicorn config.wsgi --bind 0.0.0.0:$PORT
+fi
