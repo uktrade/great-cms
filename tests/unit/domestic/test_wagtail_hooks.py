511cc0985b23aa48a34bf7f3c40d784a883db22a
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-02-27 23:06:11 +0000

    Fix delete button width

--- tests/unit/domestic/test_wagtail_hooks.py
+++ tests/unit/domestic/test_wagtail_hooks.py
@@ -2,17 +2,11 @@ from unittest import mock
 
 from django.test import override_settings
 
-from domestic.wagtail_hooks import editor_css, global_admin_css
+from domestic.wagtail_hooks import global_admin_css
 
 
 def _fake_static(value):
     return '/path/to/static/' + value
-
-
-@mock.patch('domestic.wagtail_hooks.static')
-def test_editor_css(mock_static):
-    mock_static.side_effect = _fake_static
-    assert editor_css() == '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic-editor.css">'
 
 
 @override_settings(ENVIRONMENT_CSS_THEME_FILE='')  # '' is the default
