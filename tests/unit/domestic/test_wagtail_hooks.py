9484b62446422f1fff71b7de09248b1415428c57
root <root@greatcms.trade.great>
2025-03-03 10:36:50 +0000

    failing tests

--- tests/unit/domestic/test_wagtail_hooks.py
+++ tests/unit/domestic/test_wagtail_hooks.py
@@ -13,15 +13,19 @@ def _fake_static(value):
 @mock.patch('domestic.wagtail_hooks.static')
 def test_global_admin_css__no_customisation(mock_static):
     mock_static.side_effect = _fake_static
-    assert global_admin_css() == '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic.css">'
+    assert global_admin_css() == (
+        '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic.css">'
+        '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic-editor.css'
+    )
 
 
 @override_settings(ENVIRONMENT_CSS_THEME_FILE='path/to/custom/file.css')
 @mock.patch('domestic.wagtail_hooks.static')
 def test_global_admin_css__with_customisation(mock_static):
     mock_static.side_effect = _fake_static
-
+    breakpoint()
     assert global_admin_css() == (
         '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic.css">'
+        '<link rel="stylesheet" href="/path/to/static/cms-admin/css/domestic-editor.css'
         '<link rel="stylesheet" href="/path/to/static/path/to/custom/file.css">'
     )
