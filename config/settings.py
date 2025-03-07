02ca28f607a4f47cc5d343403af3dda0f727806a
Peter Hudec <peterhudec@peterhudec.com>
2025-03-07 14:19:46 +0000

    Added the figures.html include
    and implemented with it the market data and annual salary sections.

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,8 @@ TEMPLATES = [
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'cta_banner',
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'action_link',
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'responsive_image',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'select',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'label',
             ROOT_DIR
             / 'node_modules'
             / '@uktrade'
