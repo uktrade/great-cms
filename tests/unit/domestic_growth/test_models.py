6bf9a6f540f04c49c0726f680fefe7d2ffafad51
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2025-03-14 13:36:45 +0000

    BGST-176: initial content snippet model

--- tests/unit/domestic_growth/test_models.py
+++ tests/unit/domestic_growth/test_models.py
@@ -6,6 +6,7 @@ from .factories import (
     DomesticGrowthGuidePageFactory,
     DomesticGrowthChildGuidePageFactory,
 )
+from domestic_growth.models import DomesticGrowthContent
 
 
 class DomesticGrowthHomePageTests(SetUpLocaleMixin, WagtailPageTests):
@@ -43,28 +44,40 @@ class DomesticGrowthHomePageTests(SetUpLocaleMixin, WagtailPageTests):
 
 
 class DomesticGrowthGuidePageTests(SetUpLocaleMixin, WagtailPageTests):
-    def test_can_create_homepage(self):
-        homepage = DomesticGrowthGuidePageFactory(
+    def test_can_create_page(self):
+        page = DomesticGrowthGuidePageFactory(
             hero_title='Test title',
             hero_intro='Test intro',
             body_title='Test title',
             body_intro='Test intro',
         )
-        self.assertEqual(homepage.title, 'guidepage')
+        self.assertEqual(page.title, 'guidepage')
 
-        self.assertEqual(homepage.hero_title, 'Test title')
-        self.assertEqual(homepage.hero_intro, 'Test intro')
-        self.assertEqual(homepage.body_title, 'Test title')
-        self.assertEqual(homepage.body_intro, 'Test intro')
+        self.assertEqual(page.hero_title, 'Test title')
+        self.assertEqual(page.hero_intro, 'Test intro')
+        self.assertEqual(page.body_title, 'Test title')
+        self.assertEqual(page.body_intro, 'Test intro')
 
 
 class DomesticGrowthChildGuidePageTests(SetUpLocaleMixin, WagtailPageTests):
-    def test_can_create_homepage(self):
-        homepage = DomesticGrowthChildGuidePageFactory(
+    def test_can_create_page(self):
+        page = DomesticGrowthChildGuidePageFactory(
             body_title='Test title',
             body_intro='Test intro',
         )
-        self.assertEqual(homepage.title, 'child-guidepage')
+        self.assertEqual(page.title, 'child-guidepage')
 
-        self.assertEqual(homepage.body_title, 'Test title')
-        self.assertEqual(homepage.body_intro, 'Test intro')
+        self.assertEqual(page.body_title, 'Test title')
+        self.assertEqual(page.body_intro, 'Test intro')
+
+
+class DomesticGrowthContentTests(SetUpLocaleMixin, WagtailPageTests):
+    def test_can_create_snippet(self):
+        snippet = DomesticGrowthContent.objects.create(
+            content_id='123', title='Test title', description='Test description', url='www.url.com'
+        )
+
+        self.assertEqual(snippet.content_id, '123')
+        self.assertEqual(snippet.title, 'Test title')
+        self.assertEqual(snippet.description, 'Test description')
+        self.assertEqual(snippet.url, 'www.url.com')
