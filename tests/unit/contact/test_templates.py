8072d1b0204273487158e98655810bdd81ff3baa
bobby-didcoding <bobby@didcoding.com>
2025-03-16 09:25:54 +0000

    fixed bug in 'old' forms

--- tests/unit/contact/test_templates.py
+++ tests/unit/contact/test_templates.py
@@ -1,7 +1,7 @@
 import pytest
 from django.conf import settings
 from django.template.loader import render_to_string
-from great_tags.context_processors import urls_processor
+from gds_tooling.context_processors import urls_processor
 
 pytestmark = pytest.mark.django_db
 
