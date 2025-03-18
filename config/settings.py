9d88049942b56603dc2494a9d87908f5c188e8f7
bobby-didcoding <bobby@didcoding.com>
2025-03-18 14:52:38 +0000

    updated settings to use gds app

--- config/settings.py
+++ config/settings.py
@@ -103,6 +103,7 @@ INSTALLED_APPS = [
     'wagtail_localize',
     'wagtail_localize.locales',
     'domestic_growth.apps.DomesticGrowthConfig',
+    'great_design_system'
 ]
 
 MIDDLEWARE = [
@@ -156,7 +157,12 @@ TEMPLATES = [
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'responsive-image',
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'input',
             ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'label',
-            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'title-arrow',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'hint',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'error-message',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'error-summary',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'textarea',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'forms',
+            ROOT_DIR / 'node_modules' / '@uktrade' / 'great-design-system' / 'dist' / 'components' / 'widgets',
             ROOT_DIR
             / 'node_modules'
             / '@uktrade'
@@ -200,6 +206,8 @@ TEMPLATES = [
         },
     },
 ]
+
+FORM_RENDERER = 'great_design_system.forms.renderers.GDSDivFormRenderer'
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
