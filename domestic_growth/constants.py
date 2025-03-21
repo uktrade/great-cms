d50da1129403105fe83ce0d24ce7c3dc2678c83c
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2025-03-21 18:27:34 +0000

    BGST-176: content snippet - dynamic rendering solution

---
+++ domestic_growth/constants.py
@@ -0,0 +1,10 @@
+DYNAMIC_SNIPPET_NAMES = (
+    ('BGS_5_004', 'Finance and support for your business'),
+    ('BGS_5_006', 'British Business Bank Regional funds'),
+    ('BGS_5_010', 'Find a grant'),
+    ('BGS_6_001', 'Innovate UK Catapult Network'),
+    ('BGS_6_002', 'Innovate UK Business Connect'),
+    ('BGS_9_21', 'Apprenticeships placeholder'),
+    ('BGS_12_005', 'Talk to an International Trade Adviser'),
+    ('BGS_13_001', 'Contracts finder'),
+)
