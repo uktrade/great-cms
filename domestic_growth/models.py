631bb048f43fb376efcf66e786e8224c71fa4ac5
Timothy Patterson <tim.patterson@digital.trade.gov.uk>
2025-03-21 09:28:07 +0000

    wip

--- domestic_growth/models.py
+++ domestic_growth/models.py
@@ -1,23 +1,18 @@
 from django.db import models
-
-from domestic_growth import (
-    cms_panels,
-    helpers,
-)
 from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
 from wagtail.blocks.stream_block import StreamBlock
-from wagtail.fields import StreamField, RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
-from wagtailseo.models import SeoMixin
 from wagtail.search import index
 from wagtail.snippets.blocks import SnippetChooserBlock
-from wagtail.admin.panels import (
-    FieldPanel,
-)
 from wagtail.snippets.models import register_snippet
+from wagtailseo.models import SeoMixin
 
-
+from core.models import TimeStampedModel
+from domestic_growth import cms_panels, helpers
 from domestic_growth.blocks import DomesticGrowthCardBlock
+from international_online_offer.models import TradeAssociation
 
 
 class DomesticGrowthHomePage(SeoMixin, cms_panels.DomesticGrowthHomePagePanels, Page):
@@ -132,6 +127,7 @@ class DomesticGrowthHomePage(SeoMixin, cms_panels.DomesticGrowthHomePagePanels, 
     def get_context(self, request):
         context = super(DomesticGrowthHomePage, self).get_context(request)
         context['news'] = helpers.get_dbt_news_articles()
+        context['trade_associations'] = TradeAssociation.objects.all()
         return context
 
 
@@ -226,3 +222,15 @@ class DomesticGrowthContent(index.Indexed, models.Model):
 
     def __str__(self):
         return self.title
+
+
+class StartingABusinessUser(TimeStampedModel):
+    # never assume email is unique in this table as users can complete the triage in different
+    # browsers / incognito mode
+    email = models.CharField(max_length=255, null=True, blank=True)
+    # the session_id is either a django session id from request.session.session_key or
+    # in the case where a user has not accepted cookies a UUIDV4
+    session_id = models.CharField(max_length=40, unique=True)
+    sector_id = models.CharField(max_length=10, null=True, blank=True)
+    dont_know_sector = models.BooleanField(default=False, null=True, blank=True)
+    postcode = models.CharField(max_length=8, null=True, blank=True)
--- domestic_growth/views.py
+++ domestic_growth/views.py
@@ -1,124 +1,59 @@
-import pickle
-
+from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
-from domestic_growth.forms import StartingABusinessSectorForm
-from domestic_growth.helpers import get_postcode_data
+from domestic_growth.forms import (
+    StartingABusinessLocationForm,
+    StartingABusinessSectorForm,
+)
 from domestic_growth.mixins import TriageMixin
-
-
-class StartingABusinessSectorFormView(FormView):
-    template_name = 'starting-a-business/starting-a-business-sector.html'
-    form_class = StartingABusinessSectorForm
+from domestic_growth.models import StartingABusinessUser
+from international_online_offer.core import region_sector_helpers
+from international_online_offer.services import get_dbt_sectors
 
 
 class StartingABusinessLocationFormView(FormView):
-    pass
+    template_name = 'starting-a-business/triage-location.html'
+    form_class = StartingABusinessLocationForm
+    success_url = reverse_lazy('domestic_growth:domestic-growth-starting-a-business-sector')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            StartingABusinessUser.objects.update_or_create(
+                session_key=self.request.session.session_key,
+                defaults={
+                    'postcode': form.cleaned_data['postcode'],
+                },
+            )
+
+        return super().form_valid(form)
 
 
-class StartingABusinessResultsView(TriageMixin, TemplateView):
-    template_name = 'starting-a-business-results.html'
+class StartingABusinessSectorFormView(FormView):
+    template_name = 'starting-a-business/triage-sector.html'
+    form_class = StartingABusinessSectorForm
+    success_url = reverse_lazy('domestic_growth:domestic-growth-starting-a-business-results')
 
     def get_context_data(self, **kwargs):
-        constituency = None
-        council = None
-        sector = None
-
-        sector_content = {
-            'Advanced manufacturing': [
-                'Advanced manufacturing 1',
-                'Advanced manufacturing 2',
-                'Advanced manufacturing 3',
-            ],
-            'Aerospace': ['Aerospace 1', 'Aerospace 2', 'Aerospace 3'],
-            'Food and drink': ['Food and drink 1', 'Food and drink 2', 'Food and drink 3'],
-        }
-
-        if self.request.session.get('domestic_growth_triage_data'):
-            form_data = pickle.loads(bytes.fromhex(self.request.session.get('domestic_growth_triage_data')))[0]
-            postcode = form_data.get('postcode')
-            sector = form_data.get('sector')
-
-            data = get_postcode_data(postcode)
-
-            constituency = data.get('result').get('parliamentary_constituency_2024')
-            council = data.get('result').get('admin_district')
+        dbt_sectors = get_dbt_sectors()
+        autocomplete_sector_data = region_sector_helpers.get_sectors_as_string(dbt_sectors)
 
         return super().get_context_data(
             **kwargs,
-            constituency=constituency,
-            council=council,
-            growth_hub={'name': 'The Growth Hub'},
-            sector_content=sector_content.get(sector),
+            autocomplete_sector_data=autocomplete_sector_data,
         )
 
+    def form_valid(self, form):
+        if form.is_valid():
+            StartingABusinessUser.objects.update_or_create(
+                session_key=self.request.session.session_key,
+                defaults={
+                    'sector_id': form.cleaned_data['sector'],
+                    'dont_know_sector': form.cleaned_data['dont_know_sector_yet'],
+                },
+            )
 
-# class StartingABusinessView(TriageMixin, FormView):
-#     form_class = StartingABusinessForm
-#     template_name = 'starting-a-business.html'
-
-#     def get_context_data(self, **kwargs):
-#         return super().get_context_data(
-#             **kwargs,
-#         )
-
-#     def get_success_url(self):
-#         return reverse_lazy('domestic_growth:domestic-growth-starting-a-business-results')
-
-#     def form_valid(self, form):
-#         self.save_data(form)
-#         return super().form_valid(form)
+        return super().form_valid(form)
 
 
-# class ScalingABusinessView(TriageMixin, FormView):
-#     form_class = ScalingABusinessForm
-#     template_name = 'scaling-a-business.html'
-
-#     def get_context_data(self, **kwargs):
-#         return super().get_context_data(
-#             **kwargs,
-#         )
-
-#     def get_success_url(self):
-#         return reverse_lazy('domestic_growth:domestic-growth-scaling-a-business-results')
-
-#     def form_valid(self, form):
-#         self.save_data(form)
-#         return super().form_valid(form)
-
-
-# class ScalingABusinessResultsView(TriageMixin, TemplateView):
-#     template_name = 'scaling-a-business-results.html'
-
-#     def get_context_data(self, **kwargs):
-#         constituency = None
-#         council = None
-#         sector = None
-
-#         sector_content = {
-#             'Advanced manufacturing': [
-#                 'Advanced manufacturing 1',
-#                 'Advanced manufacturing 2',
-#                 'Advanced manufacturing 3',
-#             ],
-#             'Aerospace': ['Aerospace 1', 'Aerospace 2', 'Aerospace 3'],
-#             'Food and drink': ['Food and drink 1', 'Food and drink 2', 'Food and drink 3'],
-#         }
-
-#         if self.request.session.get('domestic_growth_triage_data'):
-#             form_data = pickle.loads(bytes.fromhex(self.request.session.get('domestic_growth_triage_data')))[0]
-#             postcode = form_data.get('postcode')
-#             sector = form_data.get('sector')
-
-#             data = get_postcode_data(postcode)
-
-#             constituency = data.get('result').get('parliamentary_constituency_2024')
-#             council = data.get('result').get('admin_district')
-
-#         return super().get_context_data(
-#             **kwargs,
-#             constituency=constituency,
-#             council=council,
-#             growth_hub={'name': 'The Growth Hub'},
-#             sector_content=sector_content.get(sector),
-#         )
+class StartingABusinessResultsView(TriageMixin, TemplateView):
+    template_name = 'starting-a-business/results.html'
