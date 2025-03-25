11e0c3cc39790b76120b291aa3a27bad331a18fb
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2025-03-25 14:52:36 +0000

    feature: update seo ingestion script

--- core/management/commands/ingest_seo_data.py
+++ core/management/commands/ingest_seo_data.py
@@ -1,5 +1,6 @@
 import csv
 from io import StringIO
+from urllib.parse import urlparse
 
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -7,24 +8,41 @@ from django.db import transaction
 from wagtail.models import Page
 
 
+
+
 class Command(BaseCommand):
     help = 'Uploads SEO information to Wagtail articles'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--source', type=str, required=True, help='Source wagtail page (e.g., /bau-homepage-new)'
+        )
+
+    @transaction.atomic
     def handle(self, *args, **options):
+        source_hostname = options['source']
+        print('Ingesting SEO Data')
+
         with open(
             settings.ROOT_DIR / 'core/fixtures/seo-data.csv', 
             'r',
             encoding='utf-8',
         ) as f:
-            with transaction.atomic():
-                for row in csv.DictReader(StringIO(f.read()), delimiter=','):
-                    url_path = row.get('url_path')
-                    seo_title = row.get('seo_title')
-                    search_description = row.get('search_description') 
+            for row in csv.DictReader(StringIO(f.read()), delimiter=','):
+                # Construct URL path
+                url_path = urlparse(row.get('URL')).path
+                full_url_path = source_hostname + url_path
 
-                    page = Page.objects.live().filter(url_path=url_path).first()
-                    if page:
-                        page.update(
-                            seo_title=seo_title,
-                            search_description=search_description
-                        )
+                # Get metadata
+                seo_title = row.get('Title')
+                search_description = row.get('Description') 
+
+                page = Page.objects.live().filter(url_path=full_url_path, locale__language_code='en-gb').first()
+                if page:
+                    breakpoint()
+                    page.seo_title=seo_title,
+                    page.search_description=search_description
+                    page.save()
+
+                    print(f'Updated SEO Title & Description for {url_path}')
+            print('Data ingestion finished')        
