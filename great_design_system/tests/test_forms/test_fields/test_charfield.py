05a6ab86a088afa773b91c490f2dd884c600cda3
bobby-didcoding <bobby@didcoding.com>
2025-04-17 14:24:12 +0100

    added expected html files to simplify render tests

--- great_design_system/tests/test_forms/test_fields/test_charfield.py
+++ great_design_system/tests/test_forms/test_fields/test_charfield.py
@@ -93,58 +93,12 @@ def test_char_field_with_valid_invalid_text_combinations(form, form_data, form_i
 
 def test_char_field_form_render():
 
-    expected = """
-        <div class="govuk-error-summary great-hidden" data-module="govuk-error-summary">
-            <div role="alert">
-                <h2 class="govuk-error-summary__title">
-                    There was a problem
-                </h2>
-                <div class="govuk-error-summary__body">
-                    <p>
-                        There was a problem with the form submission
-                    </p>
-                    <ul class="govuk-list govuk-error-summary__list"></ul>
-                </div>
-            </div>
-        </div>
-        <div class="govuk-form-group" id="id_my_required_text_container">
-            <label class="govuk-label" for="id_my_required_text">
-                My required text
-            </label>
-            <input class="govuk-input" id="id_my_required_text" name="my_required_text" type="text"/>
-        </div>
-        <div class="govuk-form-group" id="id_my_text_container">
-            <label class="govuk-label" for="id_my_text">
-                My text
-            </label>
-            <input class="govuk-input" id="id_my_text" name="my_text" type="text"/>
-        </div>
-        <div class="govuk-form-group" id="id_my_required_text_with_required_error_message_override_container">
-            <label class="govuk-label" for="id_my_required_text_with_required_error_message_override">
-                My required text with required error message override
-            </label>
-            <input class="govuk-input" id="id_my_required_text_with_required_error_message_override"
-            name="my_required_text_with_required_error_message_override" type="text"/>
-        </div>
-        <div class="govuk-form-group" id="id_my_text_with_invalid_error_message_override_container">
-            <label class="govuk-label" for="id_my_text_with_invalid_error_message_override">
-                My text with invalid error message override
-            </label>
-            <input class="govuk-input" id="id_my_text_with_invalid_error_message_override"
-            name="my_text_with_invalid_error_message_override" type="text"/>
-        </div>
-        <div class="govuk-form-group" id="id_my_text_with_postcode_validator_container">
-            <label class="govuk-label" for="id_my_text_with_postcode_validator">
-                My text with postcode validator
-            </label>
-            <input class="govuk-input" id="id_my_text_with_postcode_validator"
-            name="my_text_with_postcode_validator" type="text"/>
-        </div>
-    """
-
+    html = open('./great_design_system/tests/test_forms/expected_html/char_field.html', 'r')
+    expected = html.read()
     actual = str(CharFieldForm())
-
     actual_stripped_html = BeautifulSoup(actual, 'html.parser').get_text(strip=True)
     expected_stripped_html = BeautifulSoup(expected, 'html.parser').get_text(strip=True)
-
     assert actual_stripped_html == expected_stripped_html
+
+    # Close the file
+    html.close()
