aa2035efb67ff108d593fc26fc8418de37119602
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-05-08 14:02:14 +0100

    Added python script to Update parameter store from json file

---
+++ scripts/update_params.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+
+
+def print_usage():
+    print("Usage: python -m update_params <json file name>")
+    sys.exit(1)
+
+
+def update_params(file_name):
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+        for param in data:
+            name = param['Name']
+            value = param['Value']
+            type = param['Type']
+            overwrite = param['Overwrite']
+            if overwrite:
+                over_str = '--overwrite'
+            else:
+                over_str = '--no-overwrite'
+            cmd = f'aws ssm put-parameter --name "{name}" --type {type} --value "{value}" {over_str}'
+            ret = os.system(cmd)
+            print(ret)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print_usage()
+    file_name = sys.argv[1]
+    update_params(file_name)
