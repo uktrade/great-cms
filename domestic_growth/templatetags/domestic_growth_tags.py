a49f5268e5a01c4dc1d327e9a0e11268e07adf8d
boxadesign <info@boxadesign.co.uk>
2025-05-09 14:17:32 +0100

    Upgrade DS version and rework templates

--- domestic_growth/templatetags/domestic_growth_tags.py
+++ domestic_growth/templatetags/domestic_growth_tags.py
@@ -11,12 +11,30 @@ def dict_merge(dict1, dict2_str):
     return {**dict1, **dict2}
 
 @register.filter
-def get_trade_association_tags(ta, sector):
+def gt(value, arg):
+    try:
+        return int(value) > int(arg)
+    except (ValueError, TypeError):
+        return False
+
+@register.filter
+def get_trade_association_tags(ta, sector_str):
     tags = []
-    if ta.type == 'sub_sector' and sector.get('sub_sector'):
-        tags.append({'text': sector['sub_sector'], 'type': 'sector'})
-    elif ta.type == 'sector' and sector.get('sector'):
-        tags.append({'text': sector['sector'], 'type': 'sector'})
-    if ta.regions:
-        tags.append({'text': ta.regions, 'type': 'location'})
+    try:
+        sector = ast.literal_eval(str(sector_str))
+        if isinstance(ta, dict):
+            if ta.get('type') == 'sub_sector' and sector.get('sub_sector'):
+                tags.append({'text': sector['sub_sector'], 'type': 'sector'})
+            elif ta.get('type') == 'sector' and sector.get('sector'):
+                tags.append({'text': sector['sector'], 'type': 'sector'})
+            if ta.get('regions'):
+                tags.append({'text': ta['regions'], 'type': 'location'})
+    except (ValueError, SyntaxError):
+        pass
     return tags
+
+@register.filter
+def create_card_attributes(value):
+    if value:
+        return {'data-hidden-ta': 'true'}
+    return {}
