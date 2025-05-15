6c6feb6f07d34ba9e8220dfbcea389de422afe00
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2025-05-15 10:16:05 +0100

    Update middleware.py

--- international/middleware.py
+++ international/middleware.py
@@ -10,12 +10,10 @@ class CheckForBGSDomainMiddleware(MiddlewareMixin):
     def process_request(self, request):
         absolute_uri = request.build_absolute_uri()
         parts = absolute_uri.split('/')
-        for part in parts[:]:
-            if not part.strip():
-                parts.remove(part)
-        url = f"/{'/'.join(parts[3:])}"
-        if is_bgs_domain(request) and f'/{settings.GREAT_INTERNATIONAL_URL}/' in absolute_uri:
+        stripped_parts = parts[:3] + [part for part in parts[3:] if part.strip()]
+        url = f"/{'/'.join(stripped_parts[3:])}"
+        if is_bgs_domain(request) and f'/{settings.GREAT_INTERNATIONAL_URL}' in url:
             bgs_international_url = url.replace(
-                f'/{settings.GREAT_INTERNATIONAL_URL}/', f'/{settings.BGS_INTERNATIONAL_URL}/'
+                f'/{settings.GREAT_INTERNATIONAL_URL}', f'/{settings.BGS_INTERNATIONAL_URL}'
             )
             return redirect(bgs_international_url)
