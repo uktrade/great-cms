2e2084f5f191100756bca1b25fc785f4d1305bf3
Timothy Patterson <tim.patterson@digital.trade.gov.uk>
2025-06-06 09:32:54 +0100

    GREATUK-2153 updated redirect map (#4251)
    * updated redirect map

--- core/management/commands/update_redirects.py
+++ core/management/commands/update_redirects.py
@@ -14,6 +14,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--redirect-file-name', type=str, help='redirect-map.csv', default='redirect-map.csv')
         parser.add_argument(
+            '--redirect-to-domain',
+            type=str,
+            help='redirect to domain - do not include trailing slash',
+            default='https://www.business.gov.uk',
+        )
+        parser.add_argument(
             '--dry_run',
             action=argparse.BooleanOptionalAction,
             default=False,
@@ -32,6 +38,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dry_run = options['dry_run']
         redirect_file_name = options['redirect_file_name']
+        redirect_to_domain = options['redirect_to_domain']
 
         self.site = None  # site=None applies redirect to all sites
 
@@ -52,21 +59,25 @@ class Command(BaseCommand):
 
                 old_path = row.get('Current URL').strip()
                 redirect_path = row.get('New URL').strip()
+                redirect_path = Redirect.normalise_path(redirect_path)
+                redirect_url = f'{redirect_to_domain}{redirect_path}'
+
                 if len(old_path) > 255:
                     self.stdout.write(self.style.WARNING(f'Redirect old_path too long - {old_path}'))
                     total_urls_exceed_limit += 1
                     continue
-                if len(redirect_path) > 255:
+                if len(redirect_url) > 255:
                     self.stdout.write(self.style.WARNING(f'Redirect redirect_link too long - {redirect_path}'))
                     total_urls_exceed_limit += 1
                     continue
+
                 normalised_old_path = Redirect.normalise_path(old_path)
                 redirect = Redirect.objects.filter(old_path=normalised_old_path, site=self.site).first()
                 if redirect and not dry_run:
-                    self.update_redirect(redirect, redirect_path)
+                    self.update_redirect(redirect, redirect_url)
                     updated_count += 1
                 elif not dry_run:
-                    redirect = self.create_redirect(old_path, redirect_path)
+                    redirect = self.create_redirect(old_path, redirect_url)
                     created_count += 1
 
             self.stdout.write(self.style.SUCCESS(f'total_urls_exceed_limit: {total_urls_exceed_limit}'))
