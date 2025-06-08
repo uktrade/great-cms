4abffceb926e13c0bbc29b49fe484f3069ecf10a
David Urquhart <56790506+davidu1975@users.noreply.github.com>
2025-06-08 19:43:52 +0100

    Need raw string for url_path (#4248)
    * Need raw string for url_path
    
    * force build

--- core/management/commands/ingest_seo_data.py
+++ core/management/commands/ingest_seo_data.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
     help = 'Uploads SEO information to Wagtail articles'
 
     def add_arguments(self, parser):
-        parser.add_argument('--source', type=str, required=True, help='Source hostname (e.g., www.business.gov.uk)')
+        parser.add_argument('--source', type=str, required=True, help='Source path (e.g., /bgs-landing)')
 
     @transaction.atomic
     def handle(self, *args, **options):
-        source_hostname = options['source']
+        source = options['source']
         self.stdout.write(self.style.SUCCESS('Ingesting SEO Data'))
 
         with open(
@@ -27,13 +27,13 @@ class Command(BaseCommand):
             for row in csv.DictReader(StringIO(f.read()), delimiter=','):
                 # Construct URL path
                 url_path = urlparse(row.get('New business.gov.uk destination URL')).path
-                full_url_path = source_hostname + url_path
+                full_url_path = f'{source}{url_path}'
 
                 # Get metadata
                 seo_title = row.get('Meta Title')
                 search_description = row.get('Meta Description')
 
-                page = Page.objects.live().filter(url_path=full_url_path, locale__language_code='en-gb').first()
+                page = Page.objects.live().filter(url_path=rf'{full_url_path}', locale__language_code='en-gb').first()
                 if page:
                     page.seo_title = seo_title
                     page.search_description = search_description
