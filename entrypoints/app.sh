601e9cf2d6a6be69233816b464d7408a3722ac95
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2025-06-13 15:35:36 +0100

    Add uvicorn worker

--- entrypoints/app.sh
+++ entrypoints/app.sh
@@ -5,9 +5,9 @@ python manage.py migrate --noinput
 if [[ "$FEATURE_DATADOG" = true ]]; then
    echo "Datadog Detected"
    echo "$COPILOT_ENVIRONMENT_NAME"
-   ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT
+   ddtrace-run gunicorn config.wsgi --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker
 else
    echo "Datadog Not Detected"
    echo "$COPILOT_ENVIRONMENT_NAME"
-   gunicorn config.wsgi --bind 0.0.0.0:$PORT
+   gunicorn config.wsgi --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker
 fi
